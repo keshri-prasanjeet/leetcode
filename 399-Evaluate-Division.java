a01b81b6c4dc454e47d516e0a286b71e2c8259f9
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-09-30 06:46:50 +0530

    [399] [Time Beats: 98.77%] [Memory Beats: 63.47%] - LeetPush

--- 399-Evaluate-Division.java
+++ 399-Evaluate-Division.java
@@ -2,7 +2,6 @@ class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         Map<String, List<List<Object>>> adjacencyList = new HashMap<>();
         for(int i=0;i<equations.size();i++){
-            
             String fir = equations.get(i).get(0);
             String sec = equations.get(i).get(1);
             double value = values[i];
@@ -13,6 +12,7 @@ class Solution {
             adjacencyList.putIfAbsent(sec, new ArrayList<>());
             adjacencyList.get(sec).add(Arrays.asList(fir, 1.0/value));
         }
+
         double[] answer = new double[queries.size()];
         for(int i=0;i<queries.size();i++){
             answer[i] = bfs(queries.get(i).get(0), queries.get(i).get(1), adjacencyList);
@@ -28,16 +28,18 @@ class Solution {
 
         q.offer(new Object[]{src, 1.0});
         visited.add(src);
-        
         while(!q.isEmpty()){
             Object [] head = q.poll();
             String node = (String) head[0];
-            double weight = (double) head[1];
-            if(node.equals(target)) return weight;
-            for(List<Object> srcNeighbors: adjacencyList.get(node)){
-                String nei = (String) srcNeighbors.get(0);
-                Double wei = (Double) srcNeighbors.get(1);
-                double newWeight = wei * weight;
+            double value = (double) head[1];
+
+            if(node.equals(target)) return value;
+
+            for(List<Object> neighbors: adjacencyList.get(node)){
+                String nei = (String) neighbors.get(0);
+                double wei = (double) neighbors.get(1);
+
+                double newWeight = wei * value;
                 if(!visited.contains(nei)){
                     q.offer(new Object[]{nei, newWeight});
                     visited.add(nei);
