c1c36cf9d0a51bf88f0c3772d74bc83b43bd5077
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-10-30 03:27:54 +0530

    [977] [Time Beats: 100.00%] [Memory Beats: 24.93%] - LeetPush

--- 977-Squares-of-a-Sorted-Array.java
+++ 977-Squares-of-a-Sorted-Array.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int start = 0;
-        int end = nums.length-1;
-        int[] ans = new int[nums.length];
-        int index = end;
-        while(start<=end){
-            if(squareVal(nums[start]) > squareVal(nums[end])){
-                ans[index--] = squareVal(nums[start]);
-                start++;
+        int[] sortedArray = new int[nums.length];
+        int index = nums.length-1;
+        int left = 0;
+        int right = nums.length-1;
+        while(index >=0 && left >=0 && right < nums.length && left <= right){
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+            if(leftSq >= rightSq){
+                sortedArray[index] = leftSq;
+                left++;
             }
             else{
-                ans[index--] = squareVal(nums[end]);
-                end--;
+                sortedArray[index] = rightSq;
+                right--;
             }
+            index--;
         }
-        return ans;
-    }
-
-    private int squareVal(int num){
-        return num*num;
+        return sortedArray;
     }
 }
