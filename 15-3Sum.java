9ff4db676ab28b8992ab5859e4c8681dd34b113a
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-10-30 03:02:16 +0530

    [15] [Time Beats: 67.19%] [Memory Beats: 18.33%] - LeetPush

--- 15-3Sum.java
+++ 15-3Sum.java
@@ -1,31 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> answers = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> triplets = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             int fixed = nums[i];
             int left = i+1;
             int right = nums.length-1;
-            while(left<right){
+            while(left < right){
                 int curSum = fixed + nums[left] + nums[right];
-                if(curSum==0){
+                if(curSum == 0){
                     List<Integer> triplet = new ArrayList<>();
                     triplet.add(fixed);
                     triplet.add(nums[left]);
                     triplet.add(nums[right]);
-                    answers.add(triplet);
+                    triplets.add(triplet);
                     left++;
-                    while(left<right && nums[left]==nums[left-1]){
-                        left++;
-                    }
+                    while(left < right && nums[left]==nums[left-1]) left++;
                 }
-                else if(curSum > 0){
-                    right--;
-                }
+                else if(curSum > 0) right--;
                 else left++;
             }
         }
-        return answers;
+        return triplets;
     }
 }
