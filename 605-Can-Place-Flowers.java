13af608f5a80e9d14d5cc688af9abeee135e978f
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-11-09 00:23:08 +0530

    [605] [Time Beats: 96.72%] [Memory Beats: 47.42%] - LeetPush

---
+++ 605-Can-Place-Flowers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int bedLen = flowerbed.length;
+        int maxPossibleFlowers = (bedLen+1)/2;
+        int count = 0;
+        
+        for(int i=0;i<bedLen;i++){
+            if(n==0) return true;
+            if(flowerbed[i]==0 &&
+                (i==0 || flowerbed[i-1]==0) &&
+                    (i==bedLen-1 || flowerbed[i+1]==0)){
+                        flowerbed[i] = 1;
+                        n--;
+                    }
+        }
+        if(n==0) return true;
+        return false;
+    }
+}
