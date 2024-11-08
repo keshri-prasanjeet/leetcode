22494e1303d01ccb8d4aa056fb6ed8be6a7f6c89
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-11-08 18:48:33 +0530

    [238] [Time Beats: 99.64%] [Memory Beats: 93.95%] - LeetPush

---
+++ 238-Product-of-Array-Except-Self.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int [] aux = new int[nums.length];
+        int left = 1;
+        for(int i=0;i<nums.length;i++){
+            aux[i] = left;
+            left *= nums[i];
+        }
+        int right =1;
+        for(int i=nums.length-1;i>=0;i--){
+            aux[i] *= right;
+            right *= nums[i];
+        }
+
+        return aux;
+    }
+}
