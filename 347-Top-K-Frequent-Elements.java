5591070a36a08dc8719a3653ffb71e2ae3907578
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-11-08 17:33:24 +0530

    [347] [Time Beats: 92.08%] [Memory Beats: 95.13%] - LeetPush

--- 347-Top-K-Frequent-Elements.java
+++ 347-Top-K-Frequent-Elements.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> numMap = new HashMap<>();
+        Map<Integer,Integer> numFreq = new HashMap<>();
         for(Integer num: nums){
-            numMap.put(num, numMap.getOrDefault(num, 0)+1);
+            numFreq.put(num, numFreq.getOrDefault(num,0)+1);
         }
-
         Queue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
-        for(Map.Entry<Integer,Integer> entry:numMap.entrySet()){
+
+        for(Map.Entry<Integer,Integer> entry:numFreq.entrySet()){
             pq.add(entry);
         }
+
         int[] answer = new int[k];
         for(int i=0;i<k;i++){
             answer[i] = pq.poll().getKey();
