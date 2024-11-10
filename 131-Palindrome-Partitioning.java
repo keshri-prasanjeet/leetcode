1bcf9a9de675633467e9f71d196b9777be702b72
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-11-10 17:14:21 +0530

    [131] [Time Beats: 98.63%] [Memory Beats: 43.68%] - LeetPush

--- 131-Palindrome-Partitioning.java
+++ 131-Palindrome-Partitioning.java
@@ -1,30 +1,32 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> allPartitions = new ArrayList<>();
-        List<String> partition = new ArrayList<>();
-        dfs(s, allPartitions, partition, 0);
+        backtrack(allPartitions, new ArrayList<>(), s, 0);
         return allPartitions;
     }
 
-    private void dfs(String s, List<List<String>> allPartitions, List<String> partition, int start){
+    private void backtrack(List<List<String>> allPartitions, List<String> partition, String s, int start){
         if(start == s.length()){
             allPartitions.add(new ArrayList<>(partition));
             return;
         }
 
-        for(int end = start; end<s.length();end++){
-            if(isPalindrome(s, start, end)){
-                partition.add(s.substring(start, end+1));
-                dfs(s, allPartitions, partition, end+1);
+        for(int end=start; end < s.length(); end++){
+            String substring = s.substring(start, end+1);
+            if(isPalindrome(substring)){
+                partition.add(substring);
+                backtrack(allPartitions, partition, s, end+1);
                 partition.remove(partition.size()-1);
             }
         }
         return;
     }
 
-    private boolean isPalindrome(String s, int left, int right){
-        while(left <= right){
-            if(s.charAt(left)!=s.charAt(right)) return false;
+    private boolean isPalindrome(String s){
+        int left = 0;
+        int right= s.length()-1;
+        while(left <=right){
+            if(s.charAt(left) != s.charAt(right)) return false;
             left++;
             right--;
         }
