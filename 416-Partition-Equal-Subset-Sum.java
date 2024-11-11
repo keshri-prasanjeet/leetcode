6d1a0739afc79b242a903fb7a2662b9b3a251c15
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-11-12 00:57:50 +0530

    [416] [Time Beats: 92.24%] [Memory Beats: 87.69%] - LeetPush

--- 416-Partition-Equal-Subset-Sum.java
+++ 416-Partition-Equal-Subset-Sum.java
@@ -1,8 +1,12 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = Arrays.stream(nums).sum();
+        int sum = 0;
+        for(int num:nums){
+            sum+=num;
+        }
+
         if(sum%2!=0) return false;
-        int target = sum/2;
+        int target = sum /2;
         boolean[] dp = new boolean[target+1];
         dp[0] = true;
 
@@ -10,8 +14,9 @@ class Solution {
             for(int i=target;i>=num;i--){
                 dp[i] = dp[i] || dp[i-num];
             }
-            if(dp[target]) return true;
+            if(dp[target]==true) return true;
         }
+
         return dp[target];
     }
 }
