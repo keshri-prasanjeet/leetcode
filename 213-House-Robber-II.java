8f32a7bea2ca0320010dd6949a48ad32a4d08648
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-11-13 04:29:32 +0530

    [213] [Time Beats: 100.00%] [Memory Beats: 40.65%] - LeetPush

--- 213-House-Robber-II.java
+++ 213-House-Robber-II.java
@@ -1,23 +1,20 @@
 class Solution {
+    //version for better memory
     public int rob(int[] nums) {
         if(nums.length==1) return nums[0];
         if(nums.length==2) return Math.max(nums[0], nums[1]);
-
-        return Math.max(houseRobber(0, nums.length-2, nums), houseRobber(1, nums.length-1, nums));
+        return Math.max(robWithLimits(0, nums.length-2, nums), robWithLimits(1, nums.length-1, nums));
     }
 
-    private int houseRobber(int start, int end, int[] nums){
-
-        int twoBehind = 0;
-        int oneBehind = nums[start];
-        int current = oneBehind;
-
-        for(int i=start+1;i<=end;i++){
-            current = Math.max(oneBehind, nums[i]+twoBehind);
-            twoBehind = oneBehind;
-            oneBehind = current;
+    private int robWithLimits(int startIndex, int endIndex, int[] houseMoney){
+        int twoHouseBehind = 0;
+        int oneHouseBehind = houseMoney[startIndex];
+        int currentBestRobbery = oneHouseBehind;
+        for(int i=startIndex+1; i<=endIndex; i++){
+            currentBestRobbery = Math.max(oneHouseBehind, houseMoney[i] + twoHouseBehind);
+            twoHouseBehind = oneHouseBehind;
+            oneHouseBehind = currentBestRobbery;
         }
-
-        return current;
+        return currentBestRobbery;
     }
 }
