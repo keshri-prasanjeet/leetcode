035f51890a164a51bdc79b18dd951a8b1537c5a0
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-11-14 23:42:51 +0530

    [139] [Time Beats: 72.06%] [Memory Beats: 66.85%] - LeetPush

--- 139-Word-Break.java
+++ 139-Word-Break.java
@@ -2,18 +2,17 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length()+1];
         dp[s.length()] = true;
-        
-        for(int i=s.length(); i>=0; i--){
+        for(int i=s.length();i>=0;i--){
             for(String word: wordDict){
-                if(i+word.length() <= s.length()){
-                    if(word.equals(s.substring(i,i+word.length()))){
+                int len = word.length();
+                if(i+len <= s.length()){
+                    if(word.equals(s.substring(i, i+len))){
                         dp[i] = dp[i+word.length()];
                     }
                 }
                 if(dp[i]==true) break;
             }
         }
-
         return dp[0];
     }
 }
