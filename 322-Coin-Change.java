f3513a7727a11b0e96854e7d8c72406fe963de6e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2024-11-14 16:08:01 +0530

    [322] [Time Beats: 99.64%] [Memory Beats: 44.58%] - LeetPush

--- 322-Coin-Change.java
+++ 322-Coin-Change.java
@@ -1,9 +1,7 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
-
         Arrays.fill(dp, amount+1);
-
         dp[0] = 0;
 
         for(int coin: coins){
@@ -12,6 +10,6 @@ class Solution {
             }
         }
 
-        return dp[amount]==amount+1 ? -1: dp[amount];
+        return dp[amount] == amount+1 ? -1 : dp[amount];
     }
 }
