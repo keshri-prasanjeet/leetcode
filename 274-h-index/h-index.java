b4f703cf51514ecc51e1368d7f71440a9930a585
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-01-04 14:41:52 +0530

    Time: 2 ms (69.78%) | Memory: 41.7 MB (46.42%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,15 +1,22 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int[] sortedArray = Arrays.stream(citations)
-                            .boxed()
-                            .sorted((a,b) -> b-a)
-                            .mapToInt(Integer::intValue)
-                            .toArray();
+        // int[] sortedArray = Arrays.stream(citations)
+        //                     .boxed()
+        //                     .sorted((a,b) -> b-a)
+        //                     .mapToInt(Integer::intValue)
+        //                     .toArray();
+        // int hIndex = 0;
+        // for(int i=0;i<citations.length;i++){
+        //     if(sortedArray[i] >= i+1) hIndex = i+1;
+        //     else break;
+        // }
+
+        Arrays.sort(citations);
         int hIndex = 0;
-        for(int i=0;i<citations.length;i++){
-            if(sortedArray[i] >= i+1) hIndex = i+1;
-            else break;
+        int len = citations.length;
+        for(int i=0;i<len;i++){
+            if(len-i <= citations[i]) return len-i;
         }
-        return hIndex;
+        return 0;
     }
 }
