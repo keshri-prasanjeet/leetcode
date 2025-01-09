185d47b9a2878a1a97fa0184d3715cf99340bd7b
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-01-09 10:46:36 +0530

    Time: 7 ms (13.50%) | Memory: 46.1 MB (13.41%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int len = ratings.length;
+        int[] candy = new int[len];
+        Arrays.fill(candy, 1);
+        for(int i=1;i<len;i++){
+            if(ratings[i] > ratings[i-1] && candy[i] <= candy[i-1]){
+                candy[i] = candy[i-1]+1;
+            }
+        }
+        for(int i=len-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1] && candy[i] <= candy[i+1]){
+                candy[i] = candy[i+1]+1;
+            }
+        }
+        return Arrays.stream(candy).sum();
+    }
+}
