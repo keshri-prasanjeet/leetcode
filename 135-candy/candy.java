56115cfa70e35a8b4a2daeb8a4e65b1decf852a3
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-01-09 10:48:29 +0530

    Time: 5 ms (17.13%) | Memory: 45.5 MB (78.99%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -2,7 +2,7 @@ class Solution {
     public int candy(int[] ratings) {
         int len = ratings.length;
         int[] candy = new int[len];
-        Arrays.fill(candy, 1);
+        // Arrays.fill(candy, 1);
         for(int i=1;i<len;i++){
             if(ratings[i] > ratings[i-1] && candy[i] <= candy[i-1]){
                 candy[i] = candy[i-1]+1;
@@ -13,6 +13,6 @@ class Solution {
                 candy[i] = candy[i+1]+1;
             }
         }
-        return Arrays.stream(candy).sum();
+        return Arrays.stream(candy).sum() + len; // adding len because did not fill with 1 so adding 1 candy for each kid
     }
 }
