9115eb881fa19221679c4cc8adbb2b8ebbf4211c
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-01-16 08:54:11 +0530

    Time: 1 ms (89.68%) | Memory: 45.9 MB (42.45%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -3,13 +3,8 @@ class Solution {
         int buyPrice = prices[0];
         int profit = 0;
         for(int i=1;i<prices.length;i++){
-            if(prices[i]<=buyPrice){
-                buyPrice = prices[i];
-            }
-            else{
-                profit+=prices[i]-buyPrice;
-                buyPrice = prices[i];
-            }
+            if(buyPrice < prices[i]) profit+=prices[i]-buyPrice;
+            buyPrice = prices[i];
         }
         return profit;
     }
