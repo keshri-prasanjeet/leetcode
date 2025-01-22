039ea39e297e001248282e503f712b22e6f5366a
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-01-22 07:48:39 +0530

    Time: 17 ms (5.07%) | Memory: 41.3 MB (88.79%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,22 +1,16 @@
 class Solution {
     public int hIndex(int[] citations) {
-        // int[] sortedArray = Arrays.stream(citations)
-        //                     .boxed()
-        //                     .sorted((a,b) -> b-a)
-        //                     .mapToInt(Integer::intValue)
-        //                     .toArray();
-        // int hIndex = 0;
-        // for(int i=0;i<citations.length;i++){
-        //     if(sortedArray[i] >= i+1) hIndex = i+1;
-        //     else break;
-        // }
-
-        Arrays.sort(citations);
-        int hIndex = 0;
-        int len = citations.length;
-        for(int i=0;i<len;i++){
-            if(len-i <= citations[i]) return len-i;
+        int count;
+        int maxH = 0;
+        for(int i=1;i<=citations.length;i++){
+            count = 0;
+            for(int j=0;j<citations.length;j++){
+                if(citations[j] >= i) count++;
+            }
+            if(count >= i){
+                maxH = Math.max(maxH, i);
+            }
         }
-        return 0;
+        return maxH;
     }
 }
