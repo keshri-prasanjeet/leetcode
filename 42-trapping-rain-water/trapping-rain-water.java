c3ac82933b33201f500e1d0c50bd1058f69d527e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-01-29 06:27:07 +0530

    Time: 1 ms (64.64%) | Memory: 46.4 MB (57.72%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,44 +1,26 @@
 class Solution {
     public int trap(int[] height) {
-        int start = -1;
-        int end = -1;
-        int blocks = 0;
-        int water = 0;
         int len = height.length;
-        for(int i=0;i<len;i++){
-            if(height[i]>0 && start==-1){
-                start = i;
-            }
-            else if(start != -1 && height[i] > height[start]){
-                water += Math.min(height[start], height[i]) * (i-start-1);
-                water -= blocks;
-                start = i;
-                blocks = 0;
-            }
-            else{
-                blocks += height[i];
-            }
+        int[] leftMaxArray = new int[len];
+        int[] rightMaxArray = new int[len];
+
+        leftMaxArray[0] = height[0];
+        for(int i=1;i<len;i++){
+            leftMaxArray[i] = Math.max(leftMaxArray[i-1], height[i]);
         }
 
-        start = -1;
-        end = -1;
-        blocks = 0;
+        rightMaxArray[len-1] = height[len-1];
+        for(int i=len-2;i>=0;i--){
+            rightMaxArray[i] = Math.max(rightMaxArray[i+1], height[i]);
+        }
 
-        for(int i = len-1;i>=0;i--){
-            if(height[i]>0 && start==-1){
-                start = i;
-            }
-            else if(start != -1 && height[i] >= height[start]){
-                water += Math.min(height[start], height[i]) * (start-i-1);
-                water -= blocks;
-                start = i;
-                blocks = 0;
-            }
-            else{
-                blocks += height[i];
-            }
+        int water = 0;
+
+        for(int i=1;i<len-1;i++){
+            water+= (Math.min(leftMaxArray[i], rightMaxArray[i])) - height[i];
         }
 
         return water;
+        
     }
 }
