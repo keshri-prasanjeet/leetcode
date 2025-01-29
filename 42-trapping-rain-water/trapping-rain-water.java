ccd7056e7208144c195b8356c7c1e30a67cd6f7e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-01-29 05:53:32 +0530

    Time: 340 ms (5.33%) | Memory: 48.1 MB (8.29%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int trap(int[] height) {
+        int start = -1;
+        int end = -1;
+        int blocks = 0;
+        int water = 0;
+        int len = height.length;
+        for(int i=0;i<len;i++){
+            if(height[i]>0 && start==-1){
+                start = i;
+            }
+            else if(start != -1 && height[i] > height[start]){
+                System.out.println("Start is "+ start);
+                System.out.println("End is " + i);
+                water += Math.min(height[start], height[i]) * (i-start-1);
+                water -= blocks;
+                start = i;
+                blocks = 0;
+                System.out.println("water is "+ water);
+            }
+            else{
+                blocks += height[i];
+            }
+        }
+
+        start = -1;
+        end = -1;
+        blocks = 0;
+
+        for(int i = len-1;i>=0;i--){
+            if(height[i]>0 && start==-1){
+                start = i;
+            }
+            else if(start != -1 && height[i] >= height[start]){
+                System.out.println("Start is "+ start);
+                System.out.println("End is " + i);
+                water += Math.min(height[start], height[i]) * (start-i-1);
+                water -= blocks;
+                start = i;
+                blocks = 0;
+                System.out.println("water is "+ water);
+            }
+            else{
+                blocks += height[i];
+            }
+        }
+
+        return water;
+    }
+}
