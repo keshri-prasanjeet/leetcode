2d9b9e26337e6d09170bb63055feb18c4879762a
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-01-30 07:21:53 +0530

    Time: 3 ms (98.66%) | Memory: 44.7 MB (32.49%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -6,93 +6,66 @@ class Solution {
             if(num / 1000 > 0){
                 quot = num / 1000;
                 num  = num % 1000;
-                while(quot > 0){
-                    sb.append('M');
-                    quot--;
-                }
+                loopingRoman(quot, sb, "M");
             }
-            // System.out.println("1000 Num is " + num);
             if(num / 900 > 0){
-                // System.out.println("inside 900");
-                quot = num / 900;
                 num = num % 900;
-                sb.append("CM");
+                loopingRoman(1, sb, "CM");
             }
-            // System.out.println("900 Num is " + num);
             if(num / 500 > 0){
-                quot = num / 500;
                 num = num % 500;
-                sb.append("D");
+                loopingRoman(1, sb, "D");
             }
-            // System.out.println("500 Num is " + num);
             if(num / 400 > 0){
-                quot = num / 400;
                 num = num % 400;
-                // ans += "CD";
-                sb.append("CD");
+                loopingRoman(1, sb, "CD");
             }
-            // System.out.println("400 Num is " + num);
             if(num / 100 > 0){
                 quot = num / 100;
                 num  = num % 100;
-                while(quot > 0){
-                    sb.append('C');
-                    quot--;
-                }
+                loopingRoman(quot, sb, "C");
             }
-            // System.out.println("Num is " + num);
             if(num / 90 > 0){
-                quot = num / 90;
                 num = num % 90;
-                sb.append("XC");
+                loopingRoman(1, sb, "XC");
             }
-            // System.out.println("Num is " + num);
             if(num / 50 > 0){
-                quot = num / 50;
                 num = num % 50;
-                sb.append("L");
+                loopingRoman(1, sb, "L");
             }
-            // System.out.println("Num is " + num);
             if(num / 40 > 0){
-                quot = num / 40;
                 num = num % 40;
-                sb.append("XL");
+                loopingRoman(1, sb, "XL");
             }
-            // System.out.println("Num is " + num);
             if(num / 10 > 0){
                 quot = num / 10;
                 num = num % 10;
-                while(quot > 0){
-                    sb.append("X");
-                    quot--;
-                }
+                loopingRoman(quot, sb, "X");
             }
-            // System.out.println("Num is " + num);
             if(num / 9 > 0){
                 num = num % 9;
                 sb.append("IX");
             }
-            // System.out.println("Num is " + num);
             if(num / 5 > 0){
                 num = num % 5;
                 sb.append("V");
             }
-            // System.out.println("Num is " + num);
             if(num / 4 > 0){
                 num = num % 4;
                 sb.append("IV");
             }
-            // System.out.println("Num is " + num);
             if(num / 1 > 0){
                 quot = num / 1;
                 num = num % 1;
-                while(quot > 0){
-                    sb.append("I");
-                    quot--;
-                }
+                loopingRoman(quot, sb, "I");
             }
-            // System.out.println("Num is " + num);
         }
         return sb.toString();
+    }
+    private void loopingRoman(int quot, StringBuilder sb, String romanNum){
+        while(quot > 0){
+            sb.append(romanNum);
+            quot--;
+        }
     }
 }
