19161f8a00a6c9a62011265d614745e8fcab18f9
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-02-04 23:05:44 +0530

    Time: 2 ms (82.52%) | Memory: 44.8 MB (69.70%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,21 +1,13 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int rLen = ransomNote.length();
-        int mLen = magazine.length();
-        if(mLen < rLen) return false;
-        char[] ransomArray = ransomNote.toCharArray();
-        char[] magazineArray = magazine.toCharArray();
-        Arrays.sort(ransomArray);
-        Arrays.sort(magazineArray);
-        int j=0;
-        int count = 0;
-        for(int i=0;i<mLen;i++){
-            if(ransomArray[j]==magazineArray[i]){
-                count++;
-                if(count == rLen) return true;
-                j++;
-            }
+        int[] dictionary = new int[26];
+        for(char m: magazine.toCharArray()){
+            dictionary[m-'a']++;
         }
-        return false;
+        for(char r: ransomNote.toCharArray()){
+            dictionary[r-'a']--;
+            if(dictionary[r-'a'] < 0) return false;
+        }
+        return true;
     }
 }
