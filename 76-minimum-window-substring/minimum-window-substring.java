f91079e659526b42f5a94031b326aac44741eb1b
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-02-13 08:27:53 +0530

    Time: 21 ms (38.30%) | Memory: 45.2 MB (49.38%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(s.length() < t.length()) return "";
+        Map<Character, Integer> tMap = new HashMap<>();
+        for(char c:t.toCharArray()){
+            tMap.put(c, tMap.getOrDefault(c,0)+1);
+        }
+        int minLen = Integer.MAX_VALUE;
+        Map<Character, Integer> windowMap = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int required = tMap.size();
+        int windowStart = left;
+        int found = 0;
+        while(right<s.length()){
+            char rightChar = s.charAt(right);
+            windowMap.put(rightChar, windowMap.getOrDefault(rightChar,0)+1);
+            if(tMap.containsKey(rightChar) && tMap.get(rightChar).equals(windowMap.get(rightChar))){
+                found++;
+            }
+
+            while(found==required){
+                if(minLen > right-left+1){
+                    minLen = right-left+1;
+                    windowStart = left;
+                }
+                char leftChar = s.charAt(left);
+                windowMap.put(leftChar, windowMap.get(leftChar)-1);
+                if(tMap.containsKey(leftChar) && tMap.get(leftChar) > windowMap.get(leftChar)){
+                    found--;
+                }
+                left++;
+            }
+            right++;
+        }
+        return minLen==Integer.MAX_VALUE ? "" : s.substring(windowStart, windowStart+minLen);
+    }
+}
