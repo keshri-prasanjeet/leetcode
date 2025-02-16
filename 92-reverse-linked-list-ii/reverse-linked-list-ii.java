d3f593f425bf896edf19279ff436b0b8020b9567
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-02-17 05:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (51.56%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -10,28 +10,22 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        // left and right are positions
         Stack<ListNode> rightStack = new Stack<>();
         ListNode leftPointer = head;
         ListNode rightPointer = head;
         int orgLeft = left;
         left--;
-        // right--;
         while(left!=0){
             leftPointer = leftPointer.next;
             left--;
         }
         while(right>0){
             rightStack.push(rightPointer);
-            // System.out.println("right pointer has " + rightPointer.val);
-            // System.out.println("right is " + right);
             rightPointer = rightPointer.next;
             right--;
         }
-        // System.out.println(leftPointer.val);
         int len = rightStack.size();
         len = len - orgLeft+1;
-        System.out.println(len);
         len/=2;
         while(len > 0){
             len--;
