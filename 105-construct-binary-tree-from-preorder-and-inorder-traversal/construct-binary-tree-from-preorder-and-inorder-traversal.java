179e59cf38a4071a6c96f612a7ba7fe58f5a81f1
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-02-20 10:16:20 +0530

    Time: 2 ms (65.58%) | Memory: 44.8 MB (19.93%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,26 +14,26 @@
  * }
  */
 class Solution {
-    int[] preorder;
-    Map<Integer, Integer> InMap;
+    int[] pre;
     int preIndex;
+    Map<Integer, Integer> inMap;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         if(preorder.length == 0 || inorder.length == 0) return null;
-        this.preorder = preorder;
+        this.pre = preorder;
         preIndex = 0;
-        InMap = new HashMap<>();
         int index = 0;
+        inMap = new HashMap<>();
         for(int i:inorder){
-            InMap.put(i, index++);
+            inMap.put(i, index++);
         }
-        return buildRealTree(0, inorder.length-1);
+        return treeBuilder(0, inorder.length-1);
     }
-    private TreeNode buildRealTree(int inStart, int inEnd){
-        if(inEnd < inStart) return null;
-        TreeNode node = new TreeNode(preorder[preIndex++]);
-        int rootIndex = InMap.get(node.val);
-        node.left = buildRealTree(inStart, rootIndex-1);
-        node.right = buildRealTree(rootIndex+1, inEnd);
-        return node;
+    private TreeNode treeBuilder(int inStart, int inEnd){
+        if(inStart > inEnd) return null;
+        TreeNode newNode = new TreeNode(pre[preIndex++]);
+        int nodeIndex = inMap.get(newNode.val);
+        newNode.left = treeBuilder(inStart, nodeIndex-1);
+        newNode.right = treeBuilder(nodeIndex+1, inEnd);
+        return newNode;
     }
 }
