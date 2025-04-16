feb3e03b7d91f2e08fff894b8c76a324f48b1f6e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-04-16 06:57:26 +0530

    Time: 2 ms (94.84%) | Memory: 134.2 MB (29.87%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -6,15 +6,11 @@ class Solution {
         for(int i=0;i<len;i++){
             if(nums[i] <= first){
                 first = nums[i];
-                // second = Integer.MAX_VALUE;
-                // System.out.println(first);
             }
             else if(nums[i] > first && nums[i] <= second){
                 second = nums[i];
-                // System.out.println(first+ " " + second);
             }
             else {
-                // System.out.println(first+ " " + second + " " + nums[i]);
                 return true;
             }
         }
