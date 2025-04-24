91102c1f7acdbc21d5f973c8853ac78bf831ee67
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-04-24 21:45:13 +0530

    Time: 77 ms (7.57%) | Memory: 46 MB (12.01%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,28 +1,37 @@
 class Solution {
     public String reverseVowels(String s) {
-        boolean[] vowels = new boolean[128];//all the ASCII chars
-        for(char a:"aeiouAEIOU".toCharArray()){
-            vowels[a] = true;
-        }
+        Set<Character> set = Set.of('a','e','i','o','u','A','E','I','O','U');
+        char[] sArr = s.toCharArray();
         int len = s.length();
-        int i = 0;
-        int j = len-1;
-        char[] word = s.toCharArray();
-        while(i<j){
-            while(i<j && !vowels[word[i]]){
-                i++;
+        int left = 0;
+        int right = len-1;
+        boolean lVow = false;
+        boolean rVow = false;
+        while(left <= right && left < len && right > -1){
+            if(set.contains(sArr[left])){
+                lVow = true;
             }
-            while(i<j && !vowels[word[j]]){
-                j--;
+            else{
+                left++;
             }
-            if(i<j){
-                char temp = word[i];
-                word[i] = word[j];
-                word[j] = temp;
-                i++;
-                j--;
+            if(set.contains(sArr[right])){
+                rVow = true;
+            }
+            else{
+                right--;
+            }
+            if(lVow == true && rVow == true){
+                System.out.println(left + " " + right);
+                char temp = sArr[left];
+                sArr[left] = sArr[right];
+                sArr[right] = temp;
+                System.out.println(sArr[left] + " " + sArr[right]);
+                left++;
+                right--;
+                lVow = false;
+                rVow = false;
             }
         }
-        return String.valueOf(word);
+        return new String(sArr);
     }
 }
