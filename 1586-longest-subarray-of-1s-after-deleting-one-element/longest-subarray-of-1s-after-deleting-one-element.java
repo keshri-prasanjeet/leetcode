8f99cda4605d25909517eb6395957a1beccb6c3d
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-05-17 11:39:52 +0530

    Time: 2 ms (98.02%) | Memory: 57.4 MB (11.42%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,31 +1,23 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        //see a zero, "delete" it
-        //continue until you see zero again
-        //shrink the window until that deleted zero
-        //delete this new zero and expand the window as much as you can
-        //rinse repeat
-
         boolean deleted = false;
-        int len = nums.length;
         int start = 0;
-        int longestSubArr = 0;
+        int subLen = 0;
+        int len = nums.length;
         for(int i=0;i<len;i++){
             if(nums[i]==0){
-                if(deleted != true){
+                if(deleted == false){
                     deleted = true;
                 }
                 else{
-                    while(deleted == true){
-                        if(nums[start++] == 0) deleted = false;
+                    while(nums[start]==1){
+                        start++;
                     }
-                    deleted = true;
+                    start++;
                 }
             }
-
-            longestSubArr = Math.max(longestSubArr, i-start);
-
+            subLen = Math.max(subLen, i-start);
         }
-        return longestSubArr;
+        return subLen;
     }
 }
