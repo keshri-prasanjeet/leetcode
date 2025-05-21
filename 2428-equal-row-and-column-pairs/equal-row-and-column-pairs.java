b84165fb1ed67e077870e30ea0283e14e3c0f1de
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-05-21 20:53:45 +0530

    Time: 32 ms (59.93%) | Memory: 49.7 MB (69.72%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -8,7 +8,7 @@ class Solution {
         for(int i=0;i<m;i++){
             sb.setLength(0);
             for(int num: grid[i]){
-                sb.append(num).append("÷");
+                sb.append(num).append(",");
             }
             String rowNum = sb.toString();
             rowMap.put(rowNum, rowMap.getOrDefault(rowNum,0)+1);
@@ -17,7 +17,7 @@ class Solution {
         for(int i=0;i<n;i++){
             sb.setLength(0);
             for(int j=0;j<m;j++){
-                sb.append(grid[j][i]).append("÷");
+                sb.append(grid[j][i]).append(",");
             }
             String colNum = sb.toString();
             equalPairCount += rowMap.getOrDefault(colNum, 0);
