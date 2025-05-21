a65f8f1f4918bb249f66abb95f48d44bd7c8f862
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-05-21 21:01:16 +0530

    Time: 17 ms (93.40%) | Memory: 50 MB (66.30%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,24 +1,22 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        Map<String,Integer> rowMap = new HashMap<>();
-        int m = grid.length;
-        int n = grid[0].length;
-        int equalPairCount = 0;
-        for(int i=0;i<m;i++){
-            String rowNum = Arrays.toString(grid[i]);
-            rowMap.put(rowNum, rowMap.getOrDefault(rowNum,0)+1);
-        }
-        int[] temp = new int[m];
-        int index = 0;
+        int n = grid.length;
+        Map<List<Integer>, Integer> map = new HashMap<>();
         for(int i=0;i<n;i++){
-            index = 0;
-            for(int j=0;j<m;j++){
-                temp[index++] = grid[j][i];
+            List<Integer> row = new ArrayList<>();
+            for(int j=0;j<n;j++){
+                row.add(grid[i][j]);
             }
-            String colNum = Arrays.toString(temp);
-            equalPairCount += rowMap.getOrDefault(colNum, 0);
+            map.put(row,map.getOrDefault(row,0)+1);
         }
-
-        return equalPairCount;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            List<Integer> col = new ArrayList<>();
+            for(int j=0;j<n;j++){
+                col.add(grid[j][i]);
+            }
+            count += map.getOrDefault(col,0);
+        }
+        return count;
     }
 }
