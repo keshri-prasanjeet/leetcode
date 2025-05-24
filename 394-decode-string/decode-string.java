4cf73e0bd310a03949eef71e8bc23a16a048d94c
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-05-24 08:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (68.25%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -5,26 +5,26 @@ class Solution {
         StringBuilder currentString = new StringBuilder();
         int k = 0;
 
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                k = k * 10 + (ch - '0'); // handle multi-digit numbers
-            } else if (ch == '[') {
-                countStack.push(k); // push repeat count
-                stringStack.push(currentString); // push current string so far
-                currentString = new StringBuilder(); // start fresh
-                k = 0;
-            } else if (ch == ']') {
-                int count = countStack.pop();
-                StringBuilder decoded = new StringBuilder(stringStack.pop());
-                for (int i = 0; i < count; i++) {
-                    decoded.append(currentString);
-                }
-                currentString = decoded; // updated result so far
-            } else {
-                currentString.append(ch); // building string
+        for(char ch: s.toCharArray()){
+            if(Character.isDigit(ch)){
+                k = k * 10 + (ch - '0');
             }
+            else if(ch == '['){
+                countStack.push(k);
+                stringStack.push(currentString);
+                k = 0;
+                currentString = new StringBuilder();
+            }
+            else if(ch == ']'){
+                int count = countStack.pop();
+                StringBuilder outsideString = stringStack.pop();
+                while(count-- > 0){
+                    outsideString.append(currentString);
+                }
+                currentString = outsideString;
+            }
+            else currentString.append(ch);
         }
-
         return currentString.toString();
     }
 }
