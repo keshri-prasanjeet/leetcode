d7a68422b8379504b577b4e0ff73eeff491d4127
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-05-26 10:00:54 +0530

    Time: 10 ms (88.74%) | Memory: 45.7 MB (48.49%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> radiant = new LinkedList<>();
+        Queue<Integer> dire = new LinkedList<>();
+        int n = senate.length();
+
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') {
+                radiant.add(i);
+            } else {
+                dire.add(i);
+            }
+        }
+
+        while (!radiant.isEmpty() && !dire.isEmpty()) {
+            int rIndex = radiant.poll();
+            int dIndex = dire.poll();
+
+            if (rIndex < dIndex) {
+                radiant.add(rIndex + n); // re-add R for next round
+            } else {
+                dire.add(dIndex + n); // re-add D for next round
+            }
+        }
+
+        return radiant.isEmpty() ? "Dire" : "Radiant";
+    }
+}
