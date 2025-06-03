73820c7702a06a3a34d956d9dbb7d7b7ad0bbc94
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-03 09:32:57 +0530

    Time: 3 ms (97.20%) | Memory: 44.4 MB (36.12%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -14,11 +14,11 @@
  * }
  */
 class Solution {
+    int count = 0;
     Map<Long, Integer> prefixSumMap;
-    int count;
     public int pathSum(TreeNode root, int targetSum) {
+        if(root == null) return 0;
         prefixSumMap = new HashMap<>();
-        count = 0;
         prefixSumMap.put(0L,1);
         helper(root, targetSum, 0L);
         return count;
@@ -27,12 +27,12 @@ class Solution {
     private void helper(TreeNode node, int target, long prefixSum){
         if(node == null) return;
         prefixSum+=node.val;
-        count += prefixSumMap.getOrDefault(prefixSum - target, 0);//counting if my targetsum is part of current prefix sum
-        //if prefix sum map which is freq map of all prefix sums contains the number that even after - target exists in map 
-        // then it means that <some prefix> + <k> in this path
-        prefixSumMap.put(prefixSum, prefixSumMap.getOrDefault(prefixSum,0)+1);//update map with current prefix sum
+        count += prefixSumMap.getOrDefault(prefixSum - target, 0);
+        prefixSumMap.put(prefixSum, prefixSumMap.getOrDefault(prefixSum,0)+1);
+
         helper(node.left, target, prefixSum);
         helper(node.right, target,prefixSum);
+
         prefixSumMap.put(prefixSum, prefixSumMap.get(prefixSum)-1);
     }
 }
