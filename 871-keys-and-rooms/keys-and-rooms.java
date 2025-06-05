e96d58841b8a7859e30a95cd605c4d23a951fdc0
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-06 05:28:16 +0530

    Time: 1 ms (72.87%) | Memory: 44.6 MB (36.22%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,25 +1,21 @@
 class Solution {
+    boolean[] visited;
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        Set<Integer> visited = new HashSet<>();
-        Queue<Integer> roomQ = new LinkedList<>();
-        List<Integer> zeroThRoom = rooms.get(0);
-        for(int i=0;i<zeroThRoom.size();i++){
-            roomQ.offer(zeroThRoom.get(i));
+        visited = new boolean[rooms.size()];
+        visited[0] = true;
+        visit(rooms, rooms.get(0));
+        for(Boolean b:visited){
+            if(b==false) return false;
         }
-        visited.add(0);
-        while(!roomQ.isEmpty()){
-            int top = roomQ.poll();
-            if(!visited.contains(top)){
-                List<Integer> toBeVisited = rooms.get(top);
-                visited.add(top);
-                for(int i=0;i<toBeVisited.size();i++){
-                    if(!visited.contains(toBeVisited.get(i))){
-                        roomQ.offer(toBeVisited.get(i));
-                    }
-                }
-            }
-        }
-        if(visited.size() == rooms.size()) return true;
-        return false;
+        return true;
     }
+
+    private void visit(List<List<Integer>> rooms, List<Integer> currentRoom){
+        for(int i:currentRoom){
+            if(visited[i]) continue;
+            visited[i] = true;
+            visit(rooms, rooms.get(i));
+        }
+    }
+
 }
