14d157badf4e620c660b2a0428207bfaa91cefc2
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-06 07:35:32 +0530

    Time: 1 ms (88.64%) | Memory: 48.4 MB (14.82%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,29 +1,32 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int length = isConnected.length;
-        boolean visited[] = new boolean[length];
+        int len = isConnected.length;
+        boolean[] visited = new boolean[len];
+        int totalProvinces = 0;
 
-        int provinces = 0;
-        for(int i=0;i<length;i++){
-            if(visited[i]) continue;
-            dfs(isConnected, visited, i);
-            provinces++;
+        for(int i=0;i<len;i++){
+            if(!visited[i]) {
+                dfs(isConnected, visited, i);
+                totalProvinces++;
+            }
+            
         }
-        return provinces;
+        return totalProvinces;
     }
 
-    private void dfs(int[][] matrix, boolean[] visited, int start){
+    private void dfs(int[][] adj, boolean[] visited, int start){
         Queue<Integer> cityQ = new LinkedList<>();
         cityQ.offer(start);
         visited[start] = true;
         while(!cityQ.isEmpty()){
-            int top = cityQ.poll();
-            for(int j=0;j<visited.length;j++){
-                if(matrix[top][j] == 1 && !visited[j]){
-                    cityQ.offer(j);
-                    visited[j] = true;
+            int city = cityQ.poll();
+            for(int i=0;i<adj.length;i++){
+                if(adj[city][i]==1 && !visited[i]){
+                    cityQ.offer(i);
+                    visited[i] = true;
                 }
             }
         }
+        
     }
 }
