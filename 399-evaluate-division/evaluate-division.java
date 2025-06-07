3c90084f29b305af6d39952eb727c355b556dff9
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-07 09:53:33 +0530

    Time: 2 ms (32.01%) | Memory: 42.2 MB (84.64%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,46 +1,48 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        //the centrepoint of this problem, the adjancency list
         Map<String, List<List<Object>>> adjList = new HashMap<>();
-        for (int i = 0; i < equations.size(); i++) {
+
+        for(int i=0;i<equations.size();i++){
             String numerator = equations.get(i).get(0);
-            String denominator = equations.get(i).get(1);
-            double val = values[i];
+            String denominator=equations.get(i).get(1);
+            double value = values[i];
 
             adjList.putIfAbsent(numerator, new ArrayList<>());
             adjList.putIfAbsent(denominator, new ArrayList<>());
 
-            adjList.get(numerator).add(List.of(denominator, val));
-            adjList.get(denominator).add(List.of(numerator, 1.0 / val));
+            adjList.get(numerator).add(List.of(denominator, value));
+            adjList.get(denominator).add(List.of(numerator, 1.0/value));
         }
 
-        //do dfs to find the values for queries
         double[] answer = new double[queries.size()];
-        for (int i = 0; i < queries.size(); i++) {
+
+        for(int i=0;i<queries.size();i++){
             answer[i] = bfs(queries.get(i).get(0), queries.get(i).get(1), adjList);
         }
+
         return answer;
     }
 
-    private double bfs(String src, String target, Map<String, List<List<Object>>> graph) {
-        if (!graph.containsKey(src) || !graph.containsKey(target))
-            return -1.0;
-        Set<String> visited = new HashSet<>();
+    private double bfs(String src, String target, Map<String, List<List<Object>>> adjList){
+        if(!adjList.containsKey(src) || !adjList.containsKey(target)) return -1.0;
         Queue<Object[]> q = new ArrayDeque<>();
-        q.offer(new Object[] { src, 1.0 });
+        Set<String> visited = new HashSet<>();
+        q.offer(new Object[]{src, 1.0});
         visited.add(src);
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             Object[] top = q.poll();
-            String ex = (String) top[0];
-            double val = (double) top[1];
+            String exKey = (String) top[0];
+            double exVal = (double) top[1];
 
-            if (ex.equals(target))
-                return val;
+            if(exKey.equals(target)) return exVal;
+            //this key is not the target, explore the neighbors
 
-            for (List<Object> neighbours : graph.get(ex)) {
-                String neighbourKey = (String) neighbours.get(0);
-                if (!visited.contains(neighbourKey)) {
-                    double neighbourVal = (double) neighbours.get(1);
-                    q.offer(new Object[] { neighbourKey, neighbourVal * val });
+            for(List<Object> neighbour: adjList.get(exKey)){
+                String neighbourKey = (String) neighbour.get(0);
+                if(!visited.contains(neighbourKey)){
+                    double neighbourVal = (double) neighbour.get(1);
+                    q.offer(new Object[]{neighbourKey, neighbourVal * exVal});
                     visited.add(neighbourKey);
                 }
             }
