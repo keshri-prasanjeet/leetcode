583131bddeb268bd53d76de0a4cca00010e605bd
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-07 14:05:29 +0530

    Time: 7 ms (71.58%) | Memory: 45.5 MB (89.45%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -2,47 +2,43 @@ class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
         int m = maze.length;
         int n = maze[0].length;
-        return bfs(maze, entrance, m, n);
-    }
+        Queue<int[]> bfsQ = new ArrayDeque<>();
+        bfsQ.offer(new int[]{entrance[0],entrance[1],0});
 
-    private int bfs(char[][] maze, int[] entrance, int m, int n){
-        Queue<int[]> dfsQ = new ArrayDeque<>();
-        int[] initial = new int[]{entrance[0], entrance[1], 0};
-        dfsQ.offer(initial);
         int[][] directions = {
             {-1, 0}, // up
             {1, 0},  // down
             {0, -1}, // left
             {0, 1}   // right
         };
-        while(!dfsQ.isEmpty()){
-            int[] top = dfsQ.poll();
+
+        while(!bfsQ.isEmpty()){
+            int[] top = bfsQ.poll();
             int x = top[0];
             int y = top[1];
-            int steps = top[2];
-            maze[x][y] = '+';
-            if(x-1 < 0 || y-1 < 0 || x+1 == m || y+1 == n){
+            int steps=top[2];
+
+            maze[x][y] = '+';//deque change
+            if(x-1<0 || y-1 <0 || x+1==m || y+1==n){
                 if(x==entrance[0] && y==entrance[1]){
-                    //do nothing
+                //do nothing
                 }
                 else{
-                    return steps;
+                    return steps;//first place where it is edge is the closesnt point
                 }
             }
 
-            for(int[] direction : directions){
-                int newRow = x+direction[0];
-                int newCol = y+direction[1];
-
-                if(newRow >= 0 && newRow < m && newCol >=0 && newCol < n){
-                    if(maze[newRow][newCol] == '.'){
-                        maze[newRow][newCol] = '+';
-                        int[] validCell = new int[]{newRow, newCol, steps+1};
-                        dfsQ.offer(validCell); 
+            for(int[] direction: directions){
+                int row = x+direction[0];
+                int col = y+direction[1];
+                if(row >=0 && col >=0 && row < m && col < n){
+                    //safe cell
+                    if(maze[row][col]=='.'){
+                        maze[row][col] = '+';//enque change
+                        bfsQ.offer(new int[]{row, col, steps+1});//steps tracking
                     }
                 }
             }
-            
         }
         return -1;
     }
