e48dcb42fd53d75be75993fa5ff631261f25b6fb
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-07 08:15:08 +0530

    Time: 32 ms (95.71%) | Memory: 67.1 MB (90.98%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,34 +1,42 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
-        List<List<int[]>> graph = new ArrayList<>();
+        //make an adjacency list out of the 2d array connections List<List<int[]>>
+        //in this adj list, index is the city and List<int[]> shows all the cities this index city is connected to
+        //first element show the connected city and the second element shows the direction of it
+        //start doing dfs and if you start from 0th index and recursively try to go all the cities.
+
+        List<List<int[]>> adjList = new ArrayList<>();
+
         boolean[] visited = new boolean[n];
         while(n-- > 0){
-            graph.add(new ArrayList<>());
-        }
-        for(int [] connection: connections){
-            int from = connection[0];
-            int to = connection[1];
-            graph.get(from).add(new int[]{to, 1});
-            graph.get(to).add(new int[]{from, 0});
+            adjList.add(new ArrayList<>());
         }
 
-        //do dfs now
-        
-        return dfs(0, visited, graph);
+        for(int[] connection: connections){
+            int from  = connection[0];
+            int to = connection[1];
+
+            adjList.get(from).add(new int[]{to, 1});
+            adjList.get(to).add(new int[]{from, 0});
+        }
+
+        return dfs(0, visited, adjList);
 
     }
 
-    private int dfs(int node, boolean[] visited, List<List<int[]>> graph){
+    private int dfs(int city, boolean[] visited, List<List<int[]>> adjList){
         int flips = 0;
-        visited[node] = true;
+        visited[city] = true;
 
-        for(int[] neighbourDirection: graph.get(node)){
-            int neighbour = neighbourDirection[0];
+        for(int[] neighbourAndDirection: adjList.get(city)){
+            int neighbour = neighbourAndDirection[0];
+            int direction = neighbourAndDirection[1];
             if(!visited[neighbour]){
-                flips+=neighbourDirection[1];
-                flips+=dfs(neighbour, visited, graph);
+                flips += direction;
+                flips += dfs(neighbour, visited, adjList);
             }
         }
         return flips;
     }
+
 }
