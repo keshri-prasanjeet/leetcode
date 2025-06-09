244f9b1d472335481bddf6d776cca5b9c64b0787
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-09 09:15:23 +0530

    Time: 1 ms (98.96%) | Memory: 42.4 MB (54.71%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,46 +1,47 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         Map<String, List<Object[]>> adjMatrix = new HashMap<>();
-        int len = equations.size();
-        for(int i=0;i<len;i++){
-            String numerator = equations.get(i).get(0);
-            String denominator = equations.get(i).get(1);
+        int index = 0;
+        
+        for(List<String> equation:equations){
+            String numerator = equation.get(0);
+            String denominator = equation.get(1);
+
             adjMatrix.putIfAbsent(numerator, new ArrayList<>());
             adjMatrix.putIfAbsent(denominator, new ArrayList<>());
 
-            adjMatrix.get(numerator).add(new Object[]{denominator, values[i]});
-            adjMatrix.get(denominator).add(new Object[]{numerator, 1.0/values[i]});
+            adjMatrix.get(numerator).add(new Object[]{denominator, values[index]});
+            adjMatrix.get(denominator).add(new Object[]{numerator, 1.0/values[index]});
+            index++;
         }
-        int Qlen = queries.size();
-        double[] answer = new double[Qlen];
-        for(int i=0;i<Qlen;i++){
-            String start = queries.get(i).get(0);
-            String end = queries.get(i).get(1);
-            answer[i] = solve(adjMatrix, start, end);
+        int len = queries.size();
+        double[] answer = new double[len];
+        for(int i=0;i<len;i++){
+            answer[i] = bfs(adjMatrix, queries.get(i).get(0), queries.get(i).get(1));
         }
         return answer;
     }
 
-    private double solve(Map<String, List<Object[]>> adjMatrix, String start, String end){
-        if(!adjMatrix.containsKey(start) || !adjMatrix.containsKey(end)){
-            return -1.0;
-        }
-        Queue<Object[]> equationQ = new ArrayDeque<>();
-        Set<String> visited = new HashSet<>();
-        equationQ.offer(new Object[]{start, 1.0});
-        visited.add(start);
-        while(!equationQ.isEmpty()){
-            Object[] top = equationQ.poll();
-            String key = (String) top[0];
-            double val = (double) top[1];
+    private double bfs(Map<String, List<Object[]>> adjMatrix, String src, String tar){
+        if(!adjMatrix.containsKey(src) || !adjMatrix.containsKey(tar)) return -1.0;
 
-            if(key.equals(end)) return val;
-            for(Object[] neighbourAndVal: adjMatrix.get(key)){
-                String neighbour = (String) neighbourAndVal[0];
-                if(!visited.contains(neighbour)){
-                    double neighbourVal = (double) neighbourAndVal[1];
-                    equationQ.offer(new Object[]{neighbour, neighbourVal*val});
-                    visited.add(neighbour);
+        Queue<Object[]> bfsQ = new ArrayDeque<>();
+        Set<String> visited = new HashSet<>();
+
+        bfsQ.offer(new Object[]{src, 1.0});
+        visited.add(src);
+        while(!bfsQ.isEmpty()){
+            Object[] top = bfsQ.poll();
+            String operand = (String) top[0];
+            double value = (double) top[1];
+
+            if(operand.equals(tar)) return value;
+            visited.add(operand);
+            for(Object[] neighboursAndValues: adjMatrix.get(operand)){
+                String neighbourKey = (String) neighboursAndValues[0];
+                if(!visited.contains(neighbourKey)){
+                    double neighbourVal = (double) neighboursAndValues[1];
+                    bfsQ.offer(new Object[]{neighbourKey, value*neighbourVal});
                 }
             }
         }
