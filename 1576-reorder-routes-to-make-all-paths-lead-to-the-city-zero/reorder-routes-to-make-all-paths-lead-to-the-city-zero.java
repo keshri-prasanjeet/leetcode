26d0d6d052384e4b493f0ae2bc70bed4538a334e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-09 07:13:56 +0530

    Time: 37 ms (81.40%) | Memory: 67.6 MB (87.38%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,42 +1,30 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
-        //make an adjacency list out of the 2d array connections List<List<int[]>>
-        //in this adj list, index is the city and List<int[]> shows all the cities this index city is connected to
-        //first element show the connected city and the second element shows the direction of it
-        //start doing dfs and if you start from 0th index and recursively try to go all the cities.
-
         List<List<int[]>> adjList = new ArrayList<>();
-
         boolean[] visited = new boolean[n];
-        while(n-- > 0){
+        for(int i =0;i<n;i++){
             adjList.add(new ArrayList<>());
         }
-
-        for(int[] connection: connections){
-            int from  = connection[0];
+        for(int [] connection:connections){
+            int from = connection[0];
             int to = connection[1];
-
             adjList.get(from).add(new int[]{to, 1});
             adjList.get(to).add(new int[]{from, 0});
         }
-
-        return dfs(0, visited, adjList);
-
+        return dfs(adjList, visited, 0);
     }
 
-    private int dfs(int city, boolean[] visited, List<List<int[]>> adjList){
+    private int dfs(List<List<int[]>> adjList, boolean[] visited, int city){
         int flips = 0;
         visited[city] = true;
-
         for(int[] neighbourAndDirection: adjList.get(city)){
             int neighbour = neighbourAndDirection[0];
             int direction = neighbourAndDirection[1];
             if(!visited[neighbour]){
-                flips += direction;
-                flips += dfs(neighbour, visited, adjList);
+                flips+= direction;
+                flips+= dfs(adjList, visited, neighbour);
             }
         }
         return flips;
     }
-
 }
