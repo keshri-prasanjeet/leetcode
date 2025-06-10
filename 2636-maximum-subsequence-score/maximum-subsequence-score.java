1b13d0ac5404bcaeeb6141f23bfceea88042480b
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-10 09:00:15 +0530

    Time: 79 ms (39.81%) | Memory: 66.7 MB (29.08%) - LeetSync

---
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        //need to combine the two arrays together into one
+        //then sort them based on nums2
+        int n = nums1.length;
+        int[][] combinedArr = new int[n][2];
+        for(int i=0;i<n;i++){
+            combinedArr[i][0] = nums1[i];
+            combinedArr[i][1] = nums2[i];
+        }
+
+        //0th index is the sum array
+        //1st index is the multiply array
+
+        Arrays.sort(combinedArr, (a,b) -> b[1] - a[1]); //sorting the array based on the nums2 arr
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        long maxSubsequenceScore = 0;
+        long sum = 0;
+        for(int i = 0;i<n;i++){
+            int num = combinedArr[i][0];
+            int minVal = combinedArr[i][1];
+
+            sum+=num;
+            minHeap.add(num);
+
+            if(minHeap.size() > k){
+                sum-=minHeap.poll();
+            }
+
+            if(minHeap.size() == k){
+                maxSubsequenceScore = Math.max(maxSubsequenceScore, (long) sum * minVal);
+            }
+        }
+        return maxSubsequenceScore;
+    }
+}
