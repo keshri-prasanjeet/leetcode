fff8f69b5551ffb40ad0a912d4a85bad8eeb1c8e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-10 07:23:48 +0530

    Time: 7 ms (71.46%) | Memory: 45.8 MB (58.23%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -4,29 +4,33 @@ class Solution {
         int n = maze[0].length;
         Queue<int[]> bfsQ = new ArrayDeque<>();
         bfsQ.offer(new int[]{entrance[0], entrance[1], 0});
+
         int[][] directions = {
-            {1,0},
-            {0,1},
-            {0,-1},
-            {-1,0}
+            {0,1},{1,0},{-1,0},{0,-1}
         };
 
         while(!bfsQ.isEmpty()){
-            int[] top = bfsQ.poll();
+            int top[] = bfsQ.poll();
             int x = top[0];
             int y = top[1];
             int steps = top[2];
+
+            //check if the current cell is a edge cell
             if(x==0 || y==0 || x+1==m || y+1==n){
-                if(x == entrance[0] && y == entrance[1]){}
+                if(x==entrance[0] && y==entrance[1]){}
                 else return steps;
             }
+
+            //not a edge cell
+
             for(int[] direction: directions){
-                int row = top[0]+direction[0];
-                int col = top[1]+direction[1];
-                if(row>=0 && row<m && col>=0 && col<n){
-                    //cell is not out of bounds
+                int row = direction[0]+x;
+                int col = direction[1]+y;
+
+                if(row >= 0 && col >=0 && row < m && col < n){
+                    //this cell is not out of bounds
                     if(maze[row][col] == '.'){
-                        maze[row][col] = '+';
+                        maze[row][col] = '+'; //make this a wall to not go in infinite loop
                         bfsQ.offer(new int[]{row, col, steps+1});
                     }
                 }
