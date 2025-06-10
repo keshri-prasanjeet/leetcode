8b61941e4aa3eb260de4342c2fd1c5116b194106
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-10 07:15:09 +0530

    Time: 4 ms (97.11%) | Memory: 45.8 MB (68.48%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -3,39 +3,31 @@ class Solution {
         int m = maze.length;
         int n = maze[0].length;
         Queue<int[]> bfsQ = new ArrayDeque<>();
-        bfsQ.offer(new int[]{entrance[0],entrance[1],0});
-
+        bfsQ.offer(new int[]{entrance[0], entrance[1], 0});
         int[][] directions = {
-            {-1, 0}, // up
-            {1, 0},  // down
-            {0, -1}, // left
-            {0, 1}   // right
+            {1,0},
+            {0,1},
+            {0,-1},
+            {-1,0}
         };
 
         while(!bfsQ.isEmpty()){
             int[] top = bfsQ.poll();
             int x = top[0];
             int y = top[1];
-            int steps=top[2];
-
-            maze[x][y] = '+';//deque change
-            if(x-1<0 || y-1 <0 || x+1==m || y+1==n){
-                if(x==entrance[0] && y==entrance[1]){
-                //do nothing
-                }
-                else{
-                    return steps;//first place where it is edge is the closesnt point
-                }
+            int steps = top[2];
+            if(x==0 || y==0 || x+1==m || y+1==n){
+                if(x == entrance[0] && y == entrance[1]){}
+                else return steps;
             }
-
             for(int[] direction: directions){
-                int row = x+direction[0];
-                int col = y+direction[1];
-                if(row >=0 && col >=0 && row < m && col < n){
-                    //safe cell
-                    if(maze[row][col]=='.'){
-                        maze[row][col] = '+';//enque change
-                        bfsQ.offer(new int[]{row, col, steps+1});//steps tracking
+                int row = top[0]+direction[0];
+                int col = top[1]+direction[1];
+                if(row>=0 && row<m && col>=0 && col<n){
+                    //cell is not out of bounds
+                    if(maze[row][col] == '.'){
+                        maze[row][col] = '+';
+                        bfsQ.offer(new int[]{row, col, steps+1});
                     }
                 }
             }
