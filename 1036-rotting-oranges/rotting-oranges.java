43e67b78dbc6d2e2358298224dfa3f1a80a99785
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-10 07:43:22 +0530

    Time: 2 ms (68.28%) | Memory: 42.6 MB (50.54%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,50 +2,39 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        Queue<int[]> rotten = new ArrayDeque<>();
-        int freshO = 0;
+
+        int freshOranges = 0;
+        Queue<int[]> rottenQ = new ArrayDeque<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == 1) freshO++;
-                else if(grid[i][j] == 2){
-                    rotten.offer(new int[]{i,j});
+                if(grid[i][j]==1) freshOranges++;
+                if(grid[i][j]==2){
+                    rottenQ.offer(new int[]{i,j});
                 }
             }
-        }
-        //i have a track of the rotten oranges and i also know the count of the fresh ones
-
-        if(freshO==0) return 0;
-
-        int[][] directions = {
-            {-1, 0}, // up
-            {1, 0},  // down
-            {0, -1}, // left
-            {0, 1}   // right
-        };
-        int minutes = 0;
-        while(!rotten.isEmpty()){
-            int rottenO = rotten.size();
-            while(rottenO-- > 0){
-                int[] top = rotten.poll();
-                int x = top[0];
-                int y = top[1];
-
-                for(int[] direction: directions){
-                    int newRow = x+direction[0];
-                    int newCol = y+direction[1];
-
-                    if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n){
-                        //we are at a safe grid
-                        if(grid[newRow][newCol] == 1){
-                            grid[newRow][newCol] = 2;//make it rotten
-                            freshO--;
-                            rotten.offer(new int[]{newRow, newCol});//add it to the queue
+        }if(freshOranges == 0) return 0;
+        //counted all the fresh oranges & rotten to compare with later
+        int minutesElapsed = 0;
+        int[][] directions = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        while(!rottenQ.isEmpty()){
+            int currentRotten = rottenQ.size();
+            for(int i=0;i<currentRotten;i++){
+                int[] rottenOrange = rottenQ.poll();
+                for(int[] direction:directions){
+                    int row = direction[0]+rottenOrange[0];
+                    int col = direction[1]+rottenOrange[1];
+                    if(row >=0 && row<m && col >=0 && col<n){
+                        //cell within bounds
+                        if(grid[row][col] == 1){
+                            grid[row][col] = 2;
+                            rottenQ.offer(new int[]{row,col});
+                            freshOranges--;
                         }
                     }
                 }
             }
-            minutes++;
-            if(freshO == 0) return minutes;
+            minutesElapsed++;
+            if(freshOranges == 0) return minutesElapsed;
         }
         return -1;
     }
