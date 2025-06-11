f1dd418d81db231dc7a125fc0534b9697c773898
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-11 06:55:11 +0530

    Time: 46 ms (92.19%) | Memory: 56.6 MB (59.52%) - LeetSync

---
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long totalCost(int[] costs, int k, int candidates) {
+        int len = costs.length;
+        PriorityQueue<Integer> minHeapL = new PriorityQueue<>();
+        PriorityQueue<Integer> minHeapR = new PriorityQueue<>();
+        int i = 0;
+        int j = len-1;
+
+        for(int count = 0; count< candidates; count++){
+            if(i<=j) minHeapL.offer(costs[i++]);
+            if(i<=j) minHeapR.offer(costs[j--]);
+        }
+        long totalCost = 0;
+        while(k-- > 0){
+            int leftPeek = minHeapL.isEmpty() ? Integer.MAX_VALUE : minHeapL.peek();
+            int rightPeek= minHeapR.isEmpty() ? Integer.MAX_VALUE : minHeapR.peek();
+
+
+            if(leftPeek <= rightPeek){
+                totalCost += minHeapL.poll();
+                if(i<=j) minHeapL.offer(costs[i++]);
+            }
+            else{
+                totalCost += minHeapR.poll();
+                if(i<=j) minHeapR.offer(costs[j--]);
+            }
+        }
+
+        return totalCost;
+
+    }
+}
