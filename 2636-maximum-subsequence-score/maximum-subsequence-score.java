90d7c461a8f9098dcb5daa5381c6a622a1215884
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-13 07:45:38 +0530

    Time: 79 ms (39.69%) | Memory: 67.5 MB (11.72%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.java
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.java
@@ -1,36 +1,33 @@
 class Solution {
     public long maxScore(int[] nums1, int[] nums2, int k) {
-        //consolidate the two arrays into one so that we cant sort them together
-        int n = nums1.length;
-        int[][] nums = new int[n][2];
-        for(int i=0;i<n;i++){
+        int len = nums1.length;
+        int [][] nums = new int[len][2];
+        long maxScore = 0;
+        long score = 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i=0;i<len;i++){
             nums[i][0] = nums1[i];
             nums[i][1] = nums2[i];
-        }
+        }//consolidate into one array
 
-        //sort the nums array based on nums2 val in decending order
-
-        Arrays.sort(nums, (a,b) -> b[1] - a[1]);//sorted in decending order
-
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();//pq to store only least k nums in queue
-
+        Arrays.sort(nums, (a,b) -> b[1] - a[1]);
+        //sort arrording to nums2
         long sum = 0;
-        long maxScore = 0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<len;i++){
             int nums1Val = nums[i][0];
             int nums2Val = nums[i][1];
 
             sum+=nums1Val;
             minHeap.offer(nums1Val);
-
             if(minHeap.size() > k){
                 sum-=minHeap.poll();
             }
 
             if(minHeap.size() == k){
-                long score = sum * nums2Val;
+                score = sum * nums2Val;
                 maxScore = Math.max(maxScore, score);
             }
+            
         }
         return maxScore;
     }
