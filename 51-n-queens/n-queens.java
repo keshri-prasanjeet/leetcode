8e7fb791ceafed2790ed759e5463cc2a25a02a19
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-16 05:37:55 +0530

    Time: 5 ms (30.89%) | Memory: 44.6 MB (95.38%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,53 +1,46 @@
 class Solution {
-    List<List<String>> answer;
+    List<List<String>> allBoards;
     public List<List<String>> solveNQueens(int n) {
-        answer = new ArrayList<>();
-        int temp = n;
-        String chessRow = "";
-        while(temp-- > 0){
-            chessRow+=".";
+        allBoards = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++){
+            sb.append(".");
         }
-        List<String> chessBoard = new ArrayList<>();
-        temp = n;
-        while(temp-- > 0){
-            chessBoard.add(chessRow);
+        List<String> board = new ArrayList<>();
+        String row = sb.toString();
+        for(int i=0;i<n;i++){
+            board.add(row);
         }
         Set<Integer> cols = new HashSet<>();
         Set<Integer> posD = new HashSet<>();
         Set<Integer> negD = new HashSet<>();
-        solve(chessBoard, 0, cols, posD, negD);
-
-        return answer;
+        solveNQueens(board, n, 0, cols, posD, negD);
+        return allBoards;
     }
 
-    private void solve(List<String> chessBoard, int row, Set<Integer> cols, Set<Integer> posD, Set<Integer> negD){
-        if(row == chessBoard.size()){
-            answer.add(new ArrayList<>(chessBoard));
+    private void solveNQueens(List<String> board, int n, int row, Set<Integer> cols, Set<Integer> posD, Set<Integer> negD){
+        if(row == n){
+            //we have placed all the queens
+            allBoards.add(new ArrayList<>(board));
             return;
         }
 
-        for(int i=0;i<chessBoard.size();i++){
+        for(int i=0;i<n;i++){
             if(!cols.contains(i) && !posD.contains(i+row) && !negD.contains(i-row)){
-                //if this cell is absolutely safe
-                String currentRow = chessBoard.get(row);
-
+                //this cell is safe from other queens
+                String currentRow = board.get(row);
                 char[] currentRowArr = currentRow.toCharArray();
                 currentRowArr[i] = 'Q';
-                String updatedRow = new String(currentRowArr);
-
-                chessBoard.set(row, updatedRow);
+                board.set(row, new String(currentRowArr));
                 cols.add(i);
                 posD.add(i+row);
                 negD.add(i-row);
-                solve(chessBoard, row+1, cols, posD, negD);
-                //now backtrack this
-
-                chessBoard.set(row, currentRow);
+                solveNQueens(board, n, row+1, cols, posD, negD);
+                board.set(row, currentRow);
                 cols.remove(i);
                 posD.remove(i+row);
                 negD.remove(i-row);
             }
         }
-
     }
 }
