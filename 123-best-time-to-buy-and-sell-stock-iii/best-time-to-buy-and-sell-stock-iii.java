911e5d0ddbd3c4e4f3c4f123fef36bf4ed642e16
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-18 09:25:55 +0530

    Time: 3 ms (89.00%) | Memory: 61.5 MB (50.06%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,8 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int sz = prices.length;
-        int[] leftProfit = new int[sz];
         int[] rightProfit= new int[sz];
+        int[] leftProfit = new int[sz];
+        
         int lMin = prices[0];
         for(int i=1;i<sz;i++){
             lMin = Math.min(lMin, prices[i]);
@@ -10,18 +11,15 @@ class Solution {
         }
 
         int rMax = prices[sz-1];
-        for(int j=sz-2;j>=0;j--){
-            rMax = Math.max(rMax, prices[j + 1]);
-            rightProfit[j] = Math.max(rightProfit[j+1], rMax - prices[j]);
+        for(int i=sz-2;i>=0;i--){
+            rMax= Math.max(rMax, prices[i]);
+            rightProfit[i] = Math.max(rightProfit[i+1], rMax-prices[i]);
         }
-
         int maxProfit = 0;
         for(int i=0;i<sz;i++){
-            int left = (i>0) ? leftProfit[i-1] : 0;
-            int right= (i< sz) ? rightProfit[i]: 0;
-            maxProfit = Math.max(maxProfit, left + right);
+            int left = i==0 ? 0 : leftProfit[i-1];
+            maxProfit = Math.max(maxProfit, left+rightProfit[i]);
         }
-
         return maxProfit;
     }
 }
