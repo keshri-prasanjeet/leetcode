16a5e128105321a2bc2b93898b0aec0fda6b19b0
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-19 09:30:54 +0530

    Time: 3 ms (11.63%) | Memory: 46.5 MB (10.71%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,19 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = Integer.MAX_VALUE;
-        int sell = 0;
-        int profit = 0;
-        for(int stock: prices){
-            if(stock <= buy){
-                buy = stock;
-                sell= buy;
-            }
-            else{
-                sell = stock;
-                profit += (sell-buy);
-                buy = stock;
-            }
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for(int i=1;i<n;i++){
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]); 
+            //max between continue to not hold a stock and sell the current held stock and become not holding a stock
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+            //max between continue to hold the stock and getting the profit until last point without holding stock and buying a stock today
         }
-        return profit;
+        return dp[n-1][0];
     }
 }
