442aec2330fd56da1f70100238c555c48fdd45d0
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-20 08:11:35 +0530

    Time: 57 ms (45.20%) | Memory: 64.3 MB (33.46%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,26 @@
 class Solution {
-    Integer[][][] dp;
+    int[][][] dp;
     public int maxProfit(int[] prices) {
-        dp = new Integer[prices.length][2][2];
-        return calculateProfit(prices, 0, 0, 1);
-    }
-
-    private int calculateProfit(int[] prices, int day, int holdingStatus, int remTransactions){
-        if(day==prices.length || remTransactions < 0) return 0;
-        
-        if (dp[day][holdingStatus][remTransactions]!= null) return dp[day][holdingStatus][remTransactions];
-
-        int skip = calculateProfit(prices, day+1, holdingStatus, remTransactions);
-
-        int doSomething = 0;
-        if(holdingStatus == 0){
-            //not holding anything
-            doSomething = -prices[day] + calculateProfit(prices, day+1, 1, remTransactions);
+        int n = prices.length;
+        dp = new int[n+1][2][3];
+        for(int day=n-1;day>-1;day--){
+            for(int holding=0;holding<2;holding++){
+                for(int transaction=1;transaction<=2;transaction++){
+                    if(holding == 0){
+                        dp[day][0][transaction] = Math.max(
+                            -prices[day] + dp[day+1][1][transaction],
+                            dp[day+1][0][transaction]
+                        );
+                    }
+                    else{
+                        dp[day][1][transaction] = Math.max(
+                            prices[day] + dp[day+1][0][transaction-1],
+                            dp[day+1][1][transaction]
+                        );
+                    }
+                }
+            }
         }
-        else{
-            doSomething = prices[day] + calculateProfit(prices, day+1, 0, remTransactions-1);
-        }
-        return dp[day][holdingStatus][remTransactions] = Math.max(doSomething, skip); 
+        return dp[0][0][2];
     }
 }
