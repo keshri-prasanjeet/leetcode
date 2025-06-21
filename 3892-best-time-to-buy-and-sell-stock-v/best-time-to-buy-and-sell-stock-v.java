c08b0ba93aa49eed77eeb53a4aa29fb6b8f4d019
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-21 06:11:47 +0530

    Time: 146 ms (35.33%) | Memory: 87.3 MB (40.32%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
@@ -2,41 +2,35 @@ class Solution {
     Long[][][] dp;
     public long maximumProfit(int[] prices, int k) {
         dp = new Long[prices.length][3][k];
-        return calculateMaxProfit(prices, 0, 0, k-1);
+        return calculateMaximumProfit(prices, 0, 0, k-1);
     }
 
-    private long calculateMaxProfit(int[] prices, int day, int holdingStatus, int transactions){
+    private long calculateMaximumProfit(int[] prices, int day, int holdingStatus, int transactions){
 
-        if(prices.length == day || transactions < 0) {
-            //must close the position that are still open but the days have exhausted
-            if(holdingStatus == 1) return Long.MIN_VALUE /2;
-            if(holdingStatus == 2) return Long.MIN_VALUE /2;
+        if(day == prices.length || transactions == -1){
+            if(holdingStatus == 2) return Long.MIN_VALUE / 2;
             return 0;
         }
-        if(dp[day][holdingStatus][transactions]!=null) return dp[day][holdingStatus][transactions];
-        // System.out.println("day is " + day);
-        long doNothing = calculateMaxProfit(prices, day+1, holdingStatus, transactions);
 
-        long doSomething =0;
+        if(dp[day][holdingStatus][transactions] != null) return dp[day][holdingStatus][transactions];
+
+        long doNothing = calculateMaximumProfit(prices, day+1, holdingStatus, transactions);
+        long doSomething = 0;
+
         if(holdingStatus == 0){
-            //i dont hold any stocks
-            //however i can short sell or normal buy
-            // System.out.println("holding status was 0");
-            doSomething = Math.max(prices[day] + calculateMaxProfit(prices, day+1, 2, transactions),
-                                    -prices[day] + calculateMaxProfit(prices, day+1, 1, transactions));
-                                    //max between short selling and normal buying
+            //not holding any stocks
+            long regularBuy = (-prices[day] + calculateMaximumProfit(prices, day+1, 1, transactions));
+            long shortSell =  ( prices[day] + calculateMaximumProfit(prices, day+1, 2, transactions));
+            doSomething = Math.max(shortSell, regularBuy);
         }
         else if(holdingStatus == 1){
-            // System.out.println("holding status was 1");
-            //i did a normal buy and i hold a stock currently, so sell
-            doSomething = prices[day] + calculateMaxProfit(prices, day+1, 0, transactions-1);//normal sell since i hold a stock
+            //bought a stock
+            doSomething = prices[day] + calculateMaximumProfit(prices, day+1, 0, transactions-1);
         }
         else{
-            // System.out.println("holding status was 2");
-            //i short sold and got here so i gotta buy
-            doSomething = -prices[day] + calculateMaxProfit(prices, day+1, 0, transactions-1);
+            //short sold a stock
+            doSomething = -prices[day] + calculateMaximumProfit(prices, day+1, 0, transactions-1);
         }
-        // System.out.println("Do nothing " + doNothing + ", do something " + doSomething);
-        return dp[day][holdingStatus][transactions]=Math.max(doNothing, doSomething);
+        return dp[day][holdingStatus][transactions] = Math.max(doSomething, doNothing);
     }
 }
