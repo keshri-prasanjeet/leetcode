2eb5d42707694f3288988c62fb8492cb1f3ba9cf
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-21 11:12:56 +0530

    Time: 3 ms (94.32%) | Memory: 45.1 MB (46.47%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,23 +1,26 @@
 class Solution {
     Integer[][] dp;
     public int minDistance(String word1, String word2) {
-        int len1= word1.length();
-        int len2= word2.length();
+        int len1 = word1.length();
+        int len2 = word2.length();
+
         dp = new Integer[len1+1][len2+1];
         return calculateMinDistance(len1, len2, word1, word2);
     }
 
-    private int calculateMinDistance(int i, int j, String w1, String w2){
-        if(i==0) return j;
-        if(j==0) return i;
-        if(dp[i][j]!=null) return dp[i][j];
-        if(w1.charAt(i-1) == w2.charAt(j-1)) return dp[i][j] = calculateMinDistance(i-1, j-1, w1, w2);
+    private int calculateMinDistance(int index1, int index2, String word1, String word2){
+        if(index1 == 0) return index2;
+        if(index2 == 0) return index1;
 
+        if(dp[index1][index2]!=null) return dp[index1][index2];
+
+        if(word1.charAt(index1-1) == word2.charAt(index2-1)) 
+            return dp[index1][index2] = calculateMinDistance(index1-1, index2-1, word1, word2);
         else{
-            int insert = calculateMinDistance(i, j-1, w1, w2);
-            int delete = calculateMinDistance(i-1, j, w1, w2);
-            int replace= calculateMinDistance(i-1,j-1, w1,w2);
-            return dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
+            int insert = calculateMinDistance(index1, index2-1, word1, word2);
+            int delete = calculateMinDistance(index1-1, index2, word1, word2);
+            int replace= calculateMinDistance(index1-1,index2-1,word1, word2);
+            return dp[index1][index2] = 1 + Math.min(insert, Math.min(delete, replace));
         }
     }
 }
