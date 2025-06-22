c05335f7cf74bbdd8443291f2cdfd8215d7a936b
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-22 07:14:35 +0530

    Time: 21 ms (28.77%) | Memory: 55.4 MB (5.22%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,21 +1,21 @@
 class Solution {
-    Integer dp[][];
+    Integer[][] dp;
     public int numDistinct(String s, String t) {
-        int ls = s.length();
-        int lt = t.length();
-        dp = new Integer[ls][lt];
-        return subCount(s, t, ls-1, lt-1);
+        int sLen = s.length();
+        int tLen = t.length();
+        dp = new Integer[sLen][tLen];
+        return maxSub(s, t, sLen-1, tLen-1);
     }
 
-    private int subCount(String s, String t, int i, int j){
-        
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!= null) return dp[i][j];
+    private int maxSub(String s, String t, int i, int j){
+        if(j<0) return 1;//we have found the entire t
+        if(i<0) return 0;//we have exhausted our s
+        if(dp[i][j]!=null) return dp[i][j];
         if(s.charAt(i) == t.charAt(j)){
-            //have two options, take the match or act like no match
-            return dp[i][j] = subCount(s, t, i-1, j-1) + subCount(s, t, i-1, j);
+            return dp[i][j] = maxSub(s, t, i-1, j-1) + maxSub(s, t, i-1, j);
         }
-        else return dp[i][j] = subCount(s, t, i-1, j);
+        else{
+            return dp[i][j] = maxSub(s, t, i-1, j);
+        }
     }
 }
