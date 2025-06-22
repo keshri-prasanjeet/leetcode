2142c90abff82246398e314bb774e4ef971fa273
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-22 06:23:33 +0530

    Time: 22 ms (95.83%) | Memory: 57.8 MB (5.02%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -10,18 +10,15 @@ class Solution {
         if(i>j) return 0;
         if(i==j) return 1;
         if(dp[i][j]!=null) return dp[i][j];
-        int matched = 0;
-        int nMatched= 0;
         if(s.charAt(i)==s.charAt(j)){
-            matched = 2 + calculateLongest(s, i+1, j-1);
+            return dp[i][j] = 2 + calculateLongest(s, i+1, j-1);
         }
             
         else{
-            nMatched = Math.max(
+            return dp[i][j] = Math.max(
                 calculateLongest(s, i+1, j),
                 calculateLongest(s, i, j-1)
             );
         }
-        return dp[i][j] = Math.max(matched, nMatched);
     }
 }
