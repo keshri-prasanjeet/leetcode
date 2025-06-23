2b240d38bb11e846a0bfb2e27ef279ee0a228807
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-23 14:13:52 +0530

    Time: 167 ms (32.86%) | Memory: 87.6 MB (35.07%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
@@ -1,36 +1,34 @@
 class Solution {
     Long[][][] dp;
     public long maximumProfit(int[] prices, int k) {
-        dp = new Long[prices.length][3][k];
-        return calculateMaximumProfit(prices, 0, 0, k-1);
+        int len = prices.length;
+        dp = new Long[len][3][k];
+        return buySellV(prices, 0, 0, k-1);
     }
 
-    private long calculateMaximumProfit(int[] prices, int day, int holdingStatus, int transactions){
-
-        if(day == prices.length || transactions == -1){
-            if(holdingStatus == 2) return Long.MIN_VALUE / 2;
+    private long buySellV(int[] prices, int index, int holdingStatus, int remTrans){
+        if(index == prices.length || remTrans == -1) {
+            if(holdingStatus == 2) return Long.MIN_VALUE/2;
             return 0;
         }
-
-        if(dp[day][holdingStatus][transactions] != null) return dp[day][holdingStatus][transactions];
-
-        long doNothing = calculateMaximumProfit(prices, day+1, holdingStatus, transactions);
+        if(dp[index][holdingStatus][remTrans]!=null) return dp[index][holdingStatus][remTrans];
+        long doNothing = buySellV(prices, index+1, holdingStatus, remTrans);
         long doSomething = 0;
-
         if(holdingStatus == 0){
-            //not holding any stocks
-            long regularBuy = (-prices[day] + calculateMaximumProfit(prices, day+1, 1, transactions));
-            long shortSell =  ( prices[day] + calculateMaximumProfit(prices, day+1, 2, transactions));
-            doSomething = Math.max(shortSell, regularBuy);
+            //can buy and can short sell
+            doSomething = Math.max(
+                (-prices[index] + buySellV(prices, index+1, 1, remTrans)),
+                (prices[index] + buySellV(prices, index+1, 2, remTrans))
+            );
         }
         else if(holdingStatus == 1){
-            //bought a stock
-            doSomething = prices[day] + calculateMaximumProfit(prices, day+1, 0, transactions-1);
+            //alreadpy bought canot buy again
+            doSomething = prices[index] + buySellV(prices, index+1, 0, remTrans-1);
         }
         else{
-            //short sold a stock
-            doSomething = -prices[day] + calculateMaximumProfit(prices, day+1, 0, transactions-1);
+            //need to buy because we had short sold
+            doSomething = -prices[index] + buySellV(prices, index+1, 0, remTrans-1);
         }
-        return dp[day][holdingStatus][transactions] = Math.max(doSomething, doNothing);
+        return dp[index][holdingStatus][remTrans] = Math.max(doSomething, doNothing);
     }
 }
