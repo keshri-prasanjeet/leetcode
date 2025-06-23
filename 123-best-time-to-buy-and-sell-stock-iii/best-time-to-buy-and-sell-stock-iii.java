dbd8e8f9f339d078efbb6cfc6c9fc01263b0fe5f
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-23 13:42:09 +0530

    Time: 126 ms (19.95%) | Memory: 92 MB (5.03%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,22 @@
 class Solution {
     Integer[][][] dp;
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        dp = new Integer[n][2][2];
-        return calculateProfit(prices, 0, 0, 1);
+        int len = prices.length;
+        dp = new Integer[len][2][2];
+        return buySell(prices, 0, 0, 1);
     }
-
-    private int calculateProfit(int[] prices, int index, int holdingStatus, int transactions){
-        if(index == prices.length || transactions == -1) return 0;
-
-        if(dp[index][holdingStatus][transactions]!=null) return dp[index][holdingStatus][transactions];
-        int doNothing = calculateProfit(prices, index+1, holdingStatus, transactions);
+    private int buySell(int[] prices, int index, int holdingStatus, int remTrans){
+        if(index == prices.length) return 0;
+        if(remTrans == -1) return 0;
+        if(dp[index][holdingStatus][remTrans]!=null) return dp[index][holdingStatus][remTrans];
+        int doNothing = buySell(prices, index+1, holdingStatus, remTrans);
         int doSomething = 0;
         if(holdingStatus == 0){
-            //if we are not holding stocks
-            doSomething = -prices[index] + calculateProfit(prices, index+1, 1, transactions);
+            doSomething = -prices[index] + buySell(prices, index+1, 1, remTrans);
         }
         else{
-            doSomething = prices[index] + calculateProfit(prices, index+1, 0, transactions-1);
+            doSomething = prices[index] + buySell(prices, index+1, 0, remTrans-1);
         }
-
-        return dp[index][holdingStatus][transactions] = Math.max(doSomething, doNothing);
+        return dp[index][holdingStatus][remTrans] = Math.max(doSomething, doNothing);
     }
 }
