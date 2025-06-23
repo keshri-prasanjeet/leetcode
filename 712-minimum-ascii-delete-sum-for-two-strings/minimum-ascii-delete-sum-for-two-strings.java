669058c4a0dd8e60a1805bd7be73ff18b422244e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-23 13:11:29 +0530

    Time: 28 ms (23.34%) | Memory: 49.1 MB (5.10%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,41 +1,37 @@
 class Solution {
     Integer[][] dp;
     public int minimumDeleteSum(String s1, String s2) {
-        int l1 = s1.length();
-        int l2 = s2.length();
-        dp = new Integer[l1][l2];
-        return calcDel(s1, s2, l1-1, l2-1);
+        int len1 = s1.length();
+        int len2 = s2.length();
+        dp = new Integer[len1][len2];
+        return findMinSum(s1, s2, len1-1, len2-1);
     }
 
-    private int calcDel(String s, String t, int i, int j){
-        if(i < 0){
+    private int findMinSum(String s, String t, int i, int j){
+        if(i<0){
+            //return all ASCII of t[j]
             int sum = 0;
             while(j>=0){
-                sum+=t.charAt(j);
-                j--;
+                sum += t.charAt(j--);
             }
             return sum;
         }
         if(j<0){
             int sum = 0;
-            while(i>=0){
-                sum+=s.charAt(i);
-                i--;
+            while(i >=0){
+                sum+=s.charAt(i--);
             }
             return sum;
         }
         if(dp[i][j]!=null) return dp[i][j];
         if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = calcDel(s, t, i-1, j-1);
+            return dp[i][j] = findMinSum(s, t, i-1, j-1);
         }
         else{
-            int delete1 = calcDel(s, t, i-1, j);
-            int delete2 = calcDel(s, t, i, j-1);
-
-            return dp[i][j] = Math.min( 
-                (delete1 + (int)s.charAt(i)), 
-                (delete2 + (int)t.charAt(j))
-                );
+            //do delete
+            int delI = (int) s.charAt(i) + findMinSum(s,t,i-1,j);
+            int delJ = (int) t.charAt(j) + findMinSum(s,t,i,j-1);
+            return dp[i][j] = Math.min(delI, delJ);
         }
     }
 }
