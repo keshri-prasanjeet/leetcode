a901ddfe2a1182a9e3f6f589f97f7b45fa50cb5c
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-23 13:25:48 +0530

    Time: 3 ms (11.81%) | Memory: 50.1 MB (5.23%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -2,25 +2,20 @@ class Solution {
     Integer[][] dp;
     public int maxProfit(int[] prices) {
         dp = new Integer[prices.length][2];
-        return calculateMaxProfit(prices, 0, 0);
+        return buySell(prices, 0, 0);
     }
 
-    private int calculateMaxProfit(int[] prices, int index, int holdingStatus){
+    private int buySell(int[] prices, int holdingStatus, int index){
         if(index == prices.length) return 0;
         if(dp[index][holdingStatus]!=null) return dp[index][holdingStatus];
-        int doNothing = calculateMaxProfit(prices, index+1, holdingStatus);
-
+        int doNothing = buySell(prices, holdingStatus, index+1);
         int doSomething = 0;
-
         if(holdingStatus == 0){
-            //holding nothing, lets hold a stock
-            doSomething = -prices[index] + calculateMaxProfit(prices, index+1, 1);
+            doSomething += -prices[index] + buySell(prices, 1, index+1);
         }
         else{
-            //holding a stock, let try to sell it
-            doSomething = prices[index] + calculateMaxProfit(prices, index+1, 0);
+            doSomething += prices[index] + buySell(prices, 0, index+1);
         }
-
         return dp[index][holdingStatus] = Math.max(doSomething, doNothing);
     }
 }
