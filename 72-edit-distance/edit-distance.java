c37bab9ca98b59806ff1f7b9f9061afc844c1860
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-23 12:55:39 +0530

    Time: 3 ms (93.80%) | Memory: 45.4 MB (11.30%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -4,21 +4,19 @@ class Solution {
         int len1 = word1.length();
         int len2 = word2.length();
         dp = new Integer[len1][len2];
-        return calculateOperations(word1, word2, len1-1, len2-1);
+        return calculateDistance(word1, word2, len1-1, len2-1);
     }
 
-    private int calculateOperations(String s, String t, int i, int j){
-        if(i<0) return j+1; //we have exhausted the s, so insert all of j
+    private int calculateDistance(String s, String t, int i, int j){
         if(j<0) return i+1;
+        if(i<0) return j+1;
         if(dp[i][j]!=null) return dp[i][j];
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = calculateOperations(s, t, i-1, j-1);
-        }
+        if(s.charAt(i) == t.charAt(j)) return dp[i][j] = calculateDistance(s,t,i-1,j-1);
         else{
-            int update = calculateOperations(s, t, i-1, j-1);
-            int delete = calculateOperations(s, t, i-1, j);
-            int insert = calculateOperations(s, t, i, j-1);
-            return dp[i][j] = 1 + Math.min(update, Math.min(delete, insert));
+            int insert = calculateDistance(s,t,i,j-1);
+            int update = calculateDistance(s,t,i-1,j-1);
+            int delete = calculateDistance(s,t,i-1,j);
+            return dp[i][j] = 1 + Math.min(insert, Math.min(update, delete));
         }
     }
 }
