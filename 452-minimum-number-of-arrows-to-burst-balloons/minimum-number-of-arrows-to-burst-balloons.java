7fb0941a043cf67d9bc62b631bd5856ade14ea3b
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-25 07:11:36 +0530

    Time: 56 ms (30.33%) | Memory: 68.9 MB (66.80%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,29 +1,21 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        //will check if my current interval and next interval have some common area
-        //if yes then i will just keep the intersection of it.
-        //this new intersection ballon is new balloon at this point that i will compare with the next
+        Arrays.sort(points, (a,b) -> Integer.compare(a[0], b[0]));
         int len = points.length;
-        if(len==1) return 1;
-        int[] prevBalloon = new int[2];
-        Arrays.sort(points, (a,b) -> Integer.compare(a[0],b[0]));
-        prevBalloon[0] = points[0][0];
-        prevBalloon[1] = points[0][1];
-        int rougueBalloons = 0;
+        int prevS = points[0][0];
+        int prevE = points[0][1];
+        int arrowsReq = 0;
         for(int i=1;i<len;i++){
-            int currL = points[i][0];
-            int currR = points[i][1];
-            if(currL <= prevBalloon[1] && currR >= prevBalloon[0]){
-                //overlap exists
-                prevBalloon[0] = Math.max(prevBalloon[0],currL);
-                prevBalloon[1] = Math.min(prevBalloon[1],currR);
+            if(points[i][0] <= prevE && points[i][1] >= prevS){
+                prevS = Math.max(prevS, points[i][0]);
+                prevE = Math.min(prevE, points[i][1]);
             }
             else{
-                rougueBalloons++;
-                prevBalloon[0] = currL;
-                prevBalloon[1] = currR;
+                arrowsReq++;
+                prevS = points[i][0];
+                prevE = points[i][1];
             }
         }
-        return rougueBalloons+1;
+        return arrowsReq+1;
     }
 }
