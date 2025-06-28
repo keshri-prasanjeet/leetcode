a7f3f8430d655dac32e3c93885effcbb5cf92700
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-28 16:18:01 +0530

    Time: 63 ms (56.15%) | Memory: 63.2 MB (19.21%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,24 +1,15 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Pair> st = new Stack<>();
+        Stack<Integer> st = new Stack<>();
         int[] answer = new int[temperatures.length];
-        st.push(new Pair(temperatures[0], 0));
+        st.push(0);
         for(int i=0;i<temperatures.length-1;i++){
-            while(!st.isEmpty() && st.peek().val < temperatures[i+1]){
-                Pair pop = st.pop();
-                answer[pop.index] = (i+1) - (pop.index);
+            while(!st.isEmpty() && temperatures[st.peek()] < temperatures[i+1]){
+                Integer pop = st.pop();
+                answer[pop] = i+1-pop;
             }
-            st.push(new Pair(temperatures[i+1], i+1));
+            st.push(i+1);
         }
         return answer;
-    }
-}
-
-class Pair{
-    int val;
-    int index;
-    public Pair(int val, int index){
-        this.val = val;
-        this.index = index;
     }
 }
