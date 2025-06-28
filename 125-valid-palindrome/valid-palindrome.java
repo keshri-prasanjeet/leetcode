10d98228cd97e99e204aab7383853285985cfa9d
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-28 07:19:57 +0530

    Time: 4 ms (55.88%) | Memory: 43.3 MB (60.62%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(Character.isLetterOrDigit(s.charAt(i))){
-                sb.append(s.charAt(i));
+        StringBuilder stripped = new StringBuilder();
+        for(char a: s.toCharArray()){
+            if(Character.isLetterOrDigit(a)){
+                stripped.append(Character.toLowerCase(a));
             }
         }
-        String filteredString = sb.toString().toLowerCase();
-        String revFilteredString = sb.reverse().toString().toLowerCase();
-        if(filteredString.equals(revFilteredString)) return true;
-        return false;
+        // String straight = stripped.toString();
+        return stripped.toString().equals(stripped.reverse().toString());
     }
 }
