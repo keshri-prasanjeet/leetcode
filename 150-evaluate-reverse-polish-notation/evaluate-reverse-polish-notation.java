9310cbc39d85787b946f683b294b1e7f73775db4
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-28 11:11:32 +0530

    Time: 6 ms (72.66%) | Memory: 44.5 MB (91.27%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -7,7 +7,7 @@ class Solution {
             if(token.equals("*") || token.equals("+") || token.equals("-") || token.equals("/")){
                 first = st.pop();
                 second= st.pop();
-                System.out.println(first + " " + second);
+                // System.out.println(first + " " + second);
                 if(token.equals("+")) st.push(first + second);
                 else if(token.equals("-")) st.push(second - first);
                 else if(token.equals("*")) st.push(second * first);
