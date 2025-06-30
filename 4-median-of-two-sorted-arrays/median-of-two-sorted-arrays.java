2fead3eed50e9de1a6db86b13798c5b08c9216ac
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-06-30 15:31:29 +0530

    Time: 1 ms (100.00%) | Memory: 45.9 MB (84.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,40 +2,40 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
-        
-        if(len2 < len1) return findMedianSortedArrays(nums2, nums1);
 
-        // core of the problem is to find a partition point
-        // a partition in nums1 and nums2
-        // pick a point in nums1 to partition and generate a point in nums2 from point in nums1
+        if(len2 < len1) return findMedianSortedArrays(nums2, nums1);
 
         int low = 0;
         int high = len1;
-        double answer = 0.0;        
         while(low <= high){
-            int partitionOne = (low + high) / 2;
-            int partitionTwo = (len1 + len2 + 1) / 2 - partitionOne;
+            int splitOne = (high+low)/2;
+            int splitTwo = (len1 + len2 + 1)/2 - splitOne;
 
-            int firstMax = partitionOne == 0 ? Integer.MIN_VALUE : nums1[partitionOne-1];
-            int firstMin = partitionOne == len1 ? Integer.MAX_VALUE : nums1[partitionOne];
+            //i have found both the split points and now i need to compare the min and max of both
 
-            int secondMax = partitionTwo == 0 ? Integer.MIN_VALUE : nums2[partitionTwo-1];
-            int secondMin = partitionTwo == len2 ? Integer.MAX_VALUE : nums2[partitionTwo];
+            int maxOfLeftOfFirst  = splitOne == 0 ? Integer.MIN_VALUE : nums1[splitOne-1];
+            int minOfRightOfFirst = splitOne == len1 ? Integer.MAX_VALUE : nums1[splitOne];
 
-            if(firstMax <= secondMin && secondMax <= firstMin){
-                //found the center point
-                if((len1+len2) % 2 == 0){
-                    return (Math.max(firstMax, secondMax) + Math.min(firstMin, secondMin)) / 2.0;
+            int maxOfLeftOfSecond  = splitTwo == 0 ? Integer.MIN_VALUE : nums2[splitTwo-1];
+            int minOfRightOfSecond = splitTwo == len2 ? Integer.MAX_VALUE : nums2[splitTwo];
+
+            //now i have the numbers on edges of both the halves of both the arrays
+
+            if(maxOfLeftOfFirst <= minOfRightOfSecond && maxOfLeftOfSecond <= minOfRightOfFirst){
+                //found a valid split to find the median
+
+                if((len1 + len2) % 2 == 0){
+                    return (Math.max(maxOfLeftOfFirst, maxOfLeftOfSecond) + Math.min(minOfRightOfFirst, minOfRightOfSecond)) / 2.0;
                 }
-                else return (double) Math.max(firstMax, secondMax);
+                else return (double)Math.max(maxOfLeftOfFirst, maxOfLeftOfSecond);
             }
-            else if(firstMax > secondMin){
-                high = partitionOne-1;
+            else if(maxOfLeftOfFirst > minOfRightOfSecond){
+                //partition is too far right, too big
+                high = splitOne-1;
             }
-            else{
-                low  = partitionOne+1;
-            }
+            else low = splitOne+1;
         }
+
         return 0.0;
     }
 }
