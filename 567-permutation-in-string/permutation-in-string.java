e953f6b1c8762ef7c6fc91649c17a6cf7f993a4a
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-01 08:12:26 +0530

    Time: 8 ms (61.68%) | Memory: 42.9 MB (50.72%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,32 +1,25 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] globalMap = new int[26];
-        for(char a: s1.toCharArray()){
-            globalMap[a-'a']++;
+        int[] characterMap = new int[26];
+        for(char a:s1.toCharArray()){
+            characterMap[a-'a']++;
         }
-
         int left = 0;
         int right = 0;
-
-        while(left < s2.length() && right< s2.length()){
-            if(globalMap[s2.charAt(left)-'a'] > 0){
-                //found something that mathches
-                // System.out.println("Did match char " + s2.charAt(left));
-                int[] localMap = Arrays.copyOf(globalMap, globalMap.length);
-                right = left;
-                while(right < s2.length() && localMap[s2.charAt(right)-'a']>0){
-                    // System.out.println("Did match char inside while" + s2.charAt(right));
-                    localMap[s2.charAt(right)-'a']--;
-                    right++;
-                }
-                int sum = 0;
-                for(int num: localMap) sum+=num;
-                if(sum==0) return true;
+        int matchesFound = 0;
+        while(right < s2.length()){
+            char rightCh = s2.charAt(right);
+            if(characterMap[rightCh-'a'] > 0) {
+                matchesFound++;
             }
-            else{
-                // System.out.println("Did not match char " + s2.charAt(left));
+            characterMap[s2.charAt(right)-'a']--;
+            if(matchesFound == s1.length()) return true;
+            if(right-left+1 == s1.length()){
+                if(characterMap[s2.charAt(left)-'a'] >= 0) matchesFound--;
+                characterMap[s2.charAt(left)-'a']++;
+                left++;
             }
-            left++;
+            right++;
         }
         return false;
     }
