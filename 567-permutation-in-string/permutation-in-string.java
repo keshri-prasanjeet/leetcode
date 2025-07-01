ea18f1d66430130672d5a9c86ca083ff99fcfdf9
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-01 08:22:30 +0530

    Time: 8 ms (61.68%) | Memory: 43.1 MB (45.41%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] characterMap = new int[26];
-        for(char a:s1.toCharArray()){
-            characterMap[a-'a']++;
+        int[] freqMap = new int[26];
+        for(char a: s1.toCharArray()){
+            freqMap[a-'a']++;
         }
+
         int left = 0;
-        int right = 0;
-        int matchesFound = 0;
+        int right= 0;
+        int matchLen = 0;
         while(right < s2.length()){
-            char rightCh = s2.charAt(right);
-            if(characterMap[rightCh-'a'] > 0) {
-                matchesFound++;
-            }
-            characterMap[s2.charAt(right)-'a']--;
-            if(matchesFound == s1.length()) return true;
+            if(freqMap[s2.charAt(right)-'a'] > 0) matchLen++;
+            freqMap[s2.charAt(right)-'a']--;
+
+            if(matchLen == s1.length()) return true;
+
             if(right-left+1 == s1.length()){
-                if(characterMap[s2.charAt(left)-'a'] >= 0) matchesFound--;
-                characterMap[s2.charAt(left)-'a']++;
+                //window too big reduce size
+                if(freqMap[s2.charAt(left)-'a'] >= 0) matchLen--;
+                freqMap[s2.charAt(left)-'a']++;
                 left++;
             }
             right++;
