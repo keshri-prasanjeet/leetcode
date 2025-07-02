760f5ad3e4244b59f71a59947045ae84294de3d9
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-02 21:11:15 +0530

    Time: 3 ms (87.73%) | Memory: 45.1 MB (54.68%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,40 +1,36 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length() < t.length()) return "";
         int[] tMap = new int[128];
-        for (char a : t.toCharArray()) {
+        for(char a: t.toCharArray()){
             tMap[a]++;
         }
 
-        int totalMatches = 0;
-        for (int i : tMap)
-            if (i > 0)
-                totalMatches++;
-
-        int left = 0;
+        int totalRequired = 0;
+        for(int i:tMap)
+            if(i>0) 
+                totalRequired++;
+        
         int right = 0;
-        int matchFound = 0;
-        int[] windowMap = new int[128];
+        int left = 0;
+        int[] sMap = new int[128];
         int minLen = Integer.MAX_VALUE;
-        int startLen = left;
-        while (right < s.length()) {
+        int minStart = 0;
+        int matchFound = 0;
+        while(right< s.length()){
             char rightChar = s.charAt(right);
-            windowMap[rightChar]++;
+            sMap[rightChar]++;
+            if(tMap[rightChar] > 0 && sMap[rightChar] == tMap[rightChar]) matchFound++;
 
-            if (tMap[rightChar] > 0 &&
-                    tMap[rightChar] == windowMap[rightChar])
-                matchFound++;
-            while (matchFound == totalMatches) {
-                //calculate the min
-                if(right-left+1 < minLen){
+            while(matchFound == totalRequired){
+
+                if(minLen > (right-left+1)){
                     minLen = right-left+1;
-                    startLen = left;
+                    minStart = left;
                 }
 
-                //remove the left and see what happens
-                char leftChar=s.charAt(left);
-                windowMap[s.charAt(left)]--;
-                if(tMap[s.charAt(left)] > 0 && tMap[s.charAt(left)] > windowMap[s.charAt(left)]){
+                char leftChar = s.charAt(left);
+                sMap[leftChar]--;
+                if(tMap[leftChar] > 0 && tMap[leftChar] > sMap[leftChar]){
                     matchFound--;
                 }
                 left++;
@@ -42,7 +38,6 @@ class Solution {
             right++;
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(startLen, startLen + minLen);
-
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart,minStart+minLen);
     }
 }
