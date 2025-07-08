1dc5b0fbec55178571e390fadec217046166c338
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-08 18:54:24 +0530

    Time: 3 ms (73.53%) | Memory: 43 MB (97.97%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,25 +1,24 @@
 class Solution {
     List<List<Integer>> answer;
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
         answer = new ArrayList<>();
         List<Integer> combination = new ArrayList<>();
-        findCombination(candidates, target, combination, 0);
+        Arrays.sort(candidates);
+        findSum(combination, candidates, 0, target);
         return answer;
     }
 
-    private void findCombination(int[] candidates, int target, List<Integer> combination, int startIndex){
-        if(target < 0) return;
+    private void findSum(List<Integer> combination, int[] candidates, int startIndex, int target){
         if(target == 0){
             answer.add(new ArrayList<>(combination));
+            return;
         }
-        for(int i=startIndex; i<candidates.length;i++){
-            if(i > startIndex && i < candidates.length && candidates[i] == candidates[i-1]) continue;
-            // if i > startIndex  and i am same as previous then skip me becasue the previous was already added recursed, removed and i was added again
-            // you already added 1 ... did the recursion and got all possible now again 1 at the start is double
+
+        for(int i=startIndex;i<candidates.length;i++){
+            if(i > startIndex && candidates[i] == candidates[i-1]) continue;
             if(candidates[i] > target) break;
             combination.add(candidates[i]);
-            findCombination(candidates, target - candidates[i], combination, i+1);
+            findSum(combination, candidates, i+1, target-candidates[i]);
             combination.remove(combination.size()-1);
         }
     }
