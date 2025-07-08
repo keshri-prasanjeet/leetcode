0f541053fcbf85ea9f68990bed9a6236721653db
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-08 09:05:11 +0530

    Time: 4 ms (39.94%) | Memory: 43.5 MB (23.07%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,24 +1,25 @@
 class Solution {
     List<List<Integer>> answer;
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
         answer = new ArrayList<>();
         List<Integer> combination = new ArrayList<>();
-        Arrays.sort(candidates);
-        doCombinationSum(candidates, target, combination, 0);
+        findCombination(candidates, target, combination, 0);
         return answer;
     }
-    private void doCombinationSum(int[] candidates, int target, List<Integer> combination, int startIndex){
-        
+
+    private void findCombination(int[] candidates, int target, List<Integer> combination, int startIndex){
+        if(target < 0) return;
         if(target == 0){
             answer.add(new ArrayList<>(combination));
-            return;
         }
-
-        for(int i=startIndex;i<candidates.length;i++){
-            if(i> startIndex && candidates[i] == candidates[i-1]) continue;
-            if(target < 0) break;
+        for(int i=startIndex; i<candidates.length;i++){
+            if(i > startIndex && i < candidates.length && candidates[i] == candidates[i-1]) continue;
+            // if i > startIndex  and i am same as previous then skip me becasue the previous was already added recursed, removed and i was added again
+            // you already added 1 ... did the recursion and got all possible now again 1 at the start is double
+            if(i == candidates.length) break;
             combination.add(candidates[i]);
-            doCombinationSum(candidates, target-candidates[i], combination, i+1);
+            findCombination(candidates, target - candidates[i], combination, i+1);
             combination.remove(combination.size()-1);
         }
     }
