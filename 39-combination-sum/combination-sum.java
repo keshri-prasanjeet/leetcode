fbdae978a83109ae7691214b9e3b418751a15b63
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-08 08:40:40 +0530

    Time: 2 ms (83.03%) | Memory: 44.7 MB (64.25%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -3,23 +3,20 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         answer = new ArrayList<>();
         List<Integer> combination = new ArrayList<>();
-        doCombination(candidates, target, combination, 0);
+        findCombination(candidates, target, combination , 0);
         return answer;
     }
 
-    private void doCombination(int[] candidates, int target, List<Integer> combination, int startIndex){
+    private void findCombination(int[] candidates, int target, List<Integer> combination, int startIndex){
         if(target < 0) return;
         if(target == 0){
             answer.add(new ArrayList<>(combination));
-            return;
         }
-
-        for(int i=startIndex;i<candidates.length;i++){
-            if(target-candidates[i] >= 0){
-                combination.add(candidates[i]);
-                doCombination(candidates, target - candidates[i], combination, i);
-                combination.remove(combination.size()-1);
-            }
+        for(int i=startIndex;i<candidates.length; i++){
+            int num = candidates[i];
+            combination.add(num);
+            findCombination(candidates, target - num, combination, i);
+            combination.remove(combination.size()-1);
         }
     }
 }
