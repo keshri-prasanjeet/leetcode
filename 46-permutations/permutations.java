491f098581b7eabdc22865d1fc26ba56dffe531c
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-08 09:25:36 +0530

    Time: 1 ms (93.18%) | Memory: 44.8 MB (29.96%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,25 +1,23 @@
 class Solution {
     List<List<Integer>> answer;
-    
     public List<List<Integer>> permute(int[] nums) {
         answer = new ArrayList<>();
-        boolean[] usedNums = new boolean[nums.length];
         List<Integer> permutation = new ArrayList<>();
-        dfs(permutation, nums, 0, usedNums);
+        boolean[] usedNums = new boolean[nums.length];
+        findPermutations(nums, permutation, usedNums);
         return answer;
     }
 
-    private void dfs(List<Integer> permutation, int[] nums, int index, boolean[] usedNums){
-        if(index == nums.length){
+    private void findPermutations(int[] nums, List<Integer> permutation, boolean[] usedNums){
+        if(permutation.size() == nums.length){
             answer.add(new ArrayList<>(permutation));
             return;
         }
-
         for(int i=0;i<nums.length;i++){
             if(!usedNums[i]){
-                usedNums[i] = true;
                 permutation.add(nums[i]);
-                dfs(permutation, nums, index+1, usedNums);
+                usedNums[i] = true;
+                findPermutations(nums, permutation, usedNums);
                 permutation.remove(permutation.size()-1);
                 usedNums[i] = false;
             }
