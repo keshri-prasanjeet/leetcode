1f1841a8081a85327aff6fce94aa62dcd187160b
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-08 18:28:30 +0530

    Time: 1 ms (99.91%) | Memory: 43.7 MB (44.54%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<List<Integer>> answer;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        answer = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        findSubsets(nums, subset, 0);
+        return answer;
+    }
+
+    private void findSubsets(int[] nums, List<Integer> subset, int startIndex){
+        //head first approach
+        if(startIndex > nums.length) return;
+        answer.add(new ArrayList<>(subset));
+        for(int i=startIndex; i<nums.length;i++){
+            if(i > startIndex && nums[i] == nums[i-1]) continue;
+            subset.add(nums[i]);
+            findSubsets(nums, subset, i+1);
+            subset.remove(subset.size()-1);
+        }
+    }
+}
