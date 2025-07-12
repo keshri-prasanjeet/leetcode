dbaa5a26b889af0e798c197908cef995ea50ca7c
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-12 15:25:23 +0530

    Time: 190 ms (26.26%) | Memory: 41.7 MB (61.79%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    int[][] directions = {
+        {1,0},
+        {0,1},
+        {-1, 0},
+        {0, -1}
+    };
+    boolean[][] visited;
+    int m;
+    int n;
+    public boolean exist(char[][] board, String word) {
+        //keep a visited map to remember which cell you visited
+        // make a direction list to direct in four directions
+        // do not keep the terminating condition at the end keep truthness / correctness as. you try to find the word
+
+        m = board.length;
+        n = board[0].length;
+
+        visited = new boolean[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(findWord(board, i, j, word, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean findWord(char[][] board, int i, int j, String word, int wordIndex){
+        if(word.charAt(wordIndex)!=board[i][j]) return false;
+        if(wordIndex + 1 == word.length()) return true; // head first check
+
+        for(int[] direction: directions){
+            int x = direction[0] + i;
+            int y = direction[1] + j;
+
+            if(x < m && x >= 0 && y < n && y >=0 && visited[x][y] == false){
+                if(wordIndex + 1 <= word.length()){
+                    visited[i][j] = true;
+                    if(findWord(board, x, y, word, wordIndex+1)) return true;
+                    visited[i][j] = false;
+                }
+            }
+        }
+        return false;
+    }
+}
