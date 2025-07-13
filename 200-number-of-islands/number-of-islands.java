4f175e15ca0aeedf5b5ae9364050d4e8cbe799c7
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-13 08:54:45 +0530

    Time: 5 ms (33.55%) | Memory: 48.8 MB (95.37%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,23 +1,56 @@
 class Solution {
+    int[][] directions = {
+        {1,0},
+        {0,1},
+        {-1,0},
+        {0,-1}
+    };
+
+    Queue<Pair> q;
     public int numIslands(char[][] grid) {
-        int islandCount = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        q = new LinkedList<>();
+        int totalIslands = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j] == '1'){
-                    DFSIslandSubmerge(grid, i, j);
-                    islandCount++;
+                    totalIslands++;
+                    q.offer(new Pair(i,j));
+                    grid[i][j] = '*';
+                    calculateIslands(grid);
                 }
             }
         }
-        return islandCount;
+        return totalIslands;
     }
 
-    private void DFSIslandSubmerge(char[][] grid, int m, int n){
-        if(m < 0 || m >= grid.length || n < 0 || n >= grid[0].length || grid[m][n] == '0') return;
-        grid[m][n] = '0';
-        DFSIslandSubmerge(grid, m+1, n);
-        DFSIslandSubmerge(grid, m-1, n);
-        DFSIslandSubmerge(grid, m, n+1);
-        DFSIslandSubmerge(grid, m, n-1);
+    private void calculateIslands(char[][] grid){        
+        while(!q.isEmpty()){
+            Pair top = q.poll();
+            int a = top.x;
+            int b = top.y;
+            for(int[] direction: directions){
+                int x = direction[0] + a;
+                int y = direction[1] + b;
+                
+                if(x >= 0 && x < grid.length && y >= 0 && y < grid[0].length){
+                    //valid index
+                    if(grid[x][y] == '1'){
+                        q.offer(new Pair(x,y));//add the neighbour to the queue
+                        grid[x][y] = '*'; //make the current one visited
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Pair{
+    int x;
+    int y;
+    Pair(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }
