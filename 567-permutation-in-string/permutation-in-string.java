13723bbfa84efe83e57d149931b1bd65211781c9
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-15 09:23:29 +0530

    Time: 6 ms (87.55%) | Memory: 42.5 MB (95.60%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if(s2.length() < s1.length()) return false;
         int[] freqMap = new int[26];
         for(char a: s1.toCharArray()){
             freqMap[a-'a']++;
         }
 
         int left = 0;
-        int right= 0;
-        int matchLen = 0;
+        int right = 0;
+        int matchFound = 0;
+        int s1Len = s1.length();
         while(right < s2.length()){
-            if(freqMap[s2.charAt(right)-'a'] > 0) matchLen++;
+            
+            if(freqMap[s2.charAt(right)-'a'] > 0) matchFound++;
             freqMap[s2.charAt(right)-'a']--;
-
-            if(matchLen == s1.length()) return true;
-
-            if(right-left+1 == s1.length()){
-                //window too big reduce size
-                if(freqMap[s2.charAt(left)-'a'] >= 0) matchLen--;
+            if(matchFound == s1Len) return true;
+            if(right - left +1 == s1Len){
+                if(freqMap[s2.charAt(left)-'a'] >= 0) matchFound--;
                 freqMap[s2.charAt(left)-'a']++;
                 left++;
             }
