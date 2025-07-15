108e59d497c67a376c19b639049c52e4dc1d2aab
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-15 10:35:57 +0530

    Time: 1 ms (50.65%) | Memory: 42.7 MB (82.46%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,23 +1,22 @@
 class Solution {
-    int len;
     List<List<Integer>> answer;
     public List<List<Integer>> subsets(int[] nums) {
-        len = nums.length;
+        Arrays.sort(nums);
         answer = new ArrayList<>();
         List<Integer> subset = new ArrayList<>();
-        findSubsets(nums, 0, subset);
+        findSubsets(nums, subset, 0);
         return answer;
     }
 
-    private void findSubsets(int[] nums, int index, List<Integer> subset){
-        if(index == len){
-            answer.add(new ArrayList<>(subset));
-            return;
+    private void findSubsets(int[] nums, List<Integer> subset, int startIndex){
+        //head first approach
+        if(startIndex > nums.length) return;
+        answer.add(new ArrayList<>(subset));
+        for(int i=startIndex; i<nums.length;i++){
+            if(i > startIndex && nums[i] == nums[i-1]) continue;
+            subset.add(nums[i]);
+            findSubsets(nums, subset, i+1);
+            subset.remove(subset.size()-1);
         }
-        findSubsets(nums, index+1, subset);
-        subset.add(nums[index]);
-        findSubsets(nums,index+1, subset);
-        subset.remove(subset.size()-1);
-        
     }
 }
