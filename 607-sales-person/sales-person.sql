f88b544ba6c62317df7401f0c88b3a92459dad77
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-17 15:55:44 +0530

    Time: 365 ms (31.47%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+select s.name from salesperson s where not exists(
+    select 1 
+    from orders o 
+    inner join 
+    company c on 
+    o.com_id = c.com_id 
+    where o.sales_id = s.sales_id 
+    and c.name = 'RED'
+);
