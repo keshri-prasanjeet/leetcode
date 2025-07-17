b0b0ea3e0b7813fb1940aa9560cbc7104bdb6ee7
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-17 17:37:11 +0530

    Time: 5 ms (96.07%) | Memory: 42.3 MB (98.27%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,26 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()) return false;
-        int[] freqMap = new int[26];
-        for(char a: s1.toCharArray()){
-            freqMap[a-'a']++;
-        }
-
-        int left = 0;
-        int right = 0;
-        int matchFound = 0;
         int s1Len = s1.length();
-        while(right < s2.length()){
-            
-            if(freqMap[s2.charAt(right)-'a'] > 0) matchFound++;
-            freqMap[s2.charAt(right)-'a']--;
-            if(matchFound == s1Len) return true;
-            if(right - left +1 == s1Len){
-                if(freqMap[s2.charAt(left)-'a'] >= 0) matchFound--;
-                freqMap[s2.charAt(left)-'a']++;
-                left++;
+        int s2Len = s2.length();
+        if(s2Len < s1Len) return false;
+        int[] s1FreqMap = new int[26];
+
+        for(char s: s1.toCharArray()){
+            s1FreqMap[s-'a']++;
+        }//populated the frequency
+
+        int l = 0;
+        int r = 0;
+        int matchingChars = 0;
+        while(r < s2Len){
+            char chR = s2.charAt(r);
+            if(s1FreqMap[chR - 'a']>0) matchingChars++;
+            s1FreqMap[chR - 'a']--;
+            if(matchingChars == s1Len) return true;
+            if(r-l+1 == s1Len){
+                //we have exceeded the window size
+                char chL = s2.charAt(l);
+                if(s1FreqMap[chL-'a'] >=0) matchingChars--;
+                s1FreqMap[chL-'a']++;
+                l++;
             }
-            right++;
+            r++;
         }
         return false;
     }
