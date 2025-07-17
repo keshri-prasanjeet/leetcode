2f19014b5c5805626499e595ea1c76dd9de15b9a
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-17 08:06:12 +0530

    Time: 2210 ms (7.46%) | Memory: 58 MB (5.01%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,29 +1,29 @@
 class Solution {
-    Boolean[][] dp;
+    Boolean dp[][];
     public String longestPalindrome(String s) {
+        int maxPalinLen = 0;
+        int start = -1;
+
         int len = s.length();
-        int start = 0;
-        int maxLen = 0;
         dp = new Boolean[len][len];
         for(int i=0;i<len;i++){
             for(int j=i;j<len;j++){
-                if(isPalin(s, i, j) == true){
-                    if(j-i+1 > maxLen){
+                if(isPalindrome(i,j,s)){
+                    if(j-i+1 > maxPalinLen){
                         start = i;
-                        maxLen = j-i+1;
+                        maxPalinLen = j-i+1;
                     }
                 }
             }
         }
-        return s.substring(start, start+maxLen);
+        return s.substring(start, start+maxPalinLen);
     }
 
-    private boolean isPalin(String s, int i, int j){
-        if(i>=j) return true;
-        if(dp[i][j]!=null) return dp[i][j];
-        if(s.charAt(i) != s.charAt(j)) return dp[i][j] = false;
-        else{
-            return dp[i][j] = isPalin(s, i+1, j-1);
+    private boolean isPalindrome(int start, int end, String s){
+        if(start >= end) return true;
+        if(s.charAt(start)!=s.charAt(end)){
+            return false;
         }
+        return isPalindrome(start+1, end-1, s);
     }
 }
