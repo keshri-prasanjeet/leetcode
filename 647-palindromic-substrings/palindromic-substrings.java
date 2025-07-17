6852b601c9e9afdead45044202e63bf7d93a896a
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-17 08:13:17 +0530

    Time: 968 ms (5.00%) | Memory: 41.6 MB (60.46%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -2,22 +2,22 @@ class Solution {
     Boolean[][] dp;
     public int countSubstrings(String s) {
         int len = s.length();
-        dp = new Boolean[len][len];
-        int count = 0;
+        int ans = 0;
         for(int i=0;i<len;i++){
             for(int j=i;j<len;j++){
-                if(isPalind(s, i, j)==true) count++;
+                if(isPalin(i,j,s)){
+                    ans++;
+                }
             }
         }
-        return count;
+        return ans;
     }
 
-    private boolean isPalind(String s, int i , int j){
-        if(i>=j) return true;
-        if(dp[i][j] != null) return dp[i][j];
-        if(s.charAt(i)!=s.charAt(j)) return dp[i][j] = false;
-        else{
-            return dp[i][j] = isPalind(s, i+1, j-1);
+    private boolean isPalin(int start, int end, String s){
+        if(start >= end) return true;
+        if(s.charAt(start)!=s.charAt(end)){
+            return false;
         }
+        return isPalin(start+1, end-1, s);
     }
 }
