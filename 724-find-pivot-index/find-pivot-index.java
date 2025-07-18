39e35ff96a9a30a7dab7f6bba10d1e2006f1bfc9
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-07-18 18:01:05 +0530

    Time: 1 ms (92.80%) | Memory: 46.6 MB (5.39%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,18 +1,19 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int len = nums.length;
-        int[] leftArr = new int[len];
-        int[] rightArr = new int[len];
+        int[] leftArr = new int[nums.length];
+        int[] rightArr= new int[nums.length];
         leftArr[0] = nums[0];
-        rightArr[len-1] = nums[len-1];
-        for(int i=1;i<len;i++){
-            leftArr[i] = nums[i]+leftArr[i-1];
+        rightArr[nums.length-1] = nums[nums.length-1];
+        for(int i=1;i<nums.length;i++){
+            leftArr[i] = leftArr[i-1]+nums[i];
         }
-        for(int i=len-2;i>=0;i--){
-            rightArr[i] = nums[i] + rightArr[i+1];
+
+        for(int i=nums.length-2;i>=0;i--){
+            rightArr[i] = rightArr[i+1]+nums[i];
         }
-        for(int i = 0;i<len;i++){
-            if(leftArr[i] == rightArr[i]) return i;
+
+        for(int i=0;i<nums.length;i++){
+            if(rightArr[i] == leftArr[i]) return i;
         }
         return -1;
     }
