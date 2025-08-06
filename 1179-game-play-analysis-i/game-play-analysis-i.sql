e905c28b2aa2766569b2efaa3e334aa5b0ba3c4b
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-08-07 03:10:15 +0530

    Time: 484 ms (81.43%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-select player_id, MIN(event_date) as first_login from activity group by player_id;
+select player_id, min(event_date) as first_login from Activity group by player_id;
