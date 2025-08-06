73315594ec4faa954f50d531d1e87c683bb7f183
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2025-08-07 02:34:10 +0530

    Time: 205 ms (79.33%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,4 +1,4 @@
 -- Write your PostgreSQL query statement below
 -- an id of customer that is never present in order
 
-select c.name as Customers from customers c left join orders o on c.id = o.customerId where o.customerId is NULL;
+select c.name as Customers from Customers c left join Orders o on c.id = o.customerId where o.customerId IS NULL;
