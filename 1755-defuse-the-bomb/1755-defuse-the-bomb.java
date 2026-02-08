62588da2ca0f72488ea01c1f9434446aa5055d5c
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-02-08 10:19:54 +0530

    1755-DEFUSE-THE-BOMB Stats: Time: 0 ms (100%), Space: 43.9 MB (47.05%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,35 +1,17 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int len = code.length;
-        int[] answer = new int[len];
-        if(k==0) return answer;
+        if(k==0) return new int[len];
         int left = 1;
         int right = 1;
-        int moved = 0;
-        int sum = 0;
-        int curIdx = 0;
         if(k>0){
             return decode(len, left, right, code, k);
-            //  while(curIdx < len){
-            //     if(moved == Math.abs(k)){
-            //         answer[curIdx++] = sum;
-            //         sum -= code[left];
-            //         left++;
-            //         left = left % len;
-            //         moved--;
-            //     }
-            //     sum += code[right];
-            //     right++;
-            //     right = right % len;
-            //     moved++;
-            // }
         }
         else{
             left = len+k;
             right = len+k;
             return decode(len, left, right, code, k);
         }
-        // return answer;
     }
 
     private int[] decode(int len, int left, int right, int[] code, int k){
@@ -52,5 +34,4 @@ class Solution {
         }
         return answer;
     }
-    
 }
