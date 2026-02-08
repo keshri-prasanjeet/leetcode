3b30b212624ed87be03b6882bf986707742e5a3e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-02-08 10:12:57 +0530

    1755-DEFUSE-THE-BOMB Stats: Time: 0 ms (100%), Space: 44.1 MB (27.97%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -10,7 +10,7 @@ class Solution {
         int curIdx = 0;
         if(k>0){
              while(curIdx < len){
-                if(moved == k){
+                if(moved == Math.abs(k)){
                     answer[curIdx++] = sum;
                     sum -= code[left];
                     left++;
@@ -27,21 +27,14 @@ class Solution {
             left = len+k;
             right = len+k;
             while(curIdx < len){
-                System.out.println("curIdx is " + curIdx);
-                System.out.println("left is " + left + " right is " + right);
-                System.out.println("moved is " + moved);
                 if(moved == Math.abs(k)){
                     answer[curIdx++]  = sum;
-                    System.out.println("found a window will have put sum " + sum + " at index " + (curIdx -1) );
                     sum -= code[left];
-                    System.out.println("left moved now intermediate sum is " + sum);
                     left++;
                     left = left % len;
                     moved--;
-                    System.out.println("the left is repositioned to " + left + " moved is " + moved);
                 }
                 sum += code[right];
-                System.out.println("intermediate sum is " + sum);
                 right++;
                 right = right % len;
                 moved++;
