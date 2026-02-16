aa313d4c0a0ebc60cf006a87abd99206f11af9f6
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-02-16 07:44:01 +0530

    0040-COMBINATION-SUM-II Stats: Time: 5 ms (99.22%), Space: 45 MB (87.81%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -14,8 +14,9 @@ class Solution {
         }
         
         for(int i=startIndex;i<candidates.length;i++){
-            if(candidates[i] > target) break;//we are too far, cant get a solution here
+            
             if(i > startIndex && candidates[i] == candidates[i-1]) continue; // not attempting duplicate nums
+            if(candidates[i] > target) break;//we are too far, cant get a solution here
             combination.add(candidates[i]);
             findCombination(candidates, target-candidates[i], i+1, combination);
             combination.remove(combination.size()-1);
