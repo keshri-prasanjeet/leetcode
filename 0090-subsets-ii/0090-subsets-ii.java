5ffd0c5ed1b9cfa2f11f35aa627c39b95dcd37ca
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-02-22 06:19:52 +0530

    0090-SUBSETS-II Stats: Time: 3 ms (55.25%), Space: 45.2 MB (42.58%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -3,17 +3,33 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         answer = new ArrayList<>();
         Arrays.sort(nums);
-        findSubsets(nums, 0, new ArrayList<>());
+        findSubsetsBottomUp(nums, 0, new ArrayList<>());
         return answer;
     }
 
-    private void findSubsets(int[] nums, int index, List<Integer> subset){
+    private void findSubsetsTopDown(int[] nums, int index, List<Integer> subset){
         answer.add(new ArrayList<>(subset));
         for(int i=index;i<nums.length;i++){
             if(i > index && nums[i] == nums[i-1]) continue;
             subset.add(nums[i]);
-            findSubsets(nums, i+1, subset);
+            findSubsetsTopDown(nums, i+1, subset);
             subset.remove(subset.size()-1);
         }
+    }
+
+    private void findSubsetsBottomUp(int[] nums, int index, List<Integer> subset){
+        if(index == nums.length){
+            answer.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[index]);
+        findSubsetsBottomUp(nums, index+1, subset);
+        subset.remove(subset.size()-1);
+
+        int next = index+1;
+        while(next < nums.length && nums[next] == nums[next-1]) next++;
+
+        findSubsetsBottomUp(nums, next, subset);
     }
 }
