dd7c1e30ab674bc4007a4143e44976ea046e64ac
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-02-23 07:58:21 +0530

    0039-COMBINATION-SUM Stats: Time: 2 ms (95.12%), Space: 45.7 MB (69%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,23 @@
 class Solution {
     List<List<Integer>> answer;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        // since repetetion is allowed, i can take the current or skip
-        // if the sum is equal to the target then add it to the answer list
-        // if the sum has overshot then return 
-        // if the sum is still less than the target sum then keep goin on/;
         answer = new ArrayList<>();
-        List<Integer> combination = new ArrayList<>();
-        findCombination(candidates, target, 0, combination);
+        findCombinations(candidates, new ArrayList<>(), target, 0);
         return answer;
     }
 
-    private void findCombination(int[] candidates, int target, int index, List<Integer> combination){
-        if(target < 0) return;
-        if(0 == target){
+    private void findCombinations(int[] candidates, List<Integer> combination, int target, int startIdx){
+        if(target == 0){
             answer.add(new ArrayList<>(combination));
             return;
         }
-        if(index == candidates.length) return;
-        if(candidates[index] <= target){
-            combination.add(candidates[index]);
-            findCombination(candidates, target - candidates[index], index, combination);
+        if(target < 0) return;
+
+        for(int i=startIdx;i<candidates.length;i++){
+
+            combination.add(candidates[i]);
+            findCombinations(candidates, combination, target-candidates[i], i); //-------->
             combination.remove(combination.size()-1);
         }
-        findCombination(candidates, target, index+1, combination);
     }
 }
