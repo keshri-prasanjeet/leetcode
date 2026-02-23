1f8325976ff2dd2a90162dc689a10c9e22eaad6f
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-02-23 08:05:15 +0530

    0040-COMBINATION-SUM-II Stats: Time: 7 ms (40.15%), Space: 45.4 MB (39.38%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -3,22 +3,20 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         answer = new ArrayList<>();
         Arrays.sort(candidates);
-        findCombination(candidates, target, 0, new ArrayList<>());
+        findCombinations(candidates, new ArrayList<>(), target, 0);
         return answer;
     }
 
-    private void findCombination(int[] candidates, int target, int startIndex, List<Integer> combination){
+    private void findCombinations(int[] candidates, List<Integer> combination, int target, int startIdx){
         if(target == 0){
             answer.add(new ArrayList<>(combination));
             return;
         }
-        
-        for(int i=startIndex;i<candidates.length;i++){
-            
-            if(i > startIndex && candidates[i] == candidates[i-1]) continue; // not attempting duplicate nums
-            if(candidates[i] > target) break;//we are too far, cant get a solution here
+        if(target < 0) return;
+        for(int i=startIdx;i<candidates.length;i++){
+            if(i>startIdx && candidates[i] == candidates[i-1]) continue;
             combination.add(candidates[i]);
-            findCombination(candidates, target-candidates[i], i+1, combination);
+            findCombinations(candidates, combination, target - candidates[i], i+1);
             combination.remove(combination.size()-1);
         }
     }
