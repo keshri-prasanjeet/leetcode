7a3288e28ef52bde446e2b86543624e452c22e15
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-02-28 07:29:34 +0530

    2217-STEP-BY-STEP-DIRECTIONS-FROM-A-BINARY-TREE-NODE-TO-ANOTHER Stats: Time: 22 ms (81.74%), Space: 93.2 MB (72.92%) - LeetHub

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -15,42 +15,49 @@
  */
 class Solution {
     public String getDirections(TreeNode root, int startValue, int destValue) {
-        TreeNode lceNode = LCE(root, startValue, destValue);
-        StringBuilder startPath = new StringBuilder();
-        StringBuilder destPath = new StringBuilder();
-        StringBuilder result = new StringBuilder();
-        buildPath(lceNode, startValue, startPath);
-        buildPath(lceNode,  destValue, destPath );
-
-        for(int i=0;i<startPath.length();i++){
-            result.append("U");
+        TreeNode lcaNode = LCA(root, startValue, destValue);
+        if(lcaNode == null) return "";
+        StringBuilder rightPath = new StringBuilder();
+        StringBuilder leftPath =  new StringBuilder();
+        StringBuilder finalPath = new StringBuilder();
+        buildPath(lcaNode, rightPath, startValue);
+        int rightLen = rightPath.length();
+        rightPath.setLength(0);
+        for(int i=0;i<rightLen;i++){
+            rightPath.append("U");
         }
-        result.append(destPath);
-        return result.toString();
-        
+        buildPath(lcaNode, leftPath, destValue);
+        rightPath.append(leftPath);
+        return rightPath.toString();
     }
 
-    private TreeNode LCE(TreeNode root, int startValue, int destValue){
-        if(root == null || root.val == startValue || root.val == destValue) return root;
-        TreeNode leftNode = LCE(root.left, startValue, destValue);
-        TreeNode rightNode= LCE(root.right,startValue, destValue);
-        if(leftNode != null && rightNode != null) return root;
-        return leftNode != null ? leftNode : rightNode;
+    private TreeNode LCA (TreeNode node, int startVal, int destVal){
+        if(node == null || node.val == startVal || node.val == destVal) return node;
+        TreeNode leftNode =  LCA(node.left , startVal, destVal);
+        TreeNode rightNode = LCA(node.right, startVal, destVal);
+        if(leftNode != null && rightNode != null) return node;
+        return leftNode != null? leftNode : rightNode;
     }
 
-    private boolean buildPath(TreeNode node, int target, StringBuilder path){
+    private boolean buildPath(TreeNode node, StringBuilder pathSb, int target){
         if(node == null) return false;
         if(node.val == target) return true;
 
-        path.append("L");
-        if(buildPath(node.left, target, path)) return true;
-        path.setLength(path.length()-1);
+        // we will explore left
+        pathSb.append("L");
+        if(buildPath(node.left, pathSb, target)) return true;
+        //if we reached this part that mean left side did not have target
+        //reset the pathSb
+        pathSb.setLength(pathSb.length()-1);
 
-
-        path.append("R");
-        if(buildPath(node.right, target, path)) return true;
-        path.setLength(path.length()-1);
+        // we will explore the right
+        pathSb.append("R");
+        if(buildPath(node.right, pathSb, target)) return true;
+        //if rigth was a disapointment then rest pathSb
+        pathSb.setLength(pathSb.length()-1);
 
         return false;
     }
+
+
 }
