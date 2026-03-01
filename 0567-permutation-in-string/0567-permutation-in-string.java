de4cbb12e0878cc40b5143a2391e3583b028da66
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-01 07:23:53 +0530

    0567-PERMUTATION-IN-STRING Stats: Time: 5 ms (96.96%), Space: 44.2 MB (52.56%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,27 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int s1Len = s1.length();
         int s2Len = s2.length();
-        if(s2Len < s1Len) return false;
+        int s1Len = s1.length();
+        if(s1Len > s2Len) return false;
+        int left = 0;
+        int right = 0;
+        int req = s1Len;
+
         int[] charMap = new int[26];
         for(char a: s1.toCharArray()){
             charMap[a-'a']++;
         }
-        int left = 0;
-        int right = 0;
-        int required = s1Len;
+
         while(right < s2Len){
             char rightChar = s2.charAt(right);
-            if(charMap[rightChar - 'a']>0){
-                required--;
-            }
+            if(charMap[rightChar - 'a']>0) req--;
             charMap[rightChar - 'a']--;
+
             if(right - left + 1 == s1Len){
-                if(required == 0) return true;
+                //window has been created
+                if(req == 0) return true;
                 char leftChar = s2.charAt(left);
-                if(charMap[leftChar - 'a'] >=0){
-                    required++;
-                }
+                if(charMap[leftChar - 'a']>=0) req++;
                 charMap[leftChar - 'a']++;
                 left++;
             }
