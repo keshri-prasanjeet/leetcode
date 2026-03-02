88c1a4b805739f9f510346f1ce7fa138749e5c75
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-02 17:15:56 +0530

    0017-LETTER-COMBINATIONS-OF-A-PHONE-NUMBER Stats: Time: 1 ms (54.47%), Space: 43.2 MB (95.09%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,39 @@
 class Solution {
-    List<List<Character>> charMap;
+List<List<String>> charMap = new ArrayList<>
+    (
+        List.of(
+            List.of("a","b","c"), 
+            List.of("d","e","f"),
+            List.of("g","h","i"),
+            List.of("j","k","l"),
+            List.of("m","n","o"),
+            List.of("p","q","r","s"),
+            List.of("t","u","v"),
+            List.of("w","x","y","z")
+        )
+    );    
     List<String> answer;
     int strLen;
     public List<String> letterCombinations(String digits) {
 
-        charMap = new ArrayList<>();
-        answer = new ArrayList<>();
-        charMap.add(List.of('a','b','c'));
-        charMap.add(List.of('d','e','f'));
-        charMap.add(List.of('g','h','i'));
-        charMap.add(List.of('j','k','l'));
-        charMap.add(List.of('m','n','o'));
-        charMap.add(List.of('p','q','r','s'));
-        charMap.add(List.of('t','u','v'));
-        charMap.add(List.of('w','x','y','z'));
-        //need to parse the string to Integer digits
-        
-        if(digits.length()==0) return answer;
         strLen = digits.length();
-        findCombination(digits, new StringBuilder(), 0);
+        answer = new ArrayList<>();
+        if(strLen == 0) return answer;
+
+        findCombinations(digits, new StringBuilder(), 0);
         return answer;
     }
 
-    private void findCombination(String digits, StringBuilder sb, int index){
-
-        if(sb.length() == strLen) {
+    private void findCombinations(String digits, StringBuilder sb, int index){
+        if(sb.length() == strLen){
             answer.add(sb.toString());
             return;
         }
-        int num = digits.charAt(index) - '0' - 2;
+
+        int num = digits.charAt(index) - '0' -2;
         for(int i=0;i<charMap.get(num).size();i++){
             sb.append(charMap.get(num).get(i));
-            findCombination(digits, sb, index+1);
+            findCombinations(digits, sb, index+1);
             sb.deleteCharAt(sb.length()-1);
         }
     }
