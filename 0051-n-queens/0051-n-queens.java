bccd517f6f42866bdd4771a534bdc46842af06bf
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-03 08:25:24 +0530

    0051-N-QUEENS Stats: Time: 1 ms (99.74%), Space: 46.9 MB (27.28%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,45 +1,43 @@
 class Solution {
     List<List<String>> answer;
     public List<List<String>> solveNQueens(int n) {
+        answer = new ArrayList<>();
+
+        boolean[] col = new boolean[n];
+        boolean[] posiD = new boolean[(2*n)-1];
+        boolean[] negiD = new boolean[(2*n)-1];
         char[][] chessBoard = new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 chessBoard[i][j] = '.';
             }
         }
-
-        answer = new ArrayList<>();
-        boolean[] col = new boolean[n];
-        boolean[] posD =new boolean[(2*n)-1];
-        boolean[] negD =new boolean[(2*n)-1];
-
-        placeNQueens(chessBoard, 0, col, posD, negD);
-        
+        placeNQueens(chessBoard, col, posiD, negiD, 0);
         return answer;
     }
 
-    private void placeNQueens(char[][] chessBoard, int row, boolean[] col, boolean[] posD, boolean[] negD){
+    private void placeNQueens(char[][] chessBoard, boolean[] col, boolean[] posiD, boolean[] negiD, int row){
         if(row == chessBoard.length){
-            List<String> board = new ArrayList<>();
+            List<String> oneBoard = new ArrayList<>();
             for(char[] r: chessBoard){
-                board.add(new String(r));
+                oneBoard.add(new String(r));
             }
-            answer.add(board);
+            answer.add(oneBoard);
             return;
         }
-        int n = chessBoard.length;
-        for(int i=0;i<n;i++){
-            if(col[i] == false && posD[row+i] == false && negD[row-i + (n-1)] == false){
+        int rowLength = chessBoard.length;
+        for(int i=0;i<rowLength;i++){
+            if(col[i]==false && posiD[row + i]==false && negiD[(row-i) + (rowLength-1)] == false){
                 chessBoard[row][i] = 'Q';
-                col[i] = true;
-                posD[row+i] = true;
-                negD[row-i + (n-1)] = true;
-                placeNQueens(chessBoard, row + 1, col, posD, negD);
+                col[i]=true;
+                posiD[row+i]=true;
+                negiD[(row-i) + (rowLength-1)]=true;
+                placeNQueens(chessBoard, col, posiD, negiD, row+1);
                 chessBoard[row][i] = '.';
                 col[i] = false;
-                posD[row+i] = false;
-                negD[row-i + (n-1)] = false;
-            }           
-        }    
+                posiD[row+i] = false;
+                negiD[(row-i) + (rowLength-1)] = false;
+            }
+        }
     }
 }
