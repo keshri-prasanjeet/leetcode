ba5c80d7175c786e3f531388a5119fa41c09e963
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-05 07:43:08 +0530

    0093-RESTORE-IP-ADDRESSES Stats: Time: 1 ms (99.65%), Space: 43.6 MB (95.43%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,35 +1,36 @@
 class Solution {
-    List<String> allIpAddresses;
+    List<String> solution;
     public List<String> restoreIpAddresses(String s) {
         int len = s.length();
-        allIpAddresses = new ArrayList<>();
-        if(len < 4 || len > 12) return allIpAddresses;
-        findIpAddresses(s, new StringBuilder(), 0, 0);
-        return allIpAddresses;
+        solution = new ArrayList<>();
+        if(len < 4 || len > 12) return solution;
+        findIPs(s, 0, 0, new StringBuilder());
+        return solution;
     }
 
-    private void findIpAddresses(String s, StringBuilder sb, int index, int segmentCount){
-        if(index == s.length() && segmentCount == 4){
-            allIpAddresses.add(sb.toString());
+    private void findIPs(String s, int index, int segCount, StringBuilder sb){
+        if(index == s.length() && segCount == 4){
+            solution.add(sb.toString());
             return;
         }
-        if(index == s.length() || segmentCount == 4) return;
 
-        for(int substrLen = 1; substrLen <=3 && index + substrLen <= s.length(); substrLen++){
-            String part = s.substring(index, index+substrLen);
-            if(substrLen > 1 && part.startsWith("0")) break;
+        if(index == s.length() || segCount == 4) return;
 
-            int intValue = Integer.parseInt(part);
-            if(intValue > 255) break;
+        for(int len = 1; len <=3 && index + len <= s.length(); len++){
+            String octet = s.substring(index, index+len);
 
-            int lenBefore = sb.length();
+            if(len > 1 && octet.startsWith("0")) break;
 
-            if(segmentCount > 0) sb.append(".");
-            sb.append(part);
+            if(Integer.parseInt(octet) > 255) break;
 
-            findIpAddresses(s, sb, index + substrLen, segmentCount+1);
+            int prevLen = sb.length();
 
-            sb.setLength(lenBefore);
+            if(segCount > 0) sb.append(".");
+            sb.append(octet);
+
+            findIPs(s, index+len, segCount+1, sb);
+
+            sb.setLength(prevLen);
 
         }
     }
