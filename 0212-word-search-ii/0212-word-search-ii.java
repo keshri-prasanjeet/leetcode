12b87420538d41a4f7722f0f694ffb63a29614a1
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-06 08:25:35 +0530

    0212-WORD-SEARCH-II Stats: Time: 1923 ms (5.01%), Space: 47.3 MB (35.54%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -9,10 +9,8 @@ class Solution {
             wordSet.add(word);
         }
         answer = new ArrayList<>();
-        // System.out.println("longest word " + longestWord);
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                // System.out.println("starting from " + board[i][j]);
                 wordSearch2(board, wordSet, i, j, new StringBuilder());
             }
         }
@@ -21,34 +19,24 @@ class Solution {
     }
 
     private void wordSearch2(char[][] board, Set<String> wordSet, int x, int y, StringBuilder sb){
-        if(x < 0 || x == board.length || y < 0 || y == board[0].length || sb.length() > longestWord) {
-            // System.out.println("returning because x is " + x + " and y is " + y);
-            return;
-        }
-        // System.out.println("word is " + sb.toString());
+        if(x < 0 || x == board.length || y < 0 || y == board[0].length || sb.length() > longestWord) return;
         if(board[x][y] != '*'){
             sb.append(board[x][y]);
             char cur = board[x][y];
             board[x][y] = '*';
             String w = sb.toString();
             if(wordSet.contains(w)){
-                // System.out.println("found word is " + w);
                 answer.add(w);
                 wordSet.remove(w);
             }   
             
-                // System.out.println("backtracking " + sb.length());
-                wordSearch2(board, wordSet, x+1, y, sb);
-                wordSearch2(board, wordSet, x-1, y, sb);
-                wordSearch2(board, wordSet, x, y+1, sb);
-                wordSearch2(board, wordSet, x, y-1, sb);
+            wordSearch2(board, wordSet, x+1, y, sb);
+            wordSearch2(board, wordSet, x-1, y, sb);
+            wordSearch2(board, wordSet, x, y+1, sb);
+            wordSearch2(board, wordSet, x, y-1, sb);
             
             board[x][y] = cur;
             sb.setLength(sb.length()-1);
-            return;
-        }
-        else {
-            // System.out.println("met a *");
             return;
         }
     }
