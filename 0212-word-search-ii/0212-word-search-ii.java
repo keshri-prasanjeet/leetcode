4624bf3a0fcc6f6a879092c9218bf881cc69558e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-06 08:55:43 +0530

    0212-WORD-SEARCH-II Stats: Time: 156 ms (68.22%), Space: 46.6 MB (96.22%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,43 +1,61 @@
 class Solution {
-    List<String> answer;
-    int longestWord;
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+    List<String> result = new ArrayList<>();
     public List<String> findWords(char[][] board, String[] words) {
-        Set<String> wordSet = new HashSet<>();
-        longestWord = 0;
-        for(String word: words){
-            longestWord = Math.max(longestWord, word.length());
-            wordSet.add(word);
-        }
-        answer = new ArrayList<>();
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                wordSearch2(board, wordSet, i, j, new StringBuilder());
+        TrieNode root = buildTrie(words);
+
+        for(int i = 0;i < board.length; i++){
+            for(int j = 0;j < board[0].length;j++){
+                findWord2(board, i, j, root);
             }
         }
-        
-        return answer;
+        return result;
     }
 
-    private void wordSearch2(char[][] board, Set<String> wordSet, int x, int y, StringBuilder sb){
-        if(x < 0 || x == board.length || y < 0 || y == board[0].length || sb.length() > longestWord) return;
-        if(board[x][y] != '*'){
-            sb.append(board[x][y]);
-            char cur = board[x][y];
-            board[x][y] = '*';
-            String w = sb.toString();
-            if(wordSet.contains(w)){
-                answer.add(w);
-                wordSet.remove(w);
-            }   
-            
-            wordSearch2(board, wordSet, x+1, y, sb);
-            wordSearch2(board, wordSet, x-1, y, sb);
-            wordSearch2(board, wordSet, x, y+1, sb);
-            wordSearch2(board, wordSet, x, y-1, sb);
-            
-            board[x][y] = cur;
-            sb.setLength(sb.length()-1);
-            return;
+    private void findWord2(char[][] board, int x, int y, TrieNode node){
+        if(x < 0 || x == board.length || y < 0 || y == board[0].length) return;
+
+        char c = board[x][y];
+
+        if(c == '*' || node.children[c-'a'] == null) return;
+
+        node = node.children[c - 'a'];
+
+        if(node.word != null){
+            result.add(node.word);
+            node.word = null;//avoid duplicates
         }
+
+        board[x][y] = '*';
+
+        findWord2(board, x + 1, y, node);
+        findWord2(board, x - 1, y, node);
+        findWord2(board, x, y + 1, node);
+        findWord2(board, x, y - 1, node);
+
+        board[x][y] = c;
+    }
+
+    private TrieNode buildTrie(String[] words){
+        TrieNode root = new TrieNode();
+
+        for(String word: words){
+            TrieNode node = root;
+            for(char c: word.toCharArray()){
+                int index = c - 'a';
+                if(node.children[index] == null){
+                    node.children[index] = new TrieNode();
+                }
+
+                node = node.children[index];
+            }
+            node.word = word;
+        }
+
+        return root;
     }
 }
