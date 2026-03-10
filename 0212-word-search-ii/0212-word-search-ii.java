bbdc9e6a050d597ada65e04417f0f21c9c5a8cc4
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-10 07:42:15 +0530

    0212-WORD-SEARCH-II Stats: Time: 155 ms (69%), Space: 47.2 MB (55.91%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,55 +1,55 @@
 class Solution {
+    List<String> answer = new ArrayList<>();
 
     class TrieNode {
+        String word;
         TrieNode[] children = new TrieNode[26];
-        String word = null;
     }
-    List<String> answer = new ArrayList<>();
     public List<String> findWords(char[][] board, String[] words) {
         
         TrieNode root = buildTrie(words);
+
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                wordSearch(board, root, i, j);
+                if(root.children[board[i][j] - 'a'] != null){
+                    wordSearch(board, root, i, j);
+                }
             }
         }
-        
+
         return answer;
     }
 
-    private void wordSearch(char[][] board, TrieNode node, int x, int y){
-        if(x < 0 || y < 0 || x == board.length || y == board[0].length) return;
+    private void wordSearch(char[][] board, TrieNode root, int x, int y){
+        if(x < 0 || x == board.length || y < 0 || y == board[0].length || board[x][y] == '$') return;
 
-        char cur = board[x][y];
+        if(root.children[board[x][y] - 'a'] == null) return;
 
-        if(cur == '*' || node.children[cur - 'a'] == null) return;
+        root = root.children[board[x][y] - 'a'];
 
-        node = node.children[cur - 'a'];
-        if(node.word != null){
-            answer.add(node.word);
-            node.word = null;
+        if(root.word != null){
+            answer.add(root.word);
+            root.word = null;
         }
 
-        //go all four directions
+        char cur = board[x][y];
+        board[x][y] = '$';
 
-        board[x][y] = '*';
-
-        wordSearch(board, node, x-1, y);
-        wordSearch(board, node, x+1, y);
-        wordSearch(board, node, x, y-1);
-        wordSearch(board, node, x, y+1);
+        wordSearch(board, root, x - 1, y);
+        wordSearch(board, root, x + 1, y);
+        wordSearch(board, root, x, y - 1);
+        wordSearch(board, root, x, y + 1);
 
         board[x][y] = cur;
     }
 
     private TrieNode buildTrie(String[] words){
         TrieNode root = new TrieNode();
-        for(String word: words){
+        for(String word : words){
             TrieNode node = root;
-            for(char a : word.toCharArray()){
-                if(node.children[a-'a'] == null){
-                    TrieNode newNode = new TrieNode();
-                    node.children[a-'a'] = newNode;
+            for(char a: word.toCharArray()){
+                if(node.children[a - 'a'] == null){
+                    node.children[a-'a'] = new TrieNode();
                 }
                 node = node.children[a-'a'];
             }
