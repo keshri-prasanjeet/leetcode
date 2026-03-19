39fad0c93eb3201692a849b1f7bd1801f3f502bb
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-19 08:57:48 +0530

    0052-N-QUEENS-II Stats: Time: 1 ms (95.47%), Space: 42 MB (82.3%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    int solution;
+    public int totalNQueens(int n) {
+        boolean[] col = new boolean[n];
+        boolean[] pDia = new boolean[2 * n];
+        boolean[] nDia = new boolean[2 * n];
+        // solution = new ArrayList<>();
+
+        char[][] chessboard = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chessboard[i][j] = '.';
+            }
+        }
+
+        dfsPlaceQueens(col, pDia, nDia, chessboard, 0);
+        return solution;
+    }
+
+    private void dfsPlaceQueens(boolean[] col, boolean[] pDia, boolean[] nDia, char[][] chessboard, int row) {
+        if (row == chessboard.length) {
+            // //gether the answer and put it in the solution list
+            // List<String> solutionString = new ArrayList<>();
+            // for (char[] solutionRow : chessboard) {
+            //     solutionString.add(new String(solutionRow));
+            // }
+            // solution.add(solutionString);
+            solution++;
+            return;
+        }
+        int n = chessboard.length;
+        for (int i = 0; i < n; i++) {
+            if (col[i] == false && pDia[i + row] == false && nDia[(i - row) + (n - 1)] == false) {
+                chessboard[row][i] = 'Q';
+                col[i] = true;
+                pDia[i + row] = true;
+                nDia[(i - row) + (n - 1)] = true;
+                dfsPlaceQueens(col, pDia, nDia, chessboard, row + 1);
+                chessboard[row][i] = '.';
+                col[i] = false;
+                pDia[i + row] = false;
+                nDia[(i - row) + (n - 1)] = false;
+            }
+        }
+    }
+}
