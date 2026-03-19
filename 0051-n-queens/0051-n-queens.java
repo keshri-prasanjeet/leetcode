b5032869f5000440ad1b1fe5984afa0c6b74b104
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-19 08:55:25 +0530

    0051-N-QUEENS Stats: Time: 1 ms (99.86%), Space: 45.6 MB (99.93%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,46 +1,44 @@
 class Solution {
     List<List<String>> solution;
     public List<List<String>> solveNQueens(int n) {
-        //no two queens can share
-        //same row
-        //same col
-        //same rightDiagonal
-        //same leftDiagonal
         boolean[] col = new boolean[n];
-        boolean[] rDia = new boolean[2*n];
-        boolean[] lDia = new boolean[2*n];
-        char[][] chessBoard = new char[n][n];
+        boolean[] pDia = new boolean[2*n];
+        boolean[] nDia = new boolean[2*n];
+        solution = new ArrayList<>();
+
+        char[][] chessboard = new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                chessBoard[i][j] = '.';
+                chessboard[i][j] = '.';
             }
         }
-        solution = new ArrayList<>();
-        placeNQueens(chessBoard, col, rDia, lDia, 0);
+
+        dfsPlaceQueens(col, pDia, nDia, chessboard, 0);
         return solution;
     }
 
-    private void placeNQueens(char[][] chessBoard, boolean[] col, boolean[] rDia, boolean[] lDia, int row){
-        if(row == chessBoard.length){
-            List<String> completeBoard = new ArrayList<>();
-            for(char[] solutionRow:chessBoard){
-                completeBoard.add(new String(solutionRow));
+    private void dfsPlaceQueens(boolean[] col, boolean[] pDia, boolean[] nDia, char[][] chessboard, int row){
+        if(row == chessboard.length){
+            //gether the answer and put it in the solution list
+            List<String> solutionString = new ArrayList<>();
+            for(char[] solutionRow: chessboard){
+                solutionString.add(new String(solutionRow));
             }
-            solution.add(completeBoard);
+            solution.add(solutionString);
             return;
         }
-        int n = chessBoard.length;
-        for(int i=0;i<chessBoard.length;i++){
-            if(col[i] == false && rDia[i+row] == false && lDia[(i-row) + (n-1)] == false){
-                chessBoard[row][i] = 'Q';
+        int n = chessboard.length;
+        for(int i=0;i<n;i++){
+            if(col[i] == false && pDia[i+row] == false && nDia[(i-row) + (n-1)] == false){
+                chessboard[row][i] = 'Q';
                 col[i] = true;
-                rDia[i+row] = true;
-                lDia[(i-row) + (n-1)] = true;
-                placeNQueens(chessBoard, col, rDia, lDia, row+1);
-                chessBoard[row][i] = '.';
+                pDia[i+row] = true;
+                nDia[(i-row) + (n-1)] = true;
+                dfsPlaceQueens(col, pDia, nDia, chessboard, row+1);
+                chessboard[row][i] = '.';
                 col[i] = false;
-                rDia[i+row] = false;
-                lDia[(i-row) + (n-1)] = false;
+                pDia[i+row] = false;
+                nDia[(i-row) + (n-1)] = false;
             }
         }
     }
