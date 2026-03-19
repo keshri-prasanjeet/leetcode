f5c074de37cb4ab0cbfb2eac0cbf41fb7ff57f1c
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-19 07:44:01 +0530

    0473-MATCHSTICKS-TO-SQUARE Stats: Time: 7 ms (85.15%), Space: 43.3 MB (49.08%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.java
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -1,42 +1,42 @@
 class Solution {
     public boolean makesquare(int[] matchsticks) {
-        //four sides of a square must be same 
-        //find target that each side must sum to
-        //do dfs to populate each side
-        //if all sides are populated return true
-
-        int sum = 0;
-        for(int matchstick: matchsticks){
-            sum+=matchstick;
-        }
-        if(sum%4!=0) return false;
-        int sidesum = sum/4;
+        int sumOfSticks = 0;
+        for(int match: matchsticks) sumOfSticks+=match;
+        if(sumOfSticks % 4 != 0) return false;
         Arrays.sort(matchsticks);
-
-         for (int i = 0; i < matchsticks.length / 2; i++) {
-            int temp = matchsticks[i];
-            matchsticks[i] = matchsticks[matchsticks.length - 1 - i];
-            matchsticks[matchsticks.length - 1 - i] = temp;
+        int len = matchsticks.length-1;
+        int i = 0;
+        int temp=0;
+        while(i <= len){
+            temp = matchsticks[i];
+            matchsticks[i] = matchsticks[len];
+            matchsticks[len] = temp;
+            i++;
+            len--;
+        }
+        int sideLen = sumOfSticks / 4;
+        for(int match: matchsticks){
+            if(sideLen < match) return false;
         }
 
-        for(int matchstick: matchsticks){
-            if(matchstick > sidesum) return false;
-        }
-
-        return makeSquareDFS(matchsticks, sidesum, 0, new int[4]);
+        return dfsSquare(sideLen, matchsticks, 0, new int[4]);
     }
 
-    private boolean makeSquareDFS(int[] matchsticks, int sideSum, int index, int[] sides){
+    private boolean dfsSquare(int sideLen, int[] matchsticks, int index, int[] sides){
         if(index == matchsticks.length) return true;
-        int matchstick = matchsticks[index];
+
+        int match = matchsticks[index];
+
         for(int i=0;i<4;i++){
-            if((sides[i] + matchstick) <= sideSum){
-                sides[i]+=matchstick;
-                if(makeSquareDFS(matchsticks, sideSum, index+1, sides)) return true;
-                sides[i]-=matchstick;
+            if((match + sides[i]) <= sideLen){
+                // int before = sides[i];
+                sides[i]+=match;
+                if(dfsSquare(sideLen, matchsticks, index+1, sides)) return true;
+                sides[i]-=match;
                 if(sides[i] == 0) break;
             }
         }
+
         return false;
     }
 }
