f7393156700ea21f74e18a68e6b0986619da30ca
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-22 22:49:18 +0530

    0051-N-QUEENS Stats: Time: 1 ms (99.86%), Space: 46.5 MB (76.49%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,14 +1,15 @@
 class Solution {
     List<List<String>> solution;
+
     public List<List<String>> solveNQueens(int n) {
         boolean[] col = new boolean[n];
-        boolean[] pDia = new boolean[2*n];
-        boolean[] nDia = new boolean[2*n];
+        boolean[] pDia = new boolean[2 * n];
+        boolean[] nDia = new boolean[2 * n];
         solution = new ArrayList<>();
 
         char[][] chessboard = new char[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 chessboard[i][j] = '.';
             }
         }
@@ -17,28 +18,28 @@ class Solution {
         return solution;
     }
 
-    private void dfsPlaceQueens(boolean[] col, boolean[] pDia, boolean[] nDia, char[][] chessboard, int row){
-        if(row == chessboard.length){
+    private void dfsPlaceQueens(boolean[] col, boolean[] pDia, boolean[] nDia, char[][] chessboard, int row) {
+        if (row == chessboard.length) {
             //gether the answer and put it in the solution list
             List<String> solutionString = new ArrayList<>();
-            for(char[] solutionRow: chessboard){
+            for (char[] solutionRow : chessboard) {
                 solutionString.add(new String(solutionRow));
             }
             solution.add(solutionString);
             return;
         }
         int n = chessboard.length;
-        for(int i=0;i<n;i++){
-            if(col[i] == false && pDia[i+row] == false && nDia[(i-row) + (n-1)] == false){
+        for (int i = 0; i < n; i++) {
+            if (col[i] == false && pDia[i + row] == false && nDia[(i - row) + (n - 1)] == false) {
                 chessboard[row][i] = 'Q';
                 col[i] = true;
-                pDia[i+row] = true;
-                nDia[(i-row) + (n-1)] = true;
-                dfsPlaceQueens(col, pDia, nDia, chessboard, row+1);
+                pDia[i + row] = true;
+                nDia[(i - row) + (n - 1)] = true;
+                dfsPlaceQueens(col, pDia, nDia, chessboard, row + 1);
                 chessboard[row][i] = '.';
                 col[i] = false;
-                pDia[i+row] = false;
-                nDia[(i-row) + (n-1)] = false;
+                pDia[i + row] = false;
+                nDia[(i - row) + (n - 1)] = false;
             }
         }
     }
