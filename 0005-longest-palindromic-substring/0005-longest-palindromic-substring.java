60bb2cf72d073c99f4b7f44803bd57a0d6c510b5
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-27 10:41:07 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 13 ms (95.95%), Space: 43.6 MB (76.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,36 +1,37 @@
 class Solution {
-    boolean[][] dp;
-    boolean[][] seen;
     public String longestPalindrome(String s) {
         int len = s.length();
-        if(len == 1) return s;
-        int maxLen = 1;
-        int subStart = 0;
-        dp = new boolean[len][len];
-        seen = new boolean[len][len];
+        // we will expand for odd length and even lengths separately
+        // after exapnding to the maximum for each point
+        // we will check if E OR O gave us the larger length
+        // whichever has larger len select that and then divide / 2
+        // start would be i - half and end would be i + half
+
+        int start = 0;
+        int end = 0;
         for(int i=0;i<len;i++){
-            for(int j=i+maxLen;j<len;j++){
-                if(isPalindrome(i, j, s)){
-                    if(maxLen < (j-i+1)){
-                        subStart = i;
-                        maxLen = j-i+1;
-                    }
-                }
+            int len1 = expandPalindrome(i, i, s);
+            int len2 = expandPalindrome(i, i+1, s);
+            // System.out.println("len1 is " + len1 + " len2 is " + len2);
+            int maxLen = Math.max(len1, len2);
+
+            if(maxLen > end-start){
+                start = i - (maxLen-1)/2;
+                end = i + (maxLen/2);
             }
+            // System.out.println("start is " + start + " end is " + end);
         }
-        return s.substring(subStart, subStart+maxLen);
+
+        return s.substring(start, end+1);
     }
 
-    private boolean isPalindrome(int start, int end, String s){
-        if(start >= end) return true;
-        if(seen[start][end]==true) return dp[start][end];
-        if(s.charAt(start)!=s.charAt(end)) {
-            seen[start][end] = true;
-            dp[start][end] = false;
-            return false;
+    private int expandPalindrome(int start, int end, String s){
+        // System.out.println("started expand at " + start + " " + end);
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+            // System.out.println(" start and end matched " + start + " " + end);
+            start--;
+            end++;
         }
-        dp[start][end] = isPalindrome(start+1,end-1,s);
-        seen[start][end] = true;
-        return dp[start][end];
+        return end-start-1;
     }
 }
