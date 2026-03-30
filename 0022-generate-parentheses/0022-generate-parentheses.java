a8dd769b0ff7b2eb92068c9f268d823c76feb2b5
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-30 07:46:04 +0530

    0022-GENERATE-PARENTHESES Stats: Time: 0 ms (100%), Space: 44.4 MB (77.79%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,24 +1,25 @@
 class Solution {
-    List<String> allParentheses;
+    List<String> answer;
     public List<String> generateParenthesis(int n) {
-        allParentheses = new ArrayList<>();
-        generate(n, n, new StringBuilder());
-        return allParentheses;
+        answer = new ArrayList<>();
+        generateParenthesis(n, new StringBuilder(), n);
+        return answer;
     }
 
-    private void generate(int open, int close, StringBuilder sb){
-        if(open == 0 && close == 0) {
-            allParentheses.add(sb.toString());
+    private void generateParenthesis(int open, StringBuilder sb, int close){
+        if(open == 0 && close ==0){
+            answer.add(sb.toString());
             return;
         }
+
         if(open > 0){
             sb.append("(");
-            generate(open-1,close,sb);
+            generateParenthesis(open-1, sb, close);
             sb.deleteCharAt(sb.length()-1);
         }
         if(close > open){
             sb.append(")");
-            generate(open, close-1, sb);
+            generateParenthesis(open, sb, close-1);
             sb.deleteCharAt(sb.length()-1);
         }
     }
