174e2a3031880f8c341dee79e145f8d132a8bffc
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-30 07:13:31 +0530

    0046-PERMUTATIONS Stats: Time: 1 ms (84.9%), Space: 45.5 MB (72.56%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,24 @@
 class Solution {
     List<List<Integer>> answer;
+    boolean[] usedNums;
     public List<List<Integer>> permute(int[] nums) {
         answer = new ArrayList<>();
-        boolean[] numSet = new boolean[nums.length];
-        findPermutations(nums, numSet, new ArrayList<>());
+        usedNums=new boolean[nums.length];
+        findPermutations(nums, new ArrayList<>(), 0);
         return answer;
     }
 
-    private void findPermutations(int[] nums, boolean[] numSet, List<Integer> permutation){
-        if(permutation.size() == nums.length){
+    private void findPermutations(int[] nums, ArrayList<Integer> permutation, int idx){
+        if(idx  == nums.length){
             answer.add(new ArrayList<>(permutation));
             return;
         }
-
         for(int i=0;i<nums.length;i++){
-            if(numSet[i] == false){
-                numSet[i] = true;
+            if(!usedNums[i]){
+                usedNums[i] = true;
                 permutation.add(nums[i]);
-                findPermutations(nums, numSet, permutation);
-                numSet[i] = false;
+                findPermutations(nums, permutation, idx+1);
+                usedNums[i] = false;
                 permutation.remove(permutation.size()-1);
             }
         }
