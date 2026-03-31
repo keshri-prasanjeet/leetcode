a6015c520ec8c66c1d05ae0a0bb011b5a5a19c42
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-31 07:31:52 +0530

    0131-PALINDROME-PARTITIONING Stats: Time: 7 ms (97.94%), Space: 65.5 MB (32.15%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,35 +1,35 @@
 class Solution {
-    List<List<String>> palindromes;
-    boolean[][] dp;
+    List<List<String>> answer;
     public List<List<String>> partition(String s) {
-        palindromes = new ArrayList<>();
         int len = s.length();
-        dp = new boolean[len][len];
-        //palindrome precompute
+        boolean[][] dp = new boolean[len][len];
+        answer = new ArrayList<>();
+        //precompute the palindromes
         for(int i=len-1;i>=0;i--){
             for(int j=i;j<len;j++){
                 if(s.charAt(i) == s.charAt(j)){
-                    if(j-i <= 2) dp[i][j] = true;
+                    if(j - i <= 2) dp[i][j] = true;
                     else dp[i][j] = dp[i+1][j-1];
                 }
             }
         }
-        findPalindromes(s, 0, new ArrayList<>(), len);
-        return palindromes;
+
+        findPalindromePartitions(s, dp, 0, new ArrayList<>());
+        return answer;
     }
 
-    private void findPalindromes(String s, int start, List<String> palindrome, int len){
-        if(start == s.length()){
-            palindromes.add(new ArrayList<>(palindrome));
+    private void findPalindromePartitions(String s, boolean[][] dp, int idx, List<String> palindromeSubsets){
+        if(idx == s.length()){
+            answer.add(new ArrayList<>(palindromeSubsets));
             return;
         }
 
-        for(int i=start;i<len;i++){
-            if(dp[start][i]){
-                String subStr = s.substring(start, i+1);
-                palindrome.add(subStr);
-                findPalindromes(s, i+1, palindrome, len);
-                palindrome.remove(palindrome.size()-1);
+        for(int i=idx;i<s.length();i++){
+            if(dp[idx][i]){
+                String substring = s.substring(idx, i+1);
+                palindromeSubsets.add(substring);
+                findPalindromePartitions(s, dp, i+1, palindromeSubsets);
+                palindromeSubsets.remove(palindromeSubsets.size()-1);
             }
         }
     }
