2b5bc9a31693e0b1d6133a5a06edcde4e25e5006
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-03-31 07:54:24 +0530

    0017-LETTER-COMBINATIONS-OF-A-PHONE-NUMBER Stats: Time: 0 ms (100%), Space: 43.7 MB (37.61%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,4 +1,5 @@
 class Solution {
+
     List<List<Character>> keyMappings = List.of(
         List.of('a','b','c'),
         List.of('d','e','f'),
@@ -9,27 +10,31 @@ class Solution {
         List.of('t','u','v'),
         List.of('w','x','y','z')
     );
-    int digLen;
+
     List<String> answer;
     public List<String> letterCombinations(String digits) {
-        digLen = digits.length();
+        //just a loop would suffice
+        //take the first character from the list of the digit
+        //recurse until used all
+        //backtrack remove last used and use the next loop index
+        char[] digitArr = digits.toCharArray();
         answer = new ArrayList<>();
-        if(digLen == 0) return answer;
-        findLetterCombinations(digits.toCharArray(), 0, new StringBuilder());
+        findAllCombinations(digitArr, 0, new StringBuilder());
         return answer;
     }
 
-    private void findLetterCombinations(char[] digits, int index, StringBuilder sb){
-        if(index == digits.length){
+    private void findAllCombinations(char[] digitArr, int index, StringBuilder sb){
+        if(index == digitArr.length){
             answer.add(sb.toString());
             return;
         }
+        char keyPressed = digitArr[index];
+        int intKey = (keyPressed - '0') - 2;
+        List<Character> charList = keyMappings.get(intKey);
 
-        List<Character> keySpace = keyMappings.get(digits[index]-'0' -2);
-        for(int i=0;i<keySpace.size();i++){
-            char press = keySpace.get(i);
-            sb.append(press);
-            findLetterCombinations(digits, index+1, sb);
+        for(int i=0;i<charList.size();i++){
+            sb.append(charList.get(i));
+            findAllCombinations(digitArr, index+1, sb);
             sb.setLength(sb.length()-1);
         }
     }
