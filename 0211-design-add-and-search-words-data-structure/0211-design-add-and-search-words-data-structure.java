1aa989d8bc9d439ea155b7b7b0f108e772185a36
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-02 07:54:53 +0530

    0211-DESIGN-ADD-AND-SEARCH-WORDS-DATA-STRUCTURE Stats: Time: 195 ms (54.08%), Space: 276.8 MB (32.17%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,13 +1,16 @@
 class WordDictionary {
 
     TrieNode root;
+
     public WordDictionary() {
         root = new TrieNode();
     }
     
     public void addWord(String word) {
         TrieNode cur = root;
-        for(char a: word.toCharArray()){
+
+        for(int i=0;i<word.length();i++){
+            char a = word.charAt(i);
             if(cur.children[a-'a'] == null){
                 cur.children[a-'a'] = new TrieNode();
             }
@@ -17,35 +20,35 @@ class WordDictionary {
     }
     
     public boolean search(String word) {
-        return dfs(word, 0, root);
+       return dfs(word, 0, root);
     }
 
     private boolean dfs(String word, int idx, TrieNode node){
         if(node == null) return false;
 
         if(idx == word.length()) return node.isWord;
-        char c = word.charAt(idx);
-        if(c != '.'){
-            return dfs(word, idx+1, node.children[c-'a']);
+
+        char a = word.charAt(idx);
+        if(a != '.'){
+            return dfs(word, idx+1, node.children[a-'a']);
         }
-        
+
         for(TrieNode child: node.children){
             if(child != null){
                 if(dfs(word, idx+1, child) == true) return true;
             }
         }
-
         return false;
     }
 }
 
 class TrieNode{
-    boolean isWord;
     TrieNode[] children;
+    boolean isWord;
 
-    public TrieNode(){
-        isWord=false;
+    TrieNode(){
         children = new TrieNode[26];
+        isWord = false;
     }
 }
 
