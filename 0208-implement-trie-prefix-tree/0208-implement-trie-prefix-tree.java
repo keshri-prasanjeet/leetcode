f034d14ae2d10f11e5422c815e7da4421a2b609e
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-02 07:22:19 +0530

    0208-IMPLEMENT-TRIE-PREFIX-TREE Stats: Time: 34 ms (55.89%), Space: 62.1 MB (47.82%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,54 +1,57 @@
 class Trie {
-
-    TrieNode node;
-
+    TrieNode root;
     public Trie() {
-        node = new TrieNode();
+        root = new TrieNode();
     }
     
     public void insert(String word) {
-        TrieNode cur = node;
-        for(char a: word.toCharArray()){
-            if(cur.children[a-'a'] == null){
+        TrieNode cur = root;
+
+        for(int i=0;i<word.length();i++){
+            char a = word.charAt(i);
+            if(cur.children[a-'a']==null){
                 cur.children[a-'a'] = new TrieNode();
             }
             cur = cur.children[a-'a'];
         }
-        cur.word = word;
+        cur.isWord = true;
     }
     
     public boolean search(String word) {
-        TrieNode cur = node;
-        for(char a: word.toCharArray()){
-            if(cur.children[a - 'a'] == null){
-                return false;
-            }
-            cur = cur.children[a-'a'];
-            if(word.equals(cur.word)) return true;
-        }
-        return false;
-    }
+        TrieNode cur = root;
 
+        for(int i=0;i<word.length();i++){
+            char a = word.charAt(i);
+            if(cur.children[a - 'a']==null) return false;
+            cur = cur.children[a-'a'];
+        }
+        return cur.isWord;
+    }
+    
     public boolean startsWith(String prefix) {
-        TrieNode cur = node;
-        for(char a: prefix.toCharArray()){
-            if(cur.children[a-'a'] == null){
-                return false;
-            }
-            cur = cur.children[a - 'a'];
+        TrieNode cur = root;
+
+        for(int i=0;i<prefix.length();i++){
+            char a = prefix.charAt(i);
+            if(cur.children[a - 'a']==null) return false;
+            cur = cur.children[a-'a'];
         }
         return true;
     }
+
+    
 }
 
-class TrieNode{
-    String word;
+class TrieNode {
+
     TrieNode[] children;
+    boolean isWord;
 
     public TrieNode(){
-        word = null;
         children = new TrieNode[26];
+        isWord = false;
     }
+        
 }
 
 /**
