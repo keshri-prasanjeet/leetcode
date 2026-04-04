0cbb7d31067162877756e8b495f5d267275ff21c
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-04 07:15:10 +0530

    1025-MINIMUM-COST-FOR-TICKETS Stats: Time: 0 ms (100%), Space: 43.3 MB (52.57%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -1,25 +1,28 @@
 class Solution {
     Integer[] dp;
     public int mincostTickets(int[] days, int[] costs) {
-        dp = new Integer[days.length+1];
+        dp = new Integer[days.length + 1];
         return findMinCost(days, costs, 0);
     }
 
-    private int findMinCost(int[] days, int[] cost, int idx){
-        if(idx >= days.length) return 0;
+    private int findMinCost(int[] days, int[] costs, int idx){
+        if(idx == days.length){
+            return 0;
+        }
+
         if(dp[idx] != null) return dp[idx];
 
-        int cost1 = cost[0] + findMinCost(days, cost, idx+1);
+        //one day pass
+        int oneDay = costs[0] + findMinCost(days, costs, idx+1);
 
         int j = idx;
-        while(j < days.length && days[j] < days[idx]+7) j++;
-        int cost7 = cost[1] + findMinCost(days, cost, j);
+        while(j < days.length && days[j] < days[idx]+7)j++;
+        int sevDay = costs[1] + findMinCost(days, costs, j);
 
         j = idx;
-
-        while(j < days.length && days[j] < days[idx]+30) j++;
-        int cost30 = cost[2] + findMinCost(days, cost, j);
-
-        return dp[idx] = Math.min(cost1, Math.min(cost7, cost30));
+        while(j < days.length && days[j] < days[idx]+30)j++;
+        int ttyDay = costs[2] + findMinCost(days, costs, j);
+        
+        return dp[idx] = Math.min(oneDay, Math.min(sevDay, ttyDay));
     }
 }
