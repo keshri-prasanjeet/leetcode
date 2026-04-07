d51163beaec94db866f9ac7a105fdd851579f421
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-07 07:42:06 +0530

    0416-PARTITION-EQUAL-SUBSET-SUM Stats: Time: 36 ms (64.98%), Space: 44.4 MB (70.91%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,28 +1,19 @@
 class Solution {
-    boolean equalSubset = false;
     public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        for(int num:nums){
-            totalSum+=num;
+        int sum = 0;
+        for(int num: nums){
+            sum+=num;
         }
-        if(totalSum%2!=0) return false;
-        int halfVal = totalSum / 2;
+        if(sum % 2 != 0) return false;
+        int target = sum /2;
         Arrays.sort(nums);
-        findPartition(nums, halfVal, 0, 0);
-        return equalSubset;
-    }
-
-    private void findPartition(int[] nums, int halfVal, int idx, int sum){
-        if(equalSubset == true) return;
-        if(idx>=nums.length) return;
-        if(sum == halfVal){
-            equalSubset = true;
-            return;
+        boolean[] dp = new boolean[target+1];
+        dp[0] = true;
+        for(int num: nums){
+            for(int j=target;j>=num;j--){
+                dp[j] = dp[j] || dp[j-num];
+            }
         }
-        
-        //take 
-        findPartition(nums, halfVal, idx+1, sum+nums[idx]);
-        //don't take
-        findPartition(nums, halfVal, idx+1, sum);
+        return dp[target];
     }
 }
