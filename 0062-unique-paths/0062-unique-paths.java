6caa3eaf3c956d2129d44802b88a1d9fbba12223
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-08 08:05:24 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 41 MB (99.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,19 +1,19 @@
 class Solution {
-    Integer[][] dp;
+    int[][] dp;
     public int uniquePaths(int m, int n) {
-        dp = new Integer[m+1][n+1];
-        return findUnique(m,n);
-    }
-
-    private int findUnique(int m, int n){
-        if(m<=0 && n<=0) return 0;
-        if(m==1 || n==1) return 1;
-
-        if(dp[m][n]!=null) return dp[m][n];
-
-        int ways = 0;
-        ways += findUnique(m-1, n);
-        ways += findUnique(m, n-1);
-        return dp[m][n] = ways;
+        dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            dp[i][n-1] = 1;
+        }
+        for(int i=0;i<n;i++){
+            dp[m-1][i] = 1;
+        }
+        // dp[m-1][n-1] = 0;
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 }
