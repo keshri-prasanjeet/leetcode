865ec35d892aaac5a24b1f092bec41f94b804f94
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-10 06:31:29 +0530

    0139-WORD-BREAK Stats: Time: 1 ms (99.58%), Space: 43.7 MB (74.31%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,26 +1,24 @@
 class Solution {
-    Boolean[] dp;
+    //solution dp with memoization
+    Boolean[] charMap;
     public boolean wordBreak(String s, List<String> wordDict) {
-        dp = new Boolean[s.length()+1];
-        return findWord(s, wordDict, 0); 
+        charMap = new Boolean[s.length()+1];
+        return findWord(s, wordDict, 0);
     }
 
-    private boolean findWord(String s, List<String> wordDict, int idx){
-        if(idx == s.length()) return true;
-        if(idx > s.length()) return false;
-        if(dp[idx]!=null) return dp[idx];
-        for(String word: wordDict){
-            int wordLen = word.length();
-            if(idx+wordLen <= s.length()){
-                if(s.substring(idx, idx+wordLen).equals(word)){
-                    if(findWord(s, wordDict, idx+wordLen) == true) {
-                        dp[idx] = true;
-                        return true;
-                    }
+    private boolean findWord(String s, List<String> words, int startIdx){
+        if(startIdx == s.length()) return true;
+        if(charMap[startIdx]!=null) return charMap[startIdx];
+        
+        for(String word: words){
+            int len=word.length();
+            if(startIdx + len > s.length()) continue;
+            if(s.substring(startIdx, startIdx+len).equals(word)){
+                if(findWord(s, words, startIdx+len)) {
+                    return charMap[startIdx] = true;
                 }
             }
         }
-        dp[idx] = false;
-        return false;
+        return charMap[startIdx] = false;
     }
 }
