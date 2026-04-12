79b44a13a8fe59ea32b017a14e5d243c00ce70a2
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-12 08:58:16 +0530

    0091-DECODE-WAYS Stats: Time: 0 ms (100%), Space: 42.9 MB (89.75%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,20 +1,23 @@
 class Solution {
     Integer[] dp;
     public int numDecodings(String s) {
+        //multiple ways to decode
+        //there are rules to branching
+        //if we reach the end that means we have decoded
+        //so do decoded+1
         dp = new Integer[s.length()];
-        return findAllDecodings(s, 0, s.length());
+        return countDecodings(s, 0, s.length());
     }
-
-    private int findAllDecodings(String s, int idx, int len){
+    private int countDecodings(String s, int idx, int len){
         if(idx == len) return 1;
         char a = s.charAt(idx);
         if(a == '0') return 0;
         if(dp[idx]!=null) return dp[idx];
         int takeOneCount = 0;
         int takeTwoCount = 0;
-        takeOneCount = findAllDecodings(s, idx+1, len);
-        if(idx+1 < len && (a == '1' || (a == '2' && ((s.charAt(idx+1) - '0') < 7)))){
-            takeTwoCount = findAllDecodings(s, idx+2, len);
+        takeOneCount = countDecodings(s, idx+1, len);
+        if(idx+1<len && (a == '1' || (a == '2' && (s.charAt(idx+1) - '0' < 7)))){
+            takeTwoCount = countDecodings(s, idx+2, len);
         }
         dp[idx] = takeOneCount + takeTwoCount;
         return dp[idx];
