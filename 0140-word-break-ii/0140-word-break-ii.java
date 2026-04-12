cb3097d7d7386c9ef62bce4af2db8867bb504252
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-12 06:47:37 +0530

    0140-WORD-BREAK-II Stats: Time: 3 ms (53.91%), Space: 48.4 MB (43.89%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,33 +1,33 @@
 class Solution {
     Map<Integer, List<String>> wordsMap = new HashMap<>();
     public List<String> wordBreak(String s, List<String> wordDict) {
-        int len = s.length();
         return findSentences(s, wordDict, 0);
     }
 
-    private List<String> findSentences(String s, List<String> words, int idx){
-        if(wordsMap.containsKey(idx)) return wordsMap.get(idx);
+    private List<String> findSentences(String s, List<String> wordDict, int index){
+        if(wordsMap.containsKey(index)) return wordsMap.get(index);
         List<String> result = new ArrayList<>();
-        if(idx == s.length()){
+        if(index == s.length()){
             result.add("");
             return result;
         }
-        for(String word:words){
+
+        for(String word:wordDict){
             int wordLen = word.length();
-            if(wordLen + idx > s.length()) continue;
-            if(s.startsWith(word, idx)){
-                List<String> subResult = findSentences(s, words, idx+wordLen);
-                for(String sub:subResult){
+            if(index + wordLen > s.length()) continue;
+            if(s.startsWith(word,index)){
+                List<String> subAnswer = findSentences(s, wordDict, index+wordLen);
+                for(String sub:subAnswer){
                     if(sub.isEmpty()){
                         result.add(word);
                     }
-                    else {
+                    else{
                         result.add(word + " " + sub);
                     }
                 }
             }
         }
-        wordsMap.put(idx, result);
+        wordsMap.put(index, result);
         return result;
     }
 }
