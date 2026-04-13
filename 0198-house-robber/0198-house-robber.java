66bcb305ce373edc53b79ff1814a2572e4472922
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-13 07:04:20 +0530

    0198-HOUSE-ROBBER Stats: Time: 0 ms (100%), Space: 42.9 MB (43.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,14 @@
 class Solution {
-    Integer[] dp;
     public int rob(int[] nums) {
-        dp = new Integer[nums.length];
-        return Math.max(robHouse(nums, 0), robHouse(nums, 1));
-    }
-    private int robHouse(int[] nums, int index){
-        if(index >= nums.length) return 0;
-        if(dp[index]!=null) return dp[index];
-        int robCur = nums[index] + robHouse(nums, index+2);
-        int skipCur = robHouse(nums, index+1);
-        return dp[index] = Math.max(robCur, skipCur);
+        int len = nums.length;
+        if(len==1) return nums[0];
+        if(len==2) return nums[0] > nums[1] ? nums[0] : nums[1];
+        int[] dp = new int[nums.length+1];
+        dp[0] = nums[0];
+        dp[1] = nums[0] > nums[1] ? nums[0] : nums[1];
+        for(int i=2;i<nums.length;i++){
+            dp[i] = Math.max((dp[i-2]+nums[i]),dp[i-1]);
+        }
+        return Math.max(dp[len-1],dp[len-2]);
     }
 }
