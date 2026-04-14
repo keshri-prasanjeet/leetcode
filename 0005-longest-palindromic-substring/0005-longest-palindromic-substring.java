543bc076239d3f60f130b2c423879a72ae5bb788
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-14 08:11:19 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 13 ms (95.98%), Space: 43.6 MB (72.98%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,29 @@
 class Solution {
     public String longestPalindrome(String s) {
         int len = s.length();
-        boolean[][] precompute = new boolean[len][len];
-        preComputePalindrome(s,precompute);
-        int longest = -1;
-        int startP = -1;
+        int longest = 0;
+        int start = 0;
+        int end = 0;
         for(int i=0;i<len;i++){
-            for(int j=i;j<len;j++){
-                if(precompute[i][j] && (j-i+1) > longest){
-                    longest = j-i+1;
-                    startP = i;
-                }
+            int even = expandForPal(i, i, s);
+            int odd  = expandForPal(i,i+1,s);
+
+            int curMax = Math.max(even, odd);
+
+            if(curMax > longest){
+                longest = curMax;
+                start = i-(curMax-1)/2;
+                end = i+(curMax/2);
             }
         }
-        return s.substring(startP, startP+longest);
+        return s.substring(start, end+1);
     }
 
-    private void preComputePalindrome(String s, boolean[][] p){
-
-        for(int i=s.length();i>=0;i--){
-            for(int j=i;j<s.length();j++){
-                if(s.charAt(i) == s.charAt(j)){
-                    if(j-i<=2) p[i][j] = true;
-                    else p[i][j] = p[i+1][j-1];
-                }
-            }
+    private int expandForPal(int start, int end, String s){
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+            start--;
+            end++;
         }
+        return end-start-1;
     }
 }
