02e7e12ccca64814d1c33ca96cf1bee68ca4f6d4
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-15 06:32:58 +0530

    0647-PALINDROMIC-SUBSTRINGS Stats: Time: 9 ms (38.76%), Space: 42.8 MB (84.54%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -2,15 +2,19 @@ class Solution {
     public int countSubstrings(String s) {
         //what if i precompute all the palindromes
         //and then i do nested loop
-        //or a even odd expansion
+        //or better an even odd expansion
         int len = s.length();
         int even = 0;
         int odd = 0;
-        for(int i=0;i<len;i++){
-            odd += expandPalindrome(s, i, i);
-            even+= expandPalindrome(s, i, i+1);
-        }
-        return odd+even;
+        // for(int i=0;i<len;i++){
+        //     odd += expandPalindrome(s, i, i);
+        //     even+= expandPalindrome(s, i, i+1);
+        // }
+        // return odd+even;
+
+        return IntStream.range(0, s.length())
+            .map(i -> expandPalindrome(s, i, i) + expandPalindrome(s, i, i+1))
+            .sum();
     }
 
     private int expandPalindrome(String s, int left, int right){
