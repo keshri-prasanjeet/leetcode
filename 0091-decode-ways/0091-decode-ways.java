7fc69b24c1e3e90a535f34d4a90b343b1850d8c9
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-15 06:59:51 +0530

    0091-DECODE-WAYS Stats: Time: 0 ms (100%), Space: 43 MB (69.55%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -7,16 +7,13 @@ class Solution {
     }
 
     private int countAllDecodings(String s, int idx, int len){
-        // System.out.println("The idx is " + idx + " and the len is " + len);
         if(idx == len) return 1;
-        if(idx > len) return 0;
         if(s.charAt(idx) == '0') return 0;
         if(dp[idx]!=null) return dp[idx];
         int totalDecodeWays = 0;
         totalDecodeWays += countAllDecodings(s, idx+1, len);
-        // System.out.println("Took single char and idx is " + idx);
         int cur = s.charAt(idx) - '0';
-        if(cur == 1 || (cur == 2 && idx+1<len && s.charAt(idx+1)-'0' < 7)){
+        if(idx+1 < len && (cur == 1 || (cur == 2 && s.charAt(idx+1)-'0' < 7))){
             totalDecodeWays += countAllDecodings(s, idx+2, len);
         }
         return dp[idx] = totalDecodeWays;
