85ab408d751044bdaf50c3880f110f7ea0727bb2
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-15 07:30:44 +0530

    0322-COIN-CHANGE Stats: Time: 25 ms (30.26%), Space: 47.1 MB (21.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,29 +2,22 @@ class Solution {
     Integer[] dp;
     public int coinChange(int[] coins, int amount) {
         dp = new Integer[amount+1];
-        int[] scoins = Arrays.stream(coins)
-                         .boxed()
-                         .sorted(Collections.reverseOrder())
-                         .mapToInt(Integer::intValue)
-                         .toArray();
-        int fewest = fewestCoinsChange(scoins, amount);
-        return fewest == Integer.MAX_VALUE ? -1 : fewest;
+        int ans = makeTargetSum(coins, amount);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
-    private int fewestCoinsChange(int[] coins, int amount){
-        if(amount < 0) return Integer.MAX_VALUE;
+    private int makeTargetSum(int[] coins, int amount){
         if(amount == 0) return 0;
         if(dp[amount]!=null) return dp[amount];
         int fewestCoins = Integer.MAX_VALUE;
         for(int coin: coins){
             if(amount - coin >= 0){
-                int coinsUsed = fewestCoinsChange(coins, amount-coin);
+                int coinsUsed = makeTargetSum(coins, amount-coin);
                 if(coinsUsed != Integer.MAX_VALUE){
                     fewestCoins = Math.min(fewestCoins, 1 + coinsUsed);
                 }
             }
         }
-        dp[amount] = fewestCoins;
-        return dp[amount];
+        return dp[amount] = fewestCoins;
     }
 }
