006565db3377747df6a3dc07cb4c3eae66bfed23
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-15 07:38:17 +0530

    0322-COIN-CHANGE Stats: Time: 10 ms (99.85%), Space: 46.2 MB (85.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,13 @@
 class Solution {
-    Integer[] dp;
     public int coinChange(int[] coins, int amount) {
-        dp = new Integer[amount+1];
-        int ans = makeTargetSum(coins, amount);
-        return ans == Integer.MAX_VALUE ? -1 : ans;
-    }
-
-    private int makeTargetSum(int[] coins, int amount){
-        if(amount == 0) return 0;
-        if(dp[amount]!=null) return dp[amount];
-        int fewestCoins = Integer.MAX_VALUE;
-        for(int coin: coins){
-            if(amount - coin >= 0){
-                int coinsUsed = makeTargetSum(coins, amount-coin);
-                if(coinsUsed != Integer.MAX_VALUE){
-                    fewestCoins = Math.min(fewestCoins, 1 + coinsUsed);
-                }
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+        for(int coin:coins){
+            for(int i = coin;i<= amount;i++){
+                dp[i] = Math.min(dp[i],dp[i-coin]+1);
             }
         }
-        return dp[amount] = fewestCoins;
+        return dp[amount] == amount+1 ? -1 : dp[amount];
     }
 }
