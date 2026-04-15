cba316bbd278e1e5f94628bf16cd90481fa8a123
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-15 07:41:14 +0530

    0322-COIN-CHANGE Stats: Time: 10 ms (99.85%), Space: 46.4 MB (63.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -4,8 +4,8 @@ class Solution {
         Arrays.fill(dp, amount+1);
         dp[0] = 0;
         for(int coin:coins){
-            for(int i = coin;i<= amount;i++){
-                dp[i] = Math.min(dp[i],dp[i-coin]+1);
+            for(int i=coin;i<=amount;i++){
+                dp[i] = Math.min(dp[i], dp[i-coin]+1);
             }
         }
         return dp[amount] == amount+1 ? -1 : dp[amount];
