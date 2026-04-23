3d3e7197548daa99abeaa7c4b8ed07ccf357933a
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-23 06:39:11 +0530

    1522-STONE-GAME-III Stats: Time: 30 ms (47.54%), Space: 94.4 MB (15.36%) - LeetHub

--- 1522-stone-game-iii/1522-stone-game-iii.java
+++ 1522-stone-game-iii/1522-stone-game-iii.java
@@ -2,21 +2,23 @@ class Solution {
     Integer[] dp;
     public String stoneGameIII(int[] stoneValue) {
         dp = new Integer[stoneValue.length + 1];
-        int res = findWinner(stoneValue, 0);
-        if(res > 0) return "Alice";
-        else if (res < 0) return "Bob";
+        int result = findWinner(stoneValue, 0);
+        if(result > 0) return "Alice";
+        else if(result < 0) return "Bob";
         else return "Tie";
     }
 
-    private int findWinner(int[] stones, int index){
-        if(index >= stones.length) return 0;
-        if(dp[index]!=null) return dp[index];
+    private int findWinner(int[] stones, int idx){
+        if(idx == stones.length) return 0;
+        if(dp[idx]!=null) return dp[idx];
         int diff = Integer.MIN_VALUE;
-        int stonesValue = 0;
-        for(int i=0;i<3 && i+index < stones.length;i++){
-            stonesValue+=stones[index+i];
-            diff = Math.max(diff, stonesValue - findWinner(stones, index+i+1));
+        int stoneValue = 0;
+
+        for(int i=0;i<3 && idx+i < stones.length;i++){
+            stoneValue+=stones[idx+i];
+            diff = Math.max(diff, stoneValue - findWinner(stones, idx+i+1));
         }
-        return dp[index] = diff;
+
+        return dp[idx] = diff;
     }
 }
