771549ec40b684ae51a67855bf723cc7d5f72a2f
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-26 19:35:38 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 42.3 MB (29.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,17 @@
 class Solution {
-    Integer[][] dp;
     public int uniquePaths(int m, int n) {
-        // only way to reach the goal is to move either right or bottom
-        // at each grid we have two options, go down or go right
-        // never go out of bounds
-        // return total number of possible paths that can one take
-        dp = new Integer[m+1][n+1];
-        return findUniquePaths(m, n);
-    }
-
-    private int findUniquePaths(int m, int n){
-        if(m <= 0 || n <= 0) return 0;
-        if(m == 1 || n == 1) return 1;
-        if(dp[m][n]!=null) return dp[m][n];
-        return dp[m][n] = findUniquePaths(m-1, n) + findUniquePaths(m, n-1);
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<m;i++){
+            dp[i][0] = 1;
+        }
+        for(int i=0;i<n;i++){
+            dp[0][i] = 1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
