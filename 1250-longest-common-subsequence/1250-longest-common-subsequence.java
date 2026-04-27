25ada6c36a7bfcb80929ed8158f3977e63484cd8
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-27 06:39:41 +0530

    1250-LONGEST-COMMON-SUBSEQUENCE Stats: Time: 31 ms (11.88%), Space: 62.3 MB (5.01%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -3,19 +3,17 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int len1 = text1.length();
         int len2 = text2.length();
-
-        dp = new Integer[len1+1][len2+1];
-        findLCS(text1, text2, 0, 0);
-        return dp[0][0];
+        dp = new Integer[len1][len2];
+        return findLCS(text1, text2, len1-1, len2-1);
     }
 
     private int findLCS(String text1, String text2, int i, int j){
-        if(i>=text1.length() || j>=text2.length()) return 0;
+        if(i<0 || j<0) return 0;
         if(dp[i][j]!=null) return dp[i][j];
         if(text1.charAt(i)==text2.charAt(j)){
-            dp[i][j] = 1 + findLCS(text1, text2, i+1, j+1);
+            dp[i][j] = 1 + findLCS(text1, text2, i-1, j-1);
         }
-        else dp[i][j] = Math.max(findLCS(text1, text2, i+1, j), findLCS(text1, text2, i, j+1));
+        else dp[i][j] = Math.max(findLCS(text1, text2, i-1, j), findLCS(text1, text2, i, j-1));
 
         return dp[i][j];
     }
