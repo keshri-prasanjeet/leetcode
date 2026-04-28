9019df5a3e390778df1b700403f7b00c13b4627b
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-04-28 06:24:57 +0530

    1250-LONGEST-COMMON-SUBSEQUENCE Stats: Time: 20 ms (78.05%), Space: 54 MB (35.23%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -3,26 +3,19 @@ class Solution {
         int len1 = text1.length();
         int len2 = text2.length();
 
-        int[][] dp = new int[len1][len2];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        int[][] dp = new int[len1+1][len2+1];
+        
+        for(int i=1;i<=len1;i++){
+            for(int j=1;j<=len2;j++){
+                char first = text1.charAt(i-1);
+                char second = text2.charAt(j-1);
+                if(first == second){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
-        return findLCE(dp, text1, text2, 0, 0);
-    }
-
-    private int findLCE(int[][] dp, String text1, String text2, int i, int j){
-        if(i==text1.length() || j==text2.length()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int res = 0;
-        if(text1.charAt(i)==text2.charAt(j)){
-            res = 1+ findLCE(dp, text1, text2, i+1, j+1);
-        }
-        else{
-            res = Math.max(
-                findLCE(dp, text1, text2, i+1, j),
-                findLCE(dp, text1, text2, i, j+1)
-            );
-        }
-        return dp[i][j] = res;
+        return dp[len1][len2];
     }
 }
