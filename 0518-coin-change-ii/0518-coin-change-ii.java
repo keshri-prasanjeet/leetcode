51606ca9a71f3cb684ea3ee395b1b0d957aef875
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-05-11 06:50:45 +0530

    0518-COIN-CHANGE-II Stats: Time: 30 ms (13.79%), Space: 64.5 MB (5.23%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,16 +1,29 @@
 class Solution {
     Integer[][] dp;
     public int change(int amount, int[] coins) {
-        dp = new Integer[amount+1][coins.length];
+        //the coins available to me
+        //total money = amount
+        //return number of combinations to make that amount
+        //if amount of money cannot be made ever then return 0
+
+        //at each step i have two choices, take or skip
+        //if i am able to make the target as 0 then the amount has been made
+        // and i can return 1
+        dp = new Integer[amount+1][coins.length+1];
         return doCoinChange(amount, coins, 0);
     }
 
-    private int doCoinChange(int target, int[] coins, int idx){
-        if(target == 0) return 1;
-        if(target < 0 || idx >= coins.length) return 0;
-        if(dp[target][idx]!=null) return dp[target][idx];
-        int take = doCoinChange(target - coins[idx], coins, idx);
-        int skip = doCoinChange(target, coins, idx+1);
-        return dp[target][idx] = take + skip;
+    private int doCoinChange(int amount, int[] coins, int index){
+        if(amount ==0) return 1;
+        if(amount < 0) return 0;
+        if(dp[amount][index]!=null) return dp[amount][index];
+        if(index >= coins.length) return 0;
+
+        //two choices
+
+        int take = doCoinChange(amount - coins[index], coins, index);
+        int skip = doCoinChange(amount, coins, index+1);
+        
+        return dp[amount][index] = take+skip;
     }
 }
