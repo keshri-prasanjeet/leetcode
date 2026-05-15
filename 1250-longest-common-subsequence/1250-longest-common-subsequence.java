2d32fb21188375c8e25dc279870ca5586c78778f
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-05-15 08:31:32 +0530

    1250-LONGEST-COMMON-SUBSEQUENCE Stats: Time: 32 ms (9.91%), Space: 53.9 MB (50.69%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,16 +1,19 @@
 class Solution {
-    Integer[][] dp;
+    int[][] dp;
     public int longestCommonSubsequence(String text1, String text2) {
         //break it into subproblem
         //we got two choices go right or go down
         //if our current cell is match we add 1 + Max(right, down) and return
-        dp = new Integer[text1.length()+1][text2.length()+1];
+        dp = new int[text1.length()+1][text2.length()+1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
         return findLCS(text1, text2, 0, 0);
     }
 
     private int findLCS(String text1, String text2, int i, int j){
         if(i == text1.length() || j == text2.length()) return 0;
-        if(dp[i][j]!=null) return dp[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
         int maxCommon = 0;
         if(text1.charAt(i) == text2.charAt(j)) maxCommon = 1 + findLCS(text1, text2, i+1, j+1);
         else maxCommon = Math.max(findLCS(text1, text2, i+1, j), findLCS(text1, text2, i, j+1));
