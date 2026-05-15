b34babc8ee96a46002f83939b34a1f93c3633a46
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-05-15 08:01:07 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 42.2 MB (46.52%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,20 @@
 class Solution {
+    int globalM;
+    int globalN;
+    Integer[][] dp;
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-        for(int i=0;i<m;i++){
-            dp[i][0] = 1;
-        }
-        for(int i=0;i<n;i++){
-            dp[0][i] = 1;
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+        //return the number of unique paths the robot can take
+        //if the robot is at column above or row left of target it can only take one step
+        globalM = m;
+        globalN = n;
+        dp = new Integer[m+1][n+1];
+        return findUniquePaths(0,0);
+    }
+
+    private int findUniquePaths(int i, int j){
+        if(i==globalM-1) return 1;
+        if(j==globalN-1) return 1;
+        if(dp[i][j]!=null) return dp[i][j];
+        return dp[i][j] = findUniquePaths(i+1,j) + findUniquePaths(i, j+1);
     }
 }
