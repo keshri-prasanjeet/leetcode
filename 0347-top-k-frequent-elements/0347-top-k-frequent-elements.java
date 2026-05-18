8e8369422d5783ca3e11c1f50d95da3f71d43d00
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-05-18 19:57:26 +0530

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 15 ms (54.46%), Space: 47.6 MB (47.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        //make a priority queue to put the more frequent items in the front and then deque it
         Map<Integer, Integer> frequencyMap = new HashMap<>();
-        Queue<Integer> pq = new PriorityQueue<>((a,b) -> Integer.compare(frequencyMap.get(b), frequencyMap.get(a)));
+        Queue<Integer> pq = new PriorityQueue<>(
+                        (a,b) -> Integer.compare(frequencyMap.get(b), frequencyMap.get(a))
+                    );
         for(int num: nums){
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0)+1);
         }
 
-        frequencyMap.forEach((key, valeu) -> {
+        frequencyMap.forEach((key, value) -> {
             pq.offer(key);
         });
         int[] answer = new int[k];
-        int i=0;
+        int i =0;
         while(i<k){
-            answer[i] = pq.poll();
-            i++;
+            answer[i++] = pq.poll();
         }
-        return answer; 
+        return answer;
     }
 }
