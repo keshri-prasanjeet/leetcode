a6032b8bf1064e53ed3d7bf5d1f202cc3f0ecc3b
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-05-21 06:53:21 +0530

    0042-TRAPPING-RAIN-WATER Stats: Time: 298 ms (4.5%), Space: 48.2 MB (34.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,26 @@
 class Solution {
     public int trap(int[] height) {
         int len = height.length;
-        int[] maxHFromLeft = new int[len];
-        int[] maxHFromRight= new int[len];
-        maxHFromLeft[0] = height[0];
-        maxHFromRight[len-1] = height[len-1];
-
-        for(int i=1;i<len;i++){
-            maxHFromLeft[i] = Math.max(maxHFromLeft[i-1], height[i]);
+        int[] maxL = new int[len];
+        int[] maxR = new int[len];
+        maxL[0] = height[0];
+        maxR[len-1] = height[len-1];
+        for(int i = 1;i<len;i++){
+            maxL[i] = Math.max(maxL[i-1], height[i]);
         }
 
-        for(int j=len-2;j>=0;j--){
-            maxHFromRight[j]= Math.max(maxHFromRight[j+1], height[j]);
+        for(int i = len-2;i>=0;i--){
+            maxR[i] = Math.max(maxR[i+1], height[i]);
         }
 
-        int totalWater = 0;
+        Arrays.stream(maxL).forEach(System.out::println);
+        System.out.println("---");
+        Arrays.stream(maxR).forEach(System.out::println);
+        int trappedWater = 0;
         for(int i=0;i<len;i++){
-            totalWater += Math.abs(Math.min(maxHFromLeft[i],maxHFromRight[i]) - height[i]);
+            int water = Math.min(maxL[i], maxR[i]) - height[i];
+            trappedWater += water > 0 ? water : 0;
         }
-        return totalWater;
+        return trappedWater;
     }
 }
