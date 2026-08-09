9e2216f2670f332044f2f59d7cc48987cca54e66
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-08-10 02:14:31 +0530

    0128-LONGEST-CONSECUTIVE-SEQUENCE Stats: Time: 39 ms (11.76%), Space: 95.4 MB (61.5%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,24 +1,23 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         Set<Integer> numSet = new HashSet<>();
-        int longestConSeq = 0;
-        for(int num:nums){
+        int longestSeq = 0;
+        for(int num: nums){
             numSet.add(num);
         }
         return numSet.stream()
-            .filter(num -> isStartOfSequence(numSet, num))
-            .mapToInt(num -> countSequenceLength(numSet, num))
-            .max()
-            .orElse(0);
+        .filter(num -> isStartOfSequence.test(numSet, num))
+        .mapToInt(num -> countSequenceLength.apply(numSet, num))
+        .max()
+        .orElse(0);
     }
 
-    private boolean isStartOfSequence(Set<Integer> numSet, int num){
-        return (!numSet.contains(num-1));
-    }
+    private BiPredicate<Set<Integer>, Integer> isStartOfSequence = (numSet, num) -> !numSet.contains(num-1);
 
-    private int countSequenceLength(Set<Integer> numSet, int num){
+    private BiFunction<Set<Integer>, Integer, Integer> countSequenceLength = (numSet, num) -> {
         int count = 0;
         while(numSet.contains(num++)) count++;
         return count;
-    }
+    };
+
 }
