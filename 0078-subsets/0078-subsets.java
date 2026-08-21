4547209cb3c23aced9c8baf68835aaafb0427e1d
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-08-21 19:30:57 +0530

    0078-SUBSETS Stats: Time: 1 ms (86.47%), Space: 44.5 MB (7%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,15 +2,15 @@ class Solution {
     List<List<Integer>> answer;
     public List<List<Integer>> subsets(int[] nums) {
         answer = new ArrayList<>();
-        findSubsets(nums, 0, new ArrayList<>());
+        findSubsets(0, nums, new ArrayList<>());
         return answer;
     }
 
-    private void findSubsets(int[] nums, int idx, List<Integer> subset){
+    private void findSubsets(int idx, int[] nums, List<Integer> subset){
         answer.add(new ArrayList<>(subset));
         for(int i=idx;i<nums.length;i++){
             subset.add(nums[i]);
-            findSubsets(nums, i+1, subset);
+            findSubsets(i+1, nums, subset);
             subset.remove(subset.size()-1);
         }
     }
