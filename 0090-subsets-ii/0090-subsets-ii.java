6fcfc971ebc5a7e7b520392b1e8ea57f9e1e7b73
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-08-22 14:42:15 +0530

    0090-SUBSETS-II Stats: Time: 3 ms (54.9%), Space: 45.2 MB (42.34%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,21 +1,18 @@
 class Solution {
-    List<List<Integer>> answer;
+    List<List<Integer>> noDuplicatesSubset;
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        answer = new ArrayList<>();
         Arrays.sort(nums);
-        findAllSubsets(nums, new ArrayList<>(), 0);
-        return answer;
+        noDuplicatesSubset = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), 0);
+        return noDuplicatesSubset;
     }
 
-    private void findAllSubsets(int[] nums, List<Integer> subset, int idx){
-        // if(idx == nums.length) return;
-
-        answer.add(new ArrayList<>(subset));
-
-        for(int i=idx;i<nums.length;i++){
-            if(i > idx && nums[i] == nums[i-1]) continue;
+    private void backtrack(int[] nums, List<Integer> subset, int start){
+        noDuplicatesSubset.add(new ArrayList<>(subset));
+        for(int i=start; i< nums.length;i++){
+            if(i > start && nums[i] == nums[i-1]) continue;
             subset.add(nums[i]);
-            findAllSubsets(nums, subset, i+1);
+            backtrack(nums, subset, i+1);
             subset.remove(subset.size()-1);
         }
     }
