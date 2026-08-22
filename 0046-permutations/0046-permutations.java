22dfac2791f467de193c3827848f5b1442508398
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-08-22 14:28:11 +0530

    0046-PERMUTATIONS Stats: Time: 1 ms (87.45%), Space: 45.2 MB (95.51%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,24 @@
 class Solution {
-    List<List<Integer>> answer;
-    boolean[] usedNums;
+    List<List<Integer>> allPermutations;
     public List<List<Integer>> permute(int[] nums) {
-        answer = new ArrayList<>();
-        usedNums=new boolean[nums.length];
-        findPermutations(nums, new ArrayList<>(), 0);
-        return answer;
+        allPermutations = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), new boolean[nums.length]);
+        return allPermutations;
     }
 
-    private void findPermutations(int[] nums, ArrayList<Integer> permutation, int idx){
-        if(idx  == nums.length){
-            answer.add(new ArrayList<>(permutation));
+    private void backtrack(int[] nums, List<Integer> permutation, boolean[] memberCheck){
+        if(permutation.size() == nums.length){
+            allPermutations.add(new ArrayList<>(permutation));
             return;
         }
+
         for(int i=0;i<nums.length;i++){
-            if(!usedNums[i]){
-                usedNums[i] = true;
-                permutation.add(nums[i]);
-                findPermutations(nums, permutation, idx+1);
-                usedNums[i] = false;
-                permutation.remove(permutation.size()-1);
-            }
+            if(memberCheck[i]) continue;
+            memberCheck[i] = true;
+            permutation.add(nums[i]);
+            backtrack(nums, permutation, memberCheck);
+            permutation.remove(permutation.size()-1);
+            memberCheck[i] = false;
         }
     }
 }
