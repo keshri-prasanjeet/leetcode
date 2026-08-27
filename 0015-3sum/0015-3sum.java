ea6b27f6b20eb20524112859f7b24234701ba086
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-08-27 10:48:00 +0530

    0015-3SUM Stats: Time: 33 ms (72.1%), Space: 59.1 MB (59.75%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //lets try fixing first num + two pointers
         List<List<Integer>> answer = new ArrayList<>();
-        Arrays.sort(nums);
         int len = nums.length;
+        Arrays.sort(nums);
         for(int i=0;i<len-2;i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            int first = nums[i];
-            int j = i+1;
-            int k = len-1;
-            while(j < k){
-                int tSum = first + nums[j] + nums[k];
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int first = i;
+            int second = i+1;
+            int third = len-1;
+            while(second < third){
+                int tSum = nums[first] + nums[second] + nums[third];
                 if(tSum == 0){
-                    answer.add(new ArrayList<>(List.of(nums[i], nums[j], nums[k])));
-                    j++;
-                    k--;
-                    while(j < k && nums[j] == nums[j-1]) j++;
-                    while(j < k && nums[k] == nums[k+1]) k--; 
+                    answer.add(List.of(nums[first], nums[second], nums[third]));
+                    second++;
+                    third--;
+                    while(second < third && nums[second] == nums[second-1]) second++;
+                    while(second < third && nums[third] == nums[third+1]) third--;
                 }
-                else if(tSum > 0){
-                    k--;
+                else if(tSum < 0){
+                    second++;
                 }
-                else j++;
+                else third--;
             }
         }
         return answer;
