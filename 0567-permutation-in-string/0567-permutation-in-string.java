fce6c536ac62a320282dce69dd3fa0b654a0ead4
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-08-29 20:19:23 +0530

    0567-PERMUTATION-IN-STRING Stats: Time: 5 ms (97.42%), Space: 43.8 MB (93.12%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,31 +1,28 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int s2Len = s2.length();
         int s1Len = s1.length();
+        int s2Len = s2.length();
         if(s1Len > s2Len) return false;
-        int left = 0;
-        int right = 0;
-        int req = s1Len;
+        // if(s1Len == s2Len && !s1.equals(s2)) return false;
 
         int[] charMap = new int[26];
+
         for(char a: s1.toCharArray()){
-            charMap[a-'a']++;
+            charMap[a - 'a']++;
         }
-
-        while(right < s2Len){
+        int req = s1Len;
+        int left = 0;
+        for(int right = 0;right<s2Len;right++){
             char rightChar = s2.charAt(right);
-            if(charMap[rightChar - 'a']>0) req--;
+            if(charMap[rightChar - 'a'] > 0) req--;
             charMap[rightChar - 'a']--;
-
             if(right - left + 1 == s1Len){
-                //window has been created
                 if(req == 0) return true;
                 char leftChar = s2.charAt(left);
-                if(charMap[leftChar - 'a']>=0) req++;
+                if(charMap[leftChar-'a']>=0) req++;
                 charMap[leftChar - 'a']++;
                 left++;
             }
-            right++;
         }
         return false;
     }
