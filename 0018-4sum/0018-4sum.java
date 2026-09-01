299195fa11c571feaabf20322281b5443b07e078
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-09-01 10:14:51 +0530

    0018-4SUM Stats: Time: 20 ms (66.49%), Space: 45.4 MB (97.46%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,29 +1,27 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        int len = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> answer = new ArrayList<>();
-        int len = nums.length;
-        for(int i=0;i<len-3;i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            for(int j=i+1;j<len-2;j++){
-                if(j > i+1 && nums[j] == nums[j-1]) continue;
-                int left = j+1;
-                int right = len-1;
-                while(left < right){
-                    long curSum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-                    if(curSum > target){
-                        right--;
+        for(int first = 0;first < len-3;first++){
+            if(first > 0 && nums[first] == nums[first-1]) continue;
+            for(int second = first+1;second<len-2;second++){
+                if(second > first+1 && nums[second] == nums[second-1]) continue;
+                int third = second+1;
+                int fourth = len-1;
+                while(third < fourth){
+                    long fourSome = (long) nums[first] + nums[second] + nums[third] + nums[fourth];
+                    if(fourSome == target){
+                        answer.add(Arrays.asList(nums[first], nums[second], nums[third], nums[fourth]));
+                        third++;
+                        fourth--;
+                        while(third < fourth && nums[third] == nums[third-1]) third++;
+                        while(third < fourth && nums[fourth] == nums[fourth+1]) fourth--;
                     }
-                    else if(curSum < target){
-                        left++;
+                    else if(fourSome < target){
+                        third++;
                     }
-                    else{
-                        answer.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        left++;
-                        right--;
-                        while(left < right && nums[left] == nums[left-1]) left++;
-                        while(right > left && nums[right] == nums[right+1]) right--;
-                    }
+                    else fourth--;
                 }
             }
         }
