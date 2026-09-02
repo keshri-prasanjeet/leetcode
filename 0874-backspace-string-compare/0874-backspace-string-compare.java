e5c2da6aefc4564c5a381d0de490deed210fd7df
Prasanjeet Keshri <keshriprasanjeet2@outlook.com>
2026-09-02 19:40:01 +0530

    0874-BACKSPACE-STRING-COMPARE Stats: Time: 1 ms (81.9%), Space: 42.9 MB (79.02%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.java
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -1,55 +1,43 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+        int sLen = s.length();
+        int tLen = t.length();
+        int i = sLen-1;
+        int j = tLen-1;
+        while(i>=0 || j>=0){
 
-        int i = s.length() - 1;
-        int j = t.length() - 1;
-
-        while (i >= 0 || j >= 0) {
-
-            // Find next valid character in s
             int skip = 0;
 
-            while (i >= 0) {
-                if (s.charAt(i) == '#') {
+            while(i>=0){
+                if(s.charAt(i) == '#'){
                     skip++;
                     i--;
                 }
-                else if (skip > 0) {
+                else if(skip >0){
                     skip--;
                     i--;
                 }
-                else {
-                    break;
-                }
+                else break;
             }
 
-            // Find next valid character in t
             skip = 0;
 
-            while (j >= 0) {
-                if (t.charAt(j) == '#') {
+            while(j>=0){
+                if(t.charAt(j) == '#'){
                     skip++;
                     j--;
                 }
-                else if (skip > 0) {
+                else if(skip > 0){
                     skip--;
                     j--;
                 }
-                else {
-                    break;
-                }
+                else break;
             }
 
-            // If both have valid characters, compare them
-            if (i >= 0 && j >= 0) {
-                if (s.charAt(i) != t.charAt(j)) {
-                    return false;
-                }
+            if(i>=0 && j>=0){
+                if(s.charAt(i)!=t.charAt(j)) return false;
             }
-            // One string still has a character, the other doesn't
-            else if (i >= 0 || j >= 0) {
-                return false;
-            }
+            else if(i>=0 || j>=0) return false;
 
             i--;
             j--;
